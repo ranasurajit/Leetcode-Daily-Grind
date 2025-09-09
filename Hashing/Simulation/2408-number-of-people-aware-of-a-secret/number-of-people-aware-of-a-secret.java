7714936d46d036cc97a7a02ae5161130822b408f
Surajit Rana <surajit1602@gmail.com>
2025-09-09 11:20:17 +0530

    Time: 214 ms (5.45%) | Memory: 44.6 MB (5.45%) - LeetSync

--- Hashing/Simulation/2408-number-of-people-aware-of-a-secret/number-of-people-aware-of-a-secret.java
+++ Hashing/Simulation/2408-number-of-people-aware-of-a-secret/number-of-people-aware-of-a-secret.java
@@ -1,11 +1,17 @@
 class Solution {
     private static final int MOD = (int) 1e9 + 7;
  
+    /**
+     * Approach : Using Hashing + Simulation Approach
+     *
+     * TC: O(N x N) + O(N) ~ O(N x N)
+     * SC: O(N)
+     */
     public int peopleAwareOfSecret(int n, int delay, int forget) {
-        Map<Integer, Integer> map = new HashMap<Integer, Integer>();
+        Map<Integer, Integer> map = new HashMap<Integer, Integer>(); // SC: O(N)
         map.put(1, 1);
-        for (int d = 2; d <= n; d++) {
-            for (Integer key : new ArrayList<Integer>(map.keySet())) {
+        for (int d = 2; d <= n; d++) { // TC: O(N)
+            for (Integer key : new ArrayList<Integer>(map.keySet())) { // TC: O(N)
                 if (d - key >= delay && d - key < forget) {
                     map.put(d, (map.getOrDefault(d, 0) + (map.get(key)) % MOD) % MOD);
                 }
@@ -15,7 +21,7 @@ class Solution {
             }
         }
         int count = 0;
-        for (Integer key : map.keySet()) {
+        for (Integer key : map.keySet()) { // TC: O(N)
             count = (count + map.get(key)) % MOD;
         }
         return count;
