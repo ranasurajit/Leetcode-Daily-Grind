15208fef4d203532953e184bfeadcfb7038de4e7
Surajit Rana <surajit1602@gmail.com>
2025-09-15 10:23:51 +0530

    Time: 5 ms (21.81%) | Memory: 42.1 MB (89.56%) - LeetSync

--- Hashing/Simulation/1264-maximum-number-of-words-you-can-type/maximum-number-of-words-you-can-type.java
+++ Hashing/Simulation/1264-maximum-number-of-words-you-can-type/maximum-number-of-words-you-can-type.java
@@ -1,13 +1,50 @@
 class Solution {
     /**
-     * Approach : Using Hashing + String Simulation Approach
+     * Approach II : Using Hashing + String Simulation (Better) Approach
+     *
+     * TC: O(N) + O(M) ~ O(N + M)
+     * SC: O(N)
+     *
+     * where M = size(text)
+     */
+    public int canBeTypedWords(String text, String brokenLetters) {
+        int n = brokenLetters.length();
+        int m = text.length();
+        Set<Character> brokenKeySet = new HashSet<Character>(); // SC: O(N)
+        for (int i = 0; i < n; i++) { // TC: O(N)
+            brokenKeySet.add(brokenLetters.charAt(i));
+        }
+        int countTyped = 0;
+        boolean isBroken = false;
+        for (int i = 0; i < m; i++) { // TC: O(M)
+            char ch = text.charAt(i);
+            if (brokenKeySet.contains(ch)) {
+                isBroken = true;
+            }
+            if (ch == ' ') {
+                if (!isBroken) {
+                    countTyped++;
+                }
+                // reset flag
+                isBroken = false;
+            }
+        }
+        // check last word
+        if (!isBroken) {
+            countTyped++;
+        }
+        return countTyped;
+    }
+
+    /**
+     * Approach I : Using Hashing + String Simulation Approach
      *
      * TC: O(N) + O(M x L) ~ O(N + M x L)
      * SC: O(N + M)
      *
      * where M = number of words in String 'text' and L = maximum length of words in String 'text'
      */
-    public int canBeTypedWords(String text, String brokenLetters) {
+    public int canBeTypedWordsApproachI(String text, String brokenLetters) {
         int n = brokenLetters.length();
         Set<Character> brokenKeySet = new HashSet<Character>(); // SC: O(N)
         for (int i = 0; i < n; i++) { // TC: O(N)
