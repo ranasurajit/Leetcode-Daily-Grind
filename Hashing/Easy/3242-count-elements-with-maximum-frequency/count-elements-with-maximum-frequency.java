e56e3d213bbfdb81a87be78ab1af5c79889f0eee
Surajit Rana <surajit1602@gmail.com>
2025-09-22 11:33:29 +0530

    Time: 2 ms (75.64%) | Memory: 42.5 MB (43.14%) - LeetSync

--- Hashing/Easy/3242-count-elements-with-maximum-frequency/count-elements-with-maximum-frequency.java
+++ Hashing/Easy/3242-count-elements-with-maximum-frequency/count-elements-with-maximum-frequency.java
@@ -1,11 +1,35 @@
 class Solution {
     /**
-     * Approach : Using Hashing Approach
+     * Approach II : Using Hashing Approach (Single Pass)
+     *
+     * TC: O(N)
+     * SC: O(N)
+     */
+    public int maxFrequencyElements(int[] nums) {
+        int n = nums.length;
+        int maxFreq = 0;
+        int totFreq = 0;
+        Map<Integer, Integer> map = new HashMap<Integer, Integer>(); // SC: O(N)
+        for (int i = 0; i < n; i++) { // TC: O(N)
+            map.put(nums[i], map.getOrDefault(nums[i], 0) + 1);
+            int freq = map.get(nums[i]);
+            if (freq > maxFreq) {
+                maxFreq = freq;
+                totFreq = freq;
+            } else if (freq == maxFreq) {
+                totFreq += freq;
+            }
+        }
+        return totFreq;
+    }
+
+    /**
+     * Approach I : Using Hashing Approach (Three Pass)
      *
      * TC: O(N) + O(N) + O(N) ~ O(N)
      * SC: O(N)
      */
-    public int maxFrequencyElements(int[] nums) {
+    public int maxFrequencyElementsApproachI(int[] nums) {
         int n = nums.length;
         Map<Integer, Integer> map = new HashMap<Integer, Integer>(); // SC: O(N)
         for (int i = 0; i < n; i++) { // TC: O(N)
