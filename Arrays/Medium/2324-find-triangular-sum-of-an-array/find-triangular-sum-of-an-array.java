9a8036f0666cfdffa82a89cd42ff834762a0588a
Surajit Rana <surajit1602@gmail.com>
2025-09-30 16:08:33 +0530

    Time: 81 ms (19.18%) | Memory: 44.3 MB (94.42%) - LeetSync

--- Arrays/Medium/2324-find-triangular-sum-of-an-array/find-triangular-sum-of-an-array.java
+++ Arrays/Medium/2324-find-triangular-sum-of-an-array/find-triangular-sum-of-an-array.java
@@ -3,16 +3,14 @@ class Solution {
      * Approach : Using Array Simulation Approach
      *
      * TC: O(N x N)
-     * SC: O(N)
+     * SC: O(1)
      */
     public int triangularSum(int[] nums) {
         int n = nums.length;
         for (int i = 1; i < n; i++) {         // TC: O(N)
-            int[] sum = new int[n];           // SC: O(N)
             for (int j = 0; j < n - 1; j++) { // TC: O(N)
-                sum[j] = (nums[j] + nums[j + 1]) % 10;
+                nums[j] = (nums[j] + nums[j + 1]) % 10;
             }
-            nums = sum.clone();
         }
         return nums[0];
     }
