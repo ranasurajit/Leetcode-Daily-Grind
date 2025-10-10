9342427acf48a2eccbde1dfa83ea4e098be8b79a
Surajit Rana <surajit1602@gmail.com>
2025-10-10 15:38:26 +0530

    Time: 9 ms (14.48%) | Memory: 63 MB (31.03%) - LeetSync

---
+++ Arrays/PrefixSuffix/Medium/3383-taking-maximum-energy-from-the-mystic-dungeon/taking-maximum-energy-from-the-mystic-dungeon.java
@@ -0,0 +1,15 @@
+class Solution {
+    /**
+     * Approach : Using Array + Prefix Sum Approach
+     *
+     * TC: O(N)
+     * SC: O(N)
+     */
+    public int maximumEnergy(int[] energy, int k) {
+        int[] dp = energy.clone();
+        for (int i = energy.length - 1 - k; i >= 0; --i) {
+            dp[i] += dp[i + k];
+        }
+        return Arrays.stream(dp).max().getAsInt();
+    }
+}
