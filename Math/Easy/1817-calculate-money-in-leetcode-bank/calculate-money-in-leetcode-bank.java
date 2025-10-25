2bc7f79e1ba1c2781342151468e466149cdfcbc0
Surajit Rana <surajit1602@gmail.com>
2025-10-25 11:26:43 +0530

    Time: 2 ms (17.17%) | Memory: 40.3 MB (90.33%) - LeetSync

--- Math/Easy/1817-calculate-money-in-leetcode-bank/calculate-money-in-leetcode-bank.java
+++ Math/Easy/1817-calculate-money-in-leetcode-bank/calculate-money-in-leetcode-bank.java
@@ -1,11 +1,32 @@
 class Solution {
     /**
-     * Approach : Using Math Approach
+     * Approach II : Using Math Approach (Without Extra Space)
+     *
+     * TC: O(N)
+     * SC: O(1)
+     */
+    public int totalMoney(int n) {
+        int prev = 1;
+        int begin = 1;
+        int balance = prev;
+        for (int i = 1; i < n; i++) { // TC: O(N)
+            int current = (i % 7 == 0) ? begin + 1 : prev + 1;
+            balance += current;
+            prev = current;
+            if (i % 7 == 0) {
+                begin = current;
+            }
+        }
+        return balance;
+    }
+
+    /**
+     * Approach I : Using Math Approach (With Extra Space)
      *
      * TC: O(N)
      * SC: O(N)
      */
-    public int totalMoney(int n) {
+    public int totalMoneyApproachI(int n) {
         int[] dp = new int[n]; // SC: O(N)
         dp[0] = 1;
         int balance = dp[0];
