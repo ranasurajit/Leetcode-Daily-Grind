e8e6a54212361e74e5df808028739f78274731a2
Surajit Rana <surajit1602@gmail.com>
2025-10-27 17:24:45 +0530

    Time: 11 ms (80.94%) | Memory: 45.1 MB (76.08%) - LeetSync

--- Arrays/2DArrays/Medium/2244-number-of-laser-beams-in-a-bank/number-of-laser-beams-in-a-bank.java
+++ Arrays/2DArrays/Medium/2244-number-of-laser-beams-in-a-bank/number-of-laser-beams-in-a-bank.java
@@ -6,13 +6,13 @@ class Solution {
      * SC: O(1)
      */
     public int numberOfBeams(String[] bank) {
-        int n = bank.length;
+        int n = bank[0].length();
         int prevDeviceCount = 0;
         int beams = 0;
-        for (int i = 0; i < n; i++) { // TC: O(M)
+        for (String device : bank) { // TC: O(M)
             int currentDeviceCount = 0;
-            for (int j = 0; j < bank[i].length(); j++) { // TC: O(N)
-                currentDeviceCount += bank[i].charAt(j) == '1' ? 1 : 0;
+            for (int i = 0; i < n; i++) { // TC: O(N)
+                currentDeviceCount += device.charAt(i) == '1' ? 1 : 0;
             }
             if (currentDeviceCount == 0) {
                 currentDeviceCount = prevDeviceCount;
