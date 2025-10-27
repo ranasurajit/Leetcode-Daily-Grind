16c9ffdcf87f4e0dbd527bae968bbba6abe3e712
Surajit Rana <surajit1602@gmail.com>
2025-10-27 17:10:10 +0530

    Time: 13 ms (15.08%) | Memory: 45.4 MB (35.36%) - LeetSync

---
+++ Arrays/2DArrays/Medium/2244-number-of-laser-beams-in-a-bank/number-of-laser-beams-in-a-bank.java
@@ -0,0 +1,29 @@
+class Solution {
+    /**
+     * Approach : Using Matrix + Prefix Sum Approach
+     *
+     * TC: O(N) + O(M x N) ~ O(M x N)
+     * SC: O(1)
+     */
+    public int numberOfBeams(String[] bank) {
+        int n = bank.length;
+        int prevDeviceCount = 0;
+        for (int i = 0; i < bank[0].length(); i++) { // TC: O(N)
+            prevDeviceCount += bank[0].charAt(i) == '1' ? 1 : 0;
+        }
+        int beams = 0;
+        for (int i = 1; i < n; i++) { // TC: O(M)
+            int currentDeviceCount = 0;
+            for (int j = 0; j < bank[i].length(); j++) { // TC: O(N)
+                currentDeviceCount += bank[i].charAt(j) == '1' ? 1 : 0;
+            }
+            if (currentDeviceCount == 0) {
+                currentDeviceCount = prevDeviceCount;
+            } else {
+                beams += currentDeviceCount * prevDeviceCount;
+            }
+            prevDeviceCount = currentDeviceCount;
+        }
+        return beams;
+    }
+}
