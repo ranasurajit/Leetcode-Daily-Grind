b757c6790e78aa1f4c79edd040b69a190435f78e
Surajit Rana <surajit1602@gmail.com>
2025-10-30 16:21:43 +0530

    Time: 4 ms (27.50%) | Memory: 58.6 MB (78.13%) - LeetSync

--- GreedyAlgorithm/Hard/3454-minimum-operations-to-make-array-equal-to-target/minimum-operations-to-make-array-equal-to-target.java
+++ GreedyAlgorithm/Hard/3454-minimum-operations-to-make-array-equal-to-target/minimum-operations-to-make-array-equal-to-target.java
@@ -1,11 +1,39 @@
 class Solution {
     /**
-     * Approach : Using Greedy Approach
+     * Approach II : Using Greedy (One Pass) Approach
+     *
+     * TC: O(N)
+     * SC: O(1)
+     */
+    public long minimumOperations(int[] nums, int[] target) {
+        int n = nums.length;
+        int prev = 0;
+        long operations = 0L;
+        for (int i = 0; i < n; i++) { // TC: O(N)
+            int current = target[i] - nums[i];
+            if ((prev < 0 && current > 0) || (prev > 0 && current < 0)) {
+                /**
+                 * if prev and current are of different signs, 
+                 * then we need to increment/decrement separately
+                 */
+                operations += Math.abs(current);
+            } else {
+                if (Math.abs(current) - Math.abs(prev) > 0) {
+                    operations += Math.abs(current) - Math.abs(prev);
+                }
+            }
+            prev = current;
+        }
+        return operations;
+    }
+
+    /**
+     * Approach I : Using Greedy (Two Pass) Approach
      *
      * TC: O(N) + O(N) ~ O(N)
      * SC: O(N)
      */
-    public long minimumOperations(int[] nums, int[] target) {
+    public long minimumOperationsBetter(int[] nums, int[] target) {
         int n = nums.length;
         long[] diff = new long[n]; // SC: O(N)
         for (int i = 0; i < n; i++) { // TC: O(N)
