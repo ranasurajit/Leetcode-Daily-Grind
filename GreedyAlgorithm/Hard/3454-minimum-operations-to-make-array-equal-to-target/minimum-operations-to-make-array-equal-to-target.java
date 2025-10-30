5f00fa9313c493d2f04fc810e6c7954f320f64f2
Surajit Rana <surajit1602@gmail.com>
2025-10-30 16:10:43 +0530

    Time: 6 ms (3.75%) | Memory: 57.7 MB (99.69%) - LeetSync

---
+++ GreedyAlgorithm/Hard/3454-minimum-operations-to-make-array-equal-to-target/minimum-operations-to-make-array-equal-to-target.java
@@ -0,0 +1,32 @@
+class Solution {
+    /**
+     * Approach : Using Greedy Approach
+     *
+     * TC: O(N) + O(N) ~ O(N)
+     * SC: O(N)
+     */
+    public long minimumOperations(int[] nums, int[] target) {
+        int n = nums.length;
+        long[] diff = new long[n]; // SC: O(N)
+        for (int i = 0; i < n; i++) { // TC: O(N)
+            diff[i] = (long) target[i] - (long) nums[i];
+        }
+        long prev = 0L;
+        long operations = 0L;
+        for (int i = 0; i < n; i++) { // TC: O(N)
+            long current = (long) diff[i];
+            if ((prev < 0 && current > 0) || (prev > 0 && current < 0)) {
+                /**
+                 * if prev and current are of different signs, 
+                 * then we need to increment/decrement separately
+                 */
+                operations += Math.abs(current);
+            } else {
+                operations += Math.abs(current) - Math.abs(prev) > 0 ? 
+                    Math.abs(current) - Math.abs(prev) : 0;
+            }
+            prev = current;
+        }
+        return operations;
+    }
+}
