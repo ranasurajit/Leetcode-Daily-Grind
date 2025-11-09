b670121dad48a62bbe752a81aa55a95dee637a7a
Surajit Rana <surajit1602@gmail.com>
2025-11-09 09:43:40 +0530

    Time: 0 ms (100.00%) | Memory: 42.2 MB (14.07%) - LeetSync

--- Math/Easy/2288-count-operations-to-obtain-zero/count-operations-to-obtain-zero.java
+++ Math/Easy/2288-count-operations-to-obtain-zero/count-operations-to-obtain-zero.java
@@ -1,11 +1,31 @@
 class Solution {
     /**
-     * Approach : Using Math Simulation Approach
+     * Approach II : Using Optimized Mathematical (Using Division) Approach
+     *
+     * TC: O(log(Max(num1, nums2)))
+     * SC: O(1)
+     */
+    public int countOperations(int num1, int num2) {
+        int operations = 0;
+        while (num1 > 0 && num2 > 0) { // TC: O(Max(num1, nums2))
+            if (num1 > num2) {
+                operations += (num1 / num2);
+                num1 %= num2;
+            } else {
+                operations += (num2 / num1);
+                num2 %= num1;
+            }
+        }
+        return operations;
+    }
+
+    /**
+     * Approach I : Using Brute-Force (Math Simulation) Approach
      *
      * TC: O(Max(num1, nums2))
      * SC: O(1)
      */
-    public int countOperations(int num1, int num2) {
+    public int countOperationsBruteForce(int num1, int num2) {
         int operations = 0;
         while (num1 > 0 && num2 > 0) { // TC: O(Max(num1, nums2))
             if (num1 > num2) {
