234c3ec922e51385808b91fd653a9fa3884e4f15
Surajit Rana <surajit1602@gmail.com>
2025-11-11 11:52:27 +0530

    Time: 82 ms (19.18%) | Memory: 100 MB (5.55%) - LeetSync

---
+++ Dynamic_Programming/Knapsack/Medium/474-ones-and-zeroes/ones-and-zeroes.java
@@ -0,0 +1,106 @@
+class Solution {
+    /**
+     * Approach II : Using Memoization Approach
+     *
+     * TC: O(K x P x M x N)
+     * SC: O(P x M x N) + O(P x M x N)
+     *
+     * Accepted (77 / 77 testcases passed)
+     */
+    public int findMaxForm(String[] strs, int m, int n) {
+        int P = strs.length;
+        int[][][] memo = new int[P][m + 1][n + 1]; // SC: O(P x M x N)
+        for (int[][] mem : memo) {
+            for (int[] me : mem) {
+                Arrays.fill(me, -1);
+            }
+        }
+        return solveMemoization(P - 1, strs, m, n, memo); // TC: O(K x P x M x N), SC: O(P x M x N)
+    }
+
+    /**
+     * Using Memoization Approach
+     *
+     * TC: O(K x P x M x N)
+     * SC: O(P x M x N)
+     */
+    private int solveMemoization(int idx, String[] strs, int m, int n, int[][][] memo) {
+        // Base Case
+        if (idx < 0) {
+            return 0;
+        }
+        if (m == 0 && n == 0) {
+            return 0;
+        }
+        // Memoization Check
+        if (memo[idx][m][n] != -1) {
+            return memo[idx][m][n];
+        }
+        // Recursion Calls
+        // pick or skip - returns the count of Strings added
+        int[] zerosOnes = getZerosAndOnes(strs[idx]); // TC: O(K)
+        int pick = 0;
+        if (zerosOnes[0] <= m && zerosOnes[1] <= n) {
+            // pick
+            pick = 1 + solveMemoization(idx - 1, strs, m - zerosOnes[0], n - zerosOnes[1], memo);
+        }
+        int skip = solveMemoization(idx - 1, strs, m, n, memo);
+        return memo[idx][m][n] = Math.max(pick, skip);
+    }
+
+    /**
+     * Approach I : Using Recursion Approach
+     *
+     * TC: O(K x 2 ^ N)
+     * SC: O(N)
+     *
+     * Time Limit Exceeded (27 / 77 testcases passed)
+     */
+    public int findMaxFormRecursion(String[] strs, int m, int n) {
+        return solveRecursion(strs.length - 1, strs, m, n); // TC: O(K x 2 ^ N), SC: O(N)
+    }
+
+    /**
+     * Using Recursion Approach
+     *
+     * TC: O(K x 2 ^ N)
+     * SC: O(N)
+     */
+    private int solveRecursion(int idx, String[] strs, int m, int n) {
+        // Base Case
+        if (idx < 0) {
+            return 0;
+        }
+        if (m == 0 && n == 0) {
+            return 0;
+        }
+        // Recursion Calls
+        // pick or skip - returns the count of Strings added
+        int[] zerosOnes = getZerosAndOnes(strs[idx]); // TC: O(K)
+        int pick = 0;
+        if (zerosOnes[0] <= m && zerosOnes[1] <= n) {
+            // pick
+            pick = 1 + solveRecursion(idx - 1, strs, m - zerosOnes[0], n - zerosOnes[1]);
+        }
+        int skip = solveRecursion(idx - 1, strs, m, n);
+        return Math.max(pick, skip);
+    }
+
+    /**
+     * Using String Simulation Approach
+     *
+     * TC: O(K)
+     * SC: O(1)
+     */
+    private int[] getZerosAndOnes(String s) {
+        int len = s.length();
+        int zeros = 0;
+        int ones = 0;
+        for (int i = 0; i < len; i++) { // TC: O(K)
+            if (s.charAt(i) == '1') {
+                ones++;
+            }
+        }
+        return new int[] { len - ones , ones };
+    }
+}
