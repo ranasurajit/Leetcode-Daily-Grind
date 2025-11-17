f9fd02fc1eecc422b4ce135b6bbd3621eff8911f
Surajit Rana <surajit1602@gmail.com>
2025-11-17 08:20:54 +0530

    Time: 1 ms (99.71%) | Memory: 65.5 MB (9.43%) - LeetSync

--- Arrays/Simulation/Easy/1548-check-if-all-1s-are-at-least-length-k-places-away/check-if-all-1s-are-at-least-length-k-places-away.java
+++ Arrays/Simulation/Easy/1548-check-if-all-1s-are-at-least-length-k-places-away/check-if-all-1s-are-at-least-length-k-places-away.java
@@ -1,11 +1,35 @@
 class Solution {
     /**
-     * Approach I : Using Brute-Force (Extra Space for Indices) Approach
+     * Approach II : Using Optimal Approach
      *
      * TC: O(N)
-     * SC: O(N)
+     * SC: O(1)
      */
     public boolean kLengthApart(int[] nums, int k) {
+        int n = nums.length;
+        int lastIdx = -1;
+        for (int i = 0; i < n; i++) { // TC: O(N)
+            if (nums[i] == 1) {
+                if (lastIdx == -1) {
+                    lastIdx = i;
+                } else {
+                    if (i - lastIdx - 1 < k) {
+                        return false;
+                    }
+                    lastIdx = i;
+                }
+            }
+        }
+        return true;
+    }
+
+    /**
+     * Approach I : Using Brute-Force (Extra Space for Indices) Approach
+     *
+     * TC: O(N) + O(N) ~ O(N)
+     * SC: O(N)
+     */
+    public boolean kLengthApartBruteForce(int[] nums, int k) {
         int n = nums.length;
         List<Integer> listOfOnesIndices = new ArrayList<Integer>(); // SC: O(N)
         for (int i = 0; i < n; i++) { // TC: O(N)
