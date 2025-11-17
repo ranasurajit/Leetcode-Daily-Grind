3ac144a9cfa6076219a26ce1cd5d3e9449b12c5a
Surajit Rana <surajit1602@gmail.com>
2025-11-17 08:23:51 +0530

    Time: 1 ms (99.71%) | Memory: 65.9 MB (5.71%) - LeetSync

--- Arrays/Simulation/Easy/1548-check-if-all-1s-are-at-least-length-k-places-away/check-if-all-1s-are-at-least-length-k-places-away.java
+++ Arrays/Simulation/Easy/1548-check-if-all-1s-are-at-least-length-k-places-away/check-if-all-1s-are-at-least-length-k-places-away.java
@@ -10,14 +10,10 @@ class Solution {
         int lastIdx = -1;
         for (int i = 0; i < n; i++) { // TC: O(N)
             if (nums[i] == 1) {
-                if (lastIdx == -1) {
-                    lastIdx = i;
-                } else {
-                    if (i - lastIdx - 1 < k) {
-                        return false;
-                    }
-                    lastIdx = i;
+                if (lastIdx != -1 && i - lastIdx - 1 < k) {
+                    return false;
                 }
+                lastIdx = i;
             }
         }
         return true;
