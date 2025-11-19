7506eed2c9a7ccf170cac5810c1a7f5dfa7b8330
Surajit Rana <surajit1602@gmail.com>
2025-11-19 10:34:07 +0530

    Time: 1 ms (93.95%) | Memory: 46 MB (17.14%) - LeetSync

--- Hashing/Easy/2274-keep-multiplying-found-values-by-two/keep-multiplying-found-values-by-two.java
+++ Hashing/Easy/2274-keep-multiplying-found-values-by-two/keep-multiplying-found-values-by-two.java
@@ -1,11 +1,28 @@
 class Solution {
     /**
+     * Approach II: Using Optimal (Hash Array) Approach
+     *
+     * TC: O(N)
+     * SC: O(1001) ~ O(1)
+     */
+    public int findFinalValue(int[] nums, int original) {
+        int[] freq = new int[1001]; // SC: O(1001) as from constraints, 1 <= nums[i], original <= 1000
+        for (int num : nums) { // TC: O(N)
+            freq[num]++;
+        }
+        while (original < 1001 && freq[original] != 0) { // TC: O(1000) ~ O(1)
+            original *= 2;
+        }
+        return original;
+    }
+
+    /**
      * Approach I : Using Brute-Force (Hashing) Approach
      *
      * TC: O(N) + O(N) ~ O(N)
      * SC: O(N)
      */
-    public int findFinalValue(int[] nums, int original) {
+    public int findFinalValueBruteForce(int[] nums, int original) {
         Map<Integer, Integer> freqMap = new HashMap<Integer, Integer>(); // SC: O(N)
         for (int num : nums) { // TC: O(N)
             freqMap.put(num, freqMap.getOrDefault(num, 0) + 1);
