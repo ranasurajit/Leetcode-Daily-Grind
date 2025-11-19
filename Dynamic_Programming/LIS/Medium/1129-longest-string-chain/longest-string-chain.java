151797866db0e10855249e08a46053af4afa5fa7
Surajit Rana <surajit1602@gmail.com>
2025-11-19 22:13:42 +0530

    Time: 117 ms (8.53%) | Memory: 56.6 MB (5.06%) - LeetSync

--- Dynamic_Programming/LIS/Medium/1129-longest-string-chain/longest-string-chain.java
+++ Dynamic_Programming/LIS/Medium/1129-longest-string-chain/longest-string-chain.java
@@ -1,16 +1,20 @@
 class Solution {
     /**
-     * Approach : Using Dynamic Programming Tabulation (LIS) Approach
+     * Approach II : Using Memoization Approach
      *
-     * TC: O(L x N x N) + O(N x log(N))
-     * SC: O(N)
+     * TC: O(L x N x N) + O(N x log(N)) ~ O(L x N x N)
+     * SC: O(N x N) + O(N)
+     * - O(N x N) - memoization memory
+     * - O(N) - recursion stack
+     *
+     * Accepted (86 / 86 testcases passed)
      */
     public int longestStrChain(String[] words) {
         int n = words.length;
         /**
-         * As we need to find predecessor so the order of the 
-         * words does not matter, rather we should sort it
-         * based upon its length
+         * Order does not matter here and we should sort the String[] words
+         * so that for any current word we can check if previous word is a
+         * predecessor or not
          */
         Arrays.sort(words, (a, b) -> {
             if (a.length() != b.length()) {
@@ -18,44 +22,111 @@ class Solution {
             }
             return a.compareTo(b);
         }); // TC: O(N x log(N))
-        int[] dp = new int[n]; // SC: O(N)
-        Arrays.fill(dp, 1);
-        int maxLength = 1;
-        for (int idx = 1; idx < n; idx++) { // TC: O(N)
-            for (int prevIdx = 0; prevIdx < idx; prevIdx++) { // TC: O(N)
-                if (isAllowed(words[idx], words[prevIdx]) && 
-                    dp[idx] < dp[prevIdx] + 1) { // TC: O(L)
-                    dp[idx] = dp[prevIdx] + 1;
-                }
-                maxLength = Math.max(maxLength, dp[idx]);
-            }
+        int[][] memo = new int[n][n + 1]; // SC: O(N x N)
+        for (int[] mem : memo) {
+            Arrays.fill(mem, -1);
         }
-        return maxLength;
+        return solveMemoization(0, -1, n, words, memo);
+    }
+
+    /**
+     * Using Recursion Approach
+     *
+     * TC: O(L x N x N)
+     * SC: O(N)
+     */
+    private int solveMemoization(int idx, int prevIdx, int n, String[] words, int[][] memo) {
+        // Base Case
+        if (idx >= n) {
+            return 0;
+        }
+        // Recursion Calls
+        // Memoization Check
+        if (memo[idx][prevIdx + 1] != -1) {
+            // coordinate shift as prevIdx starts from -1
+            return memo[idx][prevIdx + 1];
+        }
+        // skip
+        int skip = solveMemoization(idx + 1, prevIdx, n, words, memo);
+        int pick = 0;
+        if (prevIdx == -1 || isPredecessor(words[prevIdx], words[idx])) { // TC: O(L)
+            // pick
+            pick = 1 + solveMemoization(idx + 1, idx, n, words, memo);
+        }
+        return memo[idx][prevIdx + 1] = Math.max(skip, pick); // return max (longest chain)
+    }
+
+    /**
+     * Approach I : Using Recursion Approach
+     *
+     * TC: O(L x 2 ^ N) + O(N x log(N)) ~ O(L x 2 ^ N)
+     * SC: O(N)
+     * - O(N) - recursion stack
+     *
+     * Time Limit Exceeded (78 / 86 testcases passed)
+     */
+    public int longestStrChainRecursion(String[] words) {
+        int n = words.length;
+        /**
+         * Order does not matter here and we should sort the String[] words
+         * so that for any current word we can check if previous word is a
+         * predecessor or not
+         */
+        Arrays.sort(words, (a, b) -> {
+            if (a.length() != b.length()) {
+                return a.length() - b.length();
+            }
+            return a.compareTo(b);
+        }); // TC: O(N x log(N))
+        return solveRecursion(0, -1, n, words);
+    }
+
+    /**
+     * Using Recursion Approach
+     *
+     * TC: O(L x 2 ^ N)
+     * SC: O(N)
+     */
+    private int solveRecursion(int idx, int prevIdx, int n, String[] words) {
+        // Base Case
+        if (idx >= n) {
+            return 0;
+        }
+        // Recursion Calls
+        // skip
+        int skip = solveRecursion(idx + 1, prevIdx, n, words);
+        int pick = 0;
+        if (prevIdx == -1 || isPredecessor(words[prevIdx], words[idx])) { // TC: O(L)
+            // pick
+            pick = 1 + solveRecursion(idx + 1, idx, n, words);
+        }
+        return Math.max(skip, pick); // as we need to return longest String chain
     }
 
     /**
      * Using Two Pointers Approach
      *
-     * TC: O(Min(M, N)) ~ O(L), where L = average size(words[i])
+     * TC: O(Min(M, N)) ~ O(L), where L = average length of parameter
      * SC: O(1)
      */
-    private boolean isAllowed(String current, String prev) {
+    private boolean isPredecessor(String prev, String current) {
         int m = prev.length();
         int n = current.length();
         if (m + 1 != n) {
+            // predecessor should be 1 character less than current word
             return false;
         }
-        int p = 0; // pointer at the start of String 'prev'
-        int q = 0; // pointer at the start of String 'current'
-        int mismatches = 0;
+        int p = 0; // start pointer at String 'prev'
+        int q = 0; // start pointer at String 'current'
+        int skipped = 0;
         while (p < m && q < n) {
             if (prev.charAt(p) == current.charAt(q)) {
                 p++;
                 q++;
             } else {
-                mismatches++;
                 q++;
-                if (mismatches > 1) {
+                skipped++;
+                if (skipped > 1) {
                     return false;
                 }
             }
