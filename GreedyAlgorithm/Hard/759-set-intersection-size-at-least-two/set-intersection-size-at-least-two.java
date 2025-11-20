0e0647d70dcf425e1493a9801324116411997c3d
Surajit Rana <surajit1602@gmail.com>
2025-11-20 14:49:54 +0530

    Time: 139 ms (5.44%) | Memory: 47.4 MB (24.49%) - LeetSync

---
+++ GreedyAlgorithm/Hard/759-set-intersection-size-at-least-two/set-intersection-size-at-least-two.java
@@ -0,0 +1,57 @@
+class Solution {
+    /**
+     * Approach : Using Greedy + Sorting + Hashing Approach
+     *
+     * TC: O(N x log(N)) + O(N x K), where K = size/keys of HashSet
+     * SC: O(K)
+     */
+    public int intersectionSizeTwo(int[][] intervals) {
+        /**
+         * We should try to have maximum overlaps among
+         * intervals so we should sort it by end and if
+         * ends match between intervals so we can sort by
+         * decreasing order of start so that processing
+         * that first will help minimize the set
+         */
+        Arrays.sort(intervals, (a, b) -> {
+            if (a[1] == b[1]) {
+                // end matches so sort in decreasing order of start
+                return b[0] - a[0];
+            }
+            return a[1] - b[1];
+        }); // TC: O(N x log(N))
+        Set<Integer> hs = new HashSet<Integer>(); // SC: O(K)
+        for (int[] time : intervals) { // TC: O(N)
+            int start = time[0];
+            int end = time[1];
+            int hits = getContainingSet(hs, start, end); // TC: O(K), SC: O(1)
+            if (hits == 0) {
+                // we need to add both (end - 1) and end to HashSet to maximize overlap
+                hs.add(end - 1);
+                hs.add(end);
+            } else if (hits == 1) {
+                // we need to add only end to HashSet to maximize overlap
+                hs.add(end);
+            } else {
+                // it has both the intervals included so do nothing
+            }
+        }
+        return hs.size();
+    }
+
+    /**
+     * Using Hashing Approach
+     *
+     * TC: O(K), where K = size/keys of HashSet
+     * SC: O(1)
+     */
+    private int getContainingSet(Set<Integer> hs, int start, int end) {
+        int hits = 0;
+        for (Integer key : hs) { // TC: O(K)
+            if (key >= start && key <= end) {
+                hits++;
+            }
+        }
+        return hits;
+    }
+}
