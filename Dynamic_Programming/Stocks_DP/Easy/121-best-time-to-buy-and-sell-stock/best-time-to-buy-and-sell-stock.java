302eda5527d954743c9fc971a41ae827af90ba96
Surajit Rana <surajit1602@gmail.com>
2025-11-20 23:03:46 +0530

    Time: 2 ms (66.13%) | Memory: 94.6 MB (6.58%) - LeetSync

---
+++ Dynamic_Programming/Stocks_DP/Easy/121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.java
@@ -0,0 +1,25 @@
+class Solution {
+    /**
+     * Approach : Using Array Simulation Approach
+     *
+     * TC: O(N)
+     * SC: O(1)
+     */
+    public int maxProfit(int[] prices) {
+        int n = prices.length;
+        /**
+         * we will try to minimize the buy price and try
+         * to sell it on any next day and maximize sell
+         * price so that the profit can be maximized
+         */
+        int minBuy = prices[0];
+        int maximumProfit = 0;
+        for (int i = 1; i < n; i++) { // TC: O(N)
+            if (prices[i] > minBuy) {
+                maximumProfit = Math.max(maximumProfit, prices[i] - minBuy);
+            }
+            minBuy = Math.min(minBuy, prices[i]);
+        }
+        return maximumProfit;
+    }
+}
