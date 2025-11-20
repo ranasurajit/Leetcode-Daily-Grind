5d0633bce23f9843e294c137fd3ca3f1f5b6c88b
Surajit Rana <surajit1602@gmail.com>
2025-11-20 15:10:13 +0530

    Time: 11 ms (37.41%) | Memory: 47.5 MB (19.05%) - LeetSync

--- GreedyAlgorithm/Hard/759-set-intersection-size-at-least-two/set-intersection-size-at-least-two.java
+++ GreedyAlgorithm/Hard/759-set-intersection-size-at-least-two/set-intersection-size-at-least-two.java
@@ -1,11 +1,63 @@
 class Solution {
     /**
-     * Approach : Using Greedy + Sorting + Hashing Approach
+     * Approach II : Using Optimal (Greedy + Sorting) Approach
+     *
+     * TC: O(N x log(N)) + O(N) ~ O(N x log(N))
+     * SC: O(1)
+     */
+    public int intersectionSizeTwo(int[][] intervals) {
+        /**
+         * We should try to have maximum overlaps among
+         * intervals so we should sort it by end and if
+         * ends match between intervals so we can sort by
+         * decreasing order of start so that processing
+         * that first will help minimize the set
+         */
+        Arrays.sort(intervals, (a, b) -> {
+            if (a[1] == b[1]) {
+                // end matches so sort in decreasing order of start
+                return b[0] - a[0];
+            }
+            return a[1] - b[1];
+        }); // TC: O(N x log(N))
+        int last = -1;
+        int secondLast = -1;
+        int count = 0;
+        for (int[] time : intervals) { // TC: O(N)
+            int start = time[0];
+            int end = time[1];
+            int hits = 0;
+            if (last >= start && last <= end) {
+                hits++;
+            }
+            if (secondLast >= start && secondLast <= end) {
+                hits++;
+            }
+            if (hits == 2) {
+                continue;
+            } else if (hits == 1) {
+                // we need to add only end to HashSet to maximize overlap
+                secondLast = last;
+                last = end;
+                count++;
+            } else {
+                // hits = 0
+                // we need to add both (end - 1) and end to HashSet to maximize overlap
+                secondLast = end - 1;
+                last = end;
+                count += 2;
+            }
+        }
+        return count;
+    }
+
+    /**
+     * Approach I : Using Brute-Force (Greedy + Sorting + Hashing) Approach
      *
      * TC: O(N x log(N)) + O(N x K), where K = size/keys of HashSet
      * SC: O(K)
      */
-    public int intersectionSizeTwo(int[][] intervals) {
+    public int intersectionSizeTwoBruteForce(int[][] intervals) {
         /**
          * We should try to have maximum overlaps among
          * intervals so we should sort it by end and if
