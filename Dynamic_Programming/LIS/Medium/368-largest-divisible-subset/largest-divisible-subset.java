36d80aabbb49a5cfa0744998697c18a2453e3a43
Surajit Rana <surajit1602@gmail.com>
2025-11-23 01:38:41 +0530

    Time: 17 ms (32.48%) | Memory: 45.1 MB (25.65%) - LeetSync

--- Dynamic_Programming/LIS/Medium/368-largest-divisible-subset/largest-divisible-subset.java
+++ Dynamic_Programming/LIS/Medium/368-largest-divisible-subset/largest-divisible-subset.java
@@ -1,6 +1,6 @@
 class Solution {
     /**
-     * Approach II : Using Memoization Approach
+     * Approach III : Using Tabulation Approach
      *
      * TC: O(N x N x N) + O(N x log(N)) ~ O(N³)
      * SC: O(N²) + O(N)
@@ -10,6 +10,52 @@ class Solution {
      * Accepted (49 / 49 testcases passed)
      */
     public List<Integer> largestDivisibleSubset(int[] nums) {
+        int n = nums.length;
+        Arrays.sort(nums); // TC: O(N x log(N))
+        int[] dp = new int[n]; // SC: O(N)
+        int[] track = new int[n]; // SC: O(N)
+        int maxLength = 1;
+        int maxIndex = 0;
+        for (int i = 0; i < n; i++) { // TC: O(N)
+            dp[i] = 1;
+            track[i] = i;
+            for (int prev = 0; prev < i; prev++) { // TC: O(N)
+                if (nums[i] % nums[prev] == 0 && dp[i] < dp[prev] + 1) {
+                    dp[i] = dp[prev] + 1;
+                    track[i] = prev;
+                }
+            }
+            if (maxLength < dp[i]) {
+                maxLength = dp[i];
+                maxIndex = i;
+            }
+        }
+        int[] lis = new int[maxLength];
+        int index = maxLength - 1;
+        while (track[maxIndex] != maxIndex) {
+            lis[index] = nums[maxIndex];
+            maxIndex = track[maxIndex];
+            index--;
+        }
+        lis[index] = nums[maxIndex];
+        ArrayList<Integer> lisList = new ArrayList<Integer>();
+        for (int i = 0; i < maxLength; i++) {
+            lisList.add(lis[i]);
+        }
+        return lisList;
+    }
+
+    /**
+     * Approach II : Using Memoization Approach
+     *
+     * TC: O(N x N x N) + O(N x log(N)) ~ O(N³)
+     * SC: O(N²) + O(N)
+     * - O(N²) - memoization memory
+     * - O(N) - recursion stack
+     * 
+     * Accepted (49 / 49 testcases passed)
+     */
+    public List<Integer> largestDivisibleSubsetMemoization(int[] nums) {
         int n = nums.length;
         Arrays.sort(nums); // TC: O(N x log(N))
         ArrayList<Integer>[][] memo = new ArrayList[n + 1][n + 1]; // SC: O(N x N)
