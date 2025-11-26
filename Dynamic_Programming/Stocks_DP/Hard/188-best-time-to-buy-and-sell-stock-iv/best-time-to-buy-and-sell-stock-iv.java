971f4ba14f5c3432fb6b117438d2f9ba8b27c44f
Surajit Rana <surajit1602@gmail.com>
2025-11-27 00:57:16 +0530

    Time: 3 ms (58.51%) | Memory: 45.5 MB (21.64%) - LeetSync

--- Dynamic_Programming/Stocks_DP/Hard/188-best-time-to-buy-and-sell-stock-iv/best-time-to-buy-and-sell-stock-iv.java
+++ Dynamic_Programming/Stocks_DP/Hard/188-best-time-to-buy-and-sell-stock-iv/best-time-to-buy-and-sell-stock-iv.java
@@ -1,15 +1,54 @@
 class Solution {
     /**
+     * Approach III : Using Tabulation Approach
+     *
+     * TC: O(N x K)
+     * SC: O(N x K)
+     * - O(N x K) - dp memory
+     *
+     * Accepted (211 / 211 testcases passed)
+     */
+    public int maxProfit(int k, int[] prices) {
+        int n = prices.length;
+        // Initialization
+        int[][][] dp = new int[n + 1][2][k + 1]; // SC: O(N x K x 2) ~ O(N x K)
+        // Iterative Calls
+        for (int i = n - 1; i >= 0; i--) {
+            for (int j = 0; j < 2; j++) {
+                for (int p = 0; p <= k; p++) {
+                    if (p == 0) {
+                        dp[i][j][p] = 0;
+                        continue;
+                    }
+                    int pick = 0;
+                    int skip = 0;
+                    if (j == 1) {
+                        // buy (pick) at index 'idx' or skip
+                        pick = -1 * prices[i] + dp[i + 1][0][p];
+                        skip = dp[i + 1][1][p];
+                    } else {
+                        // sell (pick) at index 'idx' or skip
+                        pick = prices[i] + dp[i + 1][1][p - 1];
+                        skip = dp[i + 1][0][p];
+                    }
+                    dp[i][j][p] = Math.max(pick, skip);
+                }
+            }
+        }
+        return dp[0][1][k];
+    }
+
+    /**
      * Approach II : Using Memoization Approach
      *
-     * TC: O(2 ^ N)
+     * TC: O(N x K)
      * SC: O(N x K) + O(N)
      * - O(N x K) - memoization memory
      * - O(N) - recursion stack
      *
      * Accepted (211 / 211 testcases passed)
      */
-    public int maxProfit(int k, int[] prices) {
+    public int maxProfitMemoization(int k, int[] prices) {
         int n = prices.length;
         int[][][] memo = new int[n][2][k + 1]; // SC: O(N x K x 2) ~ O(N x K)
         for (int[][] mem : memo) {
