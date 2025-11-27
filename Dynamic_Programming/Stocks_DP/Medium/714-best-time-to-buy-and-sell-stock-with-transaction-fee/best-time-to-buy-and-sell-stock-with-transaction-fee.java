541a54696d543ad593fea3ad8937306129d3baf2
Surajit Rana <surajit1602@gmail.com>
2025-11-27 22:09:28 +0530

    Time: 32 ms (21.02%) | Memory: 77 MB (6.84%) - LeetSync

---
+++ Dynamic_Programming/Stocks_DP/Medium/714-best-time-to-buy-and-sell-stock-with-transaction-fee/best-time-to-buy-and-sell-stock-with-transaction-fee.java
@@ -0,0 +1,92 @@
+class Solution {
+    /**
+     * Approach II : Using Memoization Approach
+     *
+     * TC: O(N) + O(N) ~ O(N)
+     * SC: O(N) + O(N)
+     * - O(N) - recursion stack
+     * - O(N) - memoization memory
+     *
+     * Accepted (44 / 44 testcases passed)
+     */
+    public int maxProfit(int[] prices, int fee) {
+        int n = prices.length;
+        int[][] memo = new int[n][2];
+        for (int[] mem : memo) { // TC: O(N)
+            Arrays.fill(mem, -1);
+        }
+        return solveMemoization(0, 1, n, fee, prices, memo); // TC: O(N), SC: O(N)
+    }
+
+    /**
+     * Using Memoization Approach
+     *
+     * TC: O(N x 2) ~ O(N)
+     * SC: O(N)
+     */
+    private int solveMemoization(int idx, int canBuy, int n, int fee, int[] prices, int[][] memo) {
+        // Base Case
+        if (idx == n) {
+            // out of bounds
+            return 0;
+        }
+        // Memoization Check
+        if (memo[idx][canBuy] != -1) {
+            return memo[idx][canBuy];
+        }
+        // Recursion Calls
+        int pick = 0;
+        int skip = 0;
+        if (canBuy == 1) {
+            // we may skip buying the stock or pick (buy) it
+            skip = solveMemoization(idx + 1, 1, n, fee, prices, memo);
+            pick = -1 * prices[idx] + solveMemoization(idx + 1, 0, n, fee, prices, memo);
+        } else {
+            // we may skip selling the stock or pick (sell) it
+            skip = solveMemoization(idx + 1, 0, n, fee, prices, memo);
+            pick = prices[idx] - fee + solveMemoization(idx + 1, 1, n, fee, prices, memo);
+        }
+        return memo[idx][canBuy] = Math.max(pick, skip);
+    }
+
+    /**
+     * Approach I : Using Recursion Approach
+     *
+     * TC: O(2 ^ N)
+     * SC: O(N)
+     * - O(N) - recursion stack
+     *
+     * Time Limit Exceeded (19 / 44 testcases passed)
+     */
+    public int maxProfitRecursion(int[] prices, int fee) {
+        int n = prices.length;
+        return solveRecursion(0, 1, n, fee, prices); // TC: O(2 ^ N), SC: O(N)
+    }
+
+    /**
+     * Using Recursion Approach
+     *
+     * TC: O(2 ^ N)
+     * SC: O(N)
+     */
+    private int solveRecursion(int idx, int canBuy, int n, int fee, int[] prices) {
+        // Base Case
+        if (idx == n) {
+            // out of bounds
+            return 0;
+        }
+        // Recursion Calls
+        int pick = 0;
+        int skip = 0;
+        if (canBuy == 1) {
+            // we may skip buying the stock or pick (buy) it
+            skip = solveRecursion(idx + 1, 1, n, fee, prices);
+            pick = -1 * prices[idx] + solveRecursion(idx + 1, 0, n, fee, prices);
+        } else {
+            // we may skip selling the stock or pick (sell) it
+            skip = solveRecursion(idx + 1, 0, n, fee, prices);
+            pick = prices[idx] - fee + solveRecursion(idx + 1, 1, n, fee, prices);
+        }
+        return Math.max(pick, skip);
+    }
+}
