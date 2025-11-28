6896763bdaf5c96734ce5da074bf4fe11897fea4
Surajit Rana <surajit1602@gmail.com>
2025-11-28 23:59:41 +0530

    Time: 110 ms (10.61%) | Memory: 72.6 MB (18.72%) - LeetSync

---
+++ Dynamic_Programming/Subsequences_DP/Medium/416-partition-equal-subset-sum/partition-equal-subset-sum.java
@@ -0,0 +1,104 @@
+class Solution {
+    /**
+     * Approach II : Using Memoization Approach
+     *
+     * TC: O(N x T) + O(N) ~ O(N x T)
+     * SC: O(N x T) + O(N)
+     * - O(N x T) - memoization memory
+     * - O(N) - recursion stack
+     *
+     * Accepted (147 / 147 testcases passed)
+     */
+    public boolean canPartition(int[] nums) {
+        int n = nums.length;
+        int sum = 0;
+        for (int i = 0; i < n; i++) { // TC: O(N)
+            sum += nums[i];
+        }
+        if ((sum & 1) != 0) {
+            // odd sum cannot be divided into two equal sum subsets
+            return false;
+        }
+        sum = sum / 2;
+        int[][] memo = new int[n][sum + 1]; // SC: O(N x T)
+        for (int[] mem : memo) {
+            Arrays.fill(mem, -1);
+        }
+        // we can try to get one subset with sum (original sum / 2)
+        return solveMemoization(0, n, sum, nums, memo); // TC: O(N x T), SC: O(N)
+    }
+
+    /**
+     * Using Memoization Approach
+     *
+     * TC: O(N x T)
+     * SC: O(N)
+     */
+    private boolean solveMemoization(int idx, int n, int sum, int[] nums, int[][] memo) {
+        // Base Case
+        if (idx == n) {
+            return sum == 0;
+        }
+        // Memoization Check
+        if (memo[idx][sum] != -1) {
+            return memo[idx][sum] == 1;
+        }
+        // Recursion Calls
+        // we can skip or pick element at index 'idx'
+        // skip
+        boolean skip = solveMemoization(idx + 1, n, sum, nums, memo);
+        boolean pick = false;
+        if (nums[idx] <= sum) {
+            pick = solveMemoization(idx + 1, n, sum - nums[idx], nums, memo);
+        }
+        boolean result = pick || skip;
+        memo[idx][sum] = result ? 1 : 0;
+        return result;
+    }
+
+    /**
+     * Approach I : Using Recursion Approach
+     *
+     * TC: O(2 ^ N) + O(N) ~ O(2 ^ N)
+     * SC: O(N)
+     * - O(N) - recursion stack
+     *
+     * Time Limit Exceeded (39 / 147 testcases passed)
+     */
+    public boolean canPartitionRecursion(int[] nums) {
+        int n = nums.length;
+        int sum = 0;
+        for (int i = 0; i < n; i++) { // TC: O(N)
+            sum += nums[i];
+        }
+        if ((sum & 1) != 0) {
+            // odd sum cannot be divided into two equal sum subsets
+            return false;
+        }
+        sum = sum / 2;
+        // we can try to get one subset with sum (original sum / 2)
+        return solveRecursion(0, n, sum, nums); // TC: O(2 ^ N), SC: O(N)
+    }
+
+    /**
+     * Using Recursion Approach
+     *
+     * TC: O(2 ^ N)
+     * SC: O(N)
+     */
+    private boolean solveRecursion(int idx, int n, int sum, int[] nums) {
+        // Base Case
+        if (idx == n) {
+            return sum == 0;
+        }
+        // Recursion Calls
+        // we can skip or pick element at index 'idx'
+        // skip
+        boolean skip = solveRecursion(idx + 1, n, sum, nums);
+        boolean pick = false;
+        if (nums[idx] <= sum) {
+            pick = solveRecursion(idx + 1, n, sum - nums[idx], nums);
+        }
+        return pick || skip;
+    }
+}
