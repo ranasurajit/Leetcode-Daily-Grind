714a6d6c4a4c6dd1f46937f9e92240925d92d325
Surajit Rana <surajit1602@gmail.com>
2025-11-29 23:14:22 +0530

    Time: 37 ms (13.16%) | Memory: 44.4 MB (28.43%) - LeetSync

--- Dynamic_Programming/MCM/Hard/1669-minimum-cost-to-cut-a-stick/minimum-cost-to-cut-a-stick.java
+++ Dynamic_Programming/MCM/Hard/1669-minimum-cost-to-cut-a-stick/minimum-cost-to-cut-a-stick.java
@@ -1,79 +1,47 @@
 class Solution {
     /**
-     * Approach III : Using Tabulation Approach
+     * Approach II : Using Memoization Approach
      *
-     * TC: O(M x M x M) + O(M x log(M)) ~ O(M x M x M)
-     * SC: O(M x M)
+     * TC: TC: O(M x M x M) + O(M) + O(M x log(M)) ~ O(M³)
+     * SC: O(M x M) + O(M) + O(M)
+     * - O(M x M) - memoization memory
+     * - O(M) - arraylist cutList memory
+     * - O(M) - recursion stack
      *
      * Accepted (101 / 101 testcases passed)
      */
     public int minCost(int n, int[] cuts) {
-        int m = cuts.length;
         /**
-         * To reduce the depemdency of cuts and since order does 
-         * not matter so let's sort the cuts in ascending order
+         * As the cutting order does not matter we should sort it
+         * so that while dividing it one portion is not dependent
+         * on other as in MCM / Partition DP
          */
-        Arrays.sort(cuts); // TC: O(M x log(M))
-        List<Integer> cutIndices = new ArrayList<Integer>();
-        // we will insert 0 and n at the beginning and end
-        cutIndices.add(0);
+        int m = cuts.length;
+        List<Integer> cutList = new ArrayList<Integer>(); // SC: O(M)
         for (int i = 0; i < m; i++) { // TC: O(M)
-            cutIndices.add(cuts[i]);
+            cutList.add(cuts[i]);
         }
-        cutIndices.add(n);
-        int c = cutIndices.size() - 2;
-        int[][] dp = new int[c + 2][c + 2]; // SC: O(M x M)
-        for (int i = c; i >= 1; i--) { // TC: O(M)
-            for (int j = i; j <= c; j++) { // TC: O(M)
-                dp[i][j] = Integer.MAX_VALUE;
-                for (int k = i; k <= j; k++) { // TC: O(M)
-                    int currentCost = cutIndices.get(j + 1) - cutIndices.get(i - 1);
-                    dp[i][j] = Math.min(dp[i][j], currentCost + dp[i][k - 1] + dp[k + 1][j]);
-                }
-            }
-        }
-        return dp[1][c];
-    }
-
-    /**
-     * Approach II : Using Memoization Approach
-     *
-     * TC: O(M x M x M) + O(M x log(M)) ~ O(M x M x M)
-     * SC: O(M) + O(M x M)
-     *
-     * Accepted (101 / 101 testcases passed)
-     */
-    public int minCostMemoization(int n, int[] cuts) {
-        int m = cuts.length;
-        /**
-         * To reduce the depemdency of cuts and since order does 
-         * not matter so let's sort the cuts in ascending order
-         */
-        Arrays.sort(cuts); // TC: O(M x log(M))
-        List<Integer> cutIndices = new ArrayList<Integer>();
-        // we will insert 0 and n at the beginning and end
-        cutIndices.add(0);
-        for (int i = 0; i < m; i++) {
-            cutIndices.add(cuts[i]);
-        }
-        cutIndices.add(n);
-        int[][] memo = new int[m + 1][m + 1]; // SC: O(M x M)
+        cutList.add(0);
+        cutList.add(n);
+        // Sorting the cutList
+        Collections.sort(cutList); // TC: O(M x log(M))
+        int[][] memo = new int[m + 2][m + 2]; // SC: O(M x M)
         for (int[] mem : memo) {
             Arrays.fill(mem, -1);
         }
-        return solveMemoization(1, cutIndices.size() - 2, cutIndices, memo);
+        return solveMemoization(1, m, cutList, memo);
     }
 
     /**
      * Using Memoization Approach
      *
-     * TC: O(M x M x M)
+     * TC: O(M x M x M) ~ O(M³)
      * SC: O(M)
      */
-    private int solveMemoization(int i, int j, List<Integer> cutIndices, int[][] memo) {
+    private int solveMemoization(int i, int j, List<Integer> cutList, int[][] memo) {
         // Base Case
         if (i > j) {
-            // length of stick is invalid
+            // invalid length of stick
             return 0;
         }
         // Memoization Check
@@ -82,11 +50,13 @@ class Solution {
         }
         // Recursion Calls
         int minCost = Integer.MAX_VALUE;
-        for (int k = i; k <= j; k++) {
-            int currentCost = cutIndices.get(j + 1) - cutIndices.get(i - 1);
-            minCost = Math.min(minCost, currentCost + 
-                solveMemoization(i, k - 1, cutIndices, memo) + 
-                solveMemoization(k + 1, j, cutIndices, memo)
+        for (int k = i; k <= j; k++) { // TC: O(M)
+            // current cost is length of stick portion before cutting at index k
+            int currentCost = cutList.get(j + 1) - cutList.get(i - 1);
+            minCost = Math.min(minCost, 
+                solveMemoization(i, k - 1, cutList, memo) + 
+                currentCost + 
+                solveMemoization(k + 1, j, cutList, memo)
             );
         }
         return memo[i][j] = minCost;
@@ -95,47 +65,52 @@ class Solution {
     /**
      * Approach I : Using Recursion Approach
      *
-     * TC: O(Exponential)
-     * SC: O(M)
+     * TC: Exponential
+     * SC: O(M) + O(M)
+     * - O(M) - arraylist cutList memory
+     * - O(M) - recursion stack
      *
      * Time Limit Exceeded (12 / 101 testcases passed)
      */
     public int minCostRecursion(int n, int[] cuts) {
-        int m = cuts.length;
         /**
-         * To reduce the depemdency of cuts and since order does 
-         * not matter so let's sort the cuts in ascending order
+         * As the cutting order does not matter we should sort it
+         * so that while dividing it one portion is not dependent
+         * on other as in MCM / Partition DP
          */
-        Arrays.sort(cuts); // TC: O(M x log(M))
-        List<Integer> cutIndices = new ArrayList<Integer>();
-        // we will insert 0 and n at the beginning and end
-        cutIndices.add(0);
-        for (int i = 0; i < m; i++) {
-            cutIndices.add(cuts[i]);
+        int m = cuts.length;
+        List<Integer> cutList = new ArrayList<Integer>(); // SC: O(M)
+        for (int i = 0; i < m; i++) { // TC: O(M)
+            cutList.add(cuts[i]);
         }
-        cutIndices.add(n);
-        return solveRecursion(1, cutIndices.size() - 2, cutIndices);
+        cutList.add(0);
+        cutList.add(n);
+        // Sorting the cutList
+        Collections.sort(cutList); // TC: O(M x log(M))
+        return solveRecursion(1, m, cutList);
     }
 
     /**
      * Using Recursion Approach
      *
-     * TC: O(Exponential)
+     * TC: Exponential
      * SC: O(M)
      */
-    private int solveRecursion(int i, int j, List<Integer> cutIndices) {
+    private int solveRecursion(int i, int j, List<Integer> cutList) {
         // Base Case
         if (i > j) {
-            // length of stick is invalid
+            // invalid length of stick
             return 0;
         }
         // Recursion Calls
         int minCost = Integer.MAX_VALUE;
         for (int k = i; k <= j; k++) {
-            int currentCost = cutIndices.get(j + 1) - cutIndices.get(i - 1);
-            minCost = Math.min(minCost, currentCost + 
-                solveRecursion(i, k - 1, cutIndices) + 
-                solveRecursion(k + 1, j, cutIndices)
+            // current cost is length of stick portion before cutting at index k
+            int currentCost = cutList.get(j + 1) - cutList.get(i - 1);
+            minCost = Math.min(minCost, 
+                solveRecursion(i, k - 1, cutList) + 
+                currentCost + 
+                solveRecursion(k + 1, j, cutList)
             );
         }
         return minCost;
