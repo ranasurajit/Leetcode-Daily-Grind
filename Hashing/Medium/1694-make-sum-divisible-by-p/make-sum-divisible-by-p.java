38bb4613457becf0403d72083dad378d531010e3
Surajit Rana <surajit1602@gmail.com>
2025-11-30 11:56:39 +0530

    Time: 28 ms (57.58%) | Memory: 101.8 MB (6.15%) - LeetSync

---
+++ Hashing/Medium/1694-make-sum-divisible-by-p/make-sum-divisible-by-p.java
@@ -0,0 +1,36 @@
+class Solution {
+    /**
+     * Approach : Using Array Simulation + Hashing Approach
+     *
+     * TC: O(N) + O(N) ~ O(N)
+     * SC: O(N)
+     */
+    public int minSubarray(int[] nums, int p) {
+        int n = nums.length;
+        long sum = 0L;
+        for (int num : nums) { // TC: O(N)
+            sum += (long) num;
+        }
+        long target = sum % p;
+        if (target == 0) {
+            return 0;
+        }
+        /**
+         * now we need to find the minimum sub-array length 
+         * whose (prefix sum - target) % p is observed last
+         */
+        long prefixSum = 0L;
+        int minLength = n;
+        Map<Long, Integer> map = new HashMap<Long, Integer>(); // SC: O(N)
+        map.put(0L, -1);
+        for (int i = 0; i < n; i++) { // TC: O(N)
+            prefixSum = (prefixSum + (long) nums[i]) % p;
+            long rem = (prefixSum - target + p) % p;
+            if (map.containsKey(rem)) {
+                minLength = Math.min(minLength, i - map.get(rem));
+            }
+            map.put(prefixSum, i);
+        }
+        return minLength == n ? -1 : minLength;
+    }
+}
