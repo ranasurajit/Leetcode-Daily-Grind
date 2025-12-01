a8ff75a576115476af934ffbf4075fd373581440
Surajit Rana <surajit1602@gmail.com>
2025-12-01 11:53:10 +0530

    Time: 7 ms (97.78%) | Memory: 65.2 MB (23.76%) - LeetSync

--- Backtracking/Medium/131-palindrome-partitioning/palindrome-partitioning.java
+++ Backtracking/Medium/131-palindrome-partitioning/palindrome-partitioning.java
@@ -3,34 +3,36 @@ class Solution {
      * Approach : Using Backtracking Approach
      *
      * TC: O(N x 2 ^ N)
-     * SC: O(N) + O(N) ~ O(N)
+     * SC: O(N)
      */
     public List<List<String>> partition(String s) {
         int n = s.length();
         List<List<String>> result = new ArrayList<List<String>>();
-        backtrack(0, n, s, new ArrayList<String>(), result);
+        List<String> current = new ArrayList<String>();
+        backtrack(0, n, s, current, result);
         return result;
     }
 
     /**
      * Using Backtracking Approach
      *
-     * TC: O(N ^ 2 x 2 ^ N) ~ O(N x 2 ^ N) due to pruning
+     * TC: O(N ^ 2 x  2 ^ N) ~ O(N x 2 ^ N) due to pruning
      * SC: O(N)
      */
-    private void backtrack(int idx, int n, String s, List<String> current, List<List<String>> result) {
+    private void backtrack(int idx, int n, String s, List<String> current,
+        List<List<String>> result) {
         // Base Case
         if (idx == n) {
+            // no more partitions needed
             result.add(new ArrayList<String>(current));
             return;
         }
         // Recursion Calls
-        for (int i = idx; i < n; i++) { // TC: O(N)
-            String temp = s.substring(idx, i + 1);
-            int m = temp.length();
-            if (m == 1 || isPalindrome(temp, m)) { // TC: O(N) - pruning here
+        for (int j = idx; j < n; j++) { // TC: O(N)
+            String temp = s.substring(idx, j + 1);
+            if (isPalindrome(temp)) {   // TC: O(N)
                 current.add(temp); // modify
-                backtrack(i + 1, n, s, current, result); // explore
+                backtrack(j + 1, n, s, current, result); // backtrack
                 current.remove(current.size() - 1); // backtrack
             }
         }
@@ -42,15 +44,15 @@ class Solution {
      * TC: O(N / 2) ~ O(N)
      * SC: O(1)
      */
-    private boolean isPalindrome(String temp, int size) {
-        int low = 0;
-        int high = size - 1;
-        while (low < high) {
-            if (temp.charAt(low) != temp.charAt(high)) {
+    private boolean isPalindrome(String s) {
+        int p = 0;
+        int q = s.length() - 1;
+        while (p < q) {
+            if (s.charAt(p) != s.charAt(q)) {
                 return false;
             }
-            low++;
-            high--;
+            p++;
+            q--;
         }
         return true;
     }
