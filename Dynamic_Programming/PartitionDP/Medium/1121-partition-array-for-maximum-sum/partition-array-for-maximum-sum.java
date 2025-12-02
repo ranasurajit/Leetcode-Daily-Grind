b848a49a6736dbaadff5f5a67145d3cc8bedd9c4
Surajit Rana <surajit1602@gmail.com>
2025-12-02 08:34:38 +0530

    Time: 7 ms (50.62%) | Memory: 44 MB (10.77%) - LeetSync

--- Dynamic_Programming/PartitionDP/Medium/1121-partition-array-for-maximum-sum/partition-array-for-maximum-sum.java
+++ Dynamic_Programming/PartitionDP/Medium/1121-partition-array-for-maximum-sum/partition-array-for-maximum-sum.java
@@ -1,15 +1,43 @@
 class Solution {
     /**
+     * Approach III : Using Tabulation (Forward Partition DP) Approach
+     *
+     * TC: O(K x N)
+     * SC: O(N)
+     * - O(N) - dp array memory
+     *
+     * Accepted (52 / 52 testcases passed)
+     */
+    public int maxSumAfterPartitioning(int[] arr, int k) {
+        int n = arr.length;
+        // Initialization
+        int[] dp = new int[n + 1]; // SC: O(N)
+        dp[n] = 0;
+        for (int idx = n - 1; idx >= 0; idx--) { // TC: O(N)
+            int maximum = Integer.MIN_VALUE;
+            int totalSum = 0;
+            int maxSum = 0;
+            for (int j = idx; j < Math.min(idx + k, n); j++) { // TC: O(K)
+                maximum = Math.max(maximum, arr[j]);
+                totalSum = maximum * (j - idx + 1) + dp[j + 1];
+                maxSum = Math.max(maxSum, totalSum);
+            }
+            dp[idx] = maxSum;
+        }
+        return dp[0];
+    }
+
+    /**
      * Approach II : Using Memoization (Forward Partition DP) Approach
      *
-     * TC: O(K ^ N)
-     * SC: O(N)
+     * TC: O(N x K)
+     * SC: O(N) + O(N)
      * - O(N) - memoization memory
      * - O(N) - recursion stack
      *
      * Accepted (52 / 52 testcases passed)
      */
-    public int maxSumAfterPartitioning(int[] arr, int k) {
+    public int maxSumAfterPartitioningMemoization(int[] arr, int k) {
         int n = arr.length;
         int[] memo = new int[n]; // SC: O(N)
         Arrays.fill(memo, -1);
@@ -19,7 +47,7 @@ class Solution {
     /**
      * Using Memoization (Forward Partition DP) Approach
      *
-     * TC: O(K ^ N)
+     * TC: O(N x K)
      * SC: O(N)
      */
     private int solveMemoization(int idx, int n, int[] arr, int k, int[] memo) {
@@ -36,7 +64,7 @@ class Solution {
         int maximum = Integer.MIN_VALUE;
         int totalSum = 0;
         int maxSum = 0;
-        for (int j = idx; j < Math.min(idx + k, n); j++) {
+        for (int j = idx; j < Math.min(idx + k, n); j++) { // TC: O(K)
             maximum = Math.max(maximum, arr[j]);
             totalSum = maximum * (j - idx + 1) + solveMemoization(j + 1, n, arr, k, memo);
             maxSum = Math.max(maxSum, totalSum);
