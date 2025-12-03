51cfa852c12785bb786d55caa4fb80405338531e
Surajit Rana <surajit1602@gmail.com>
2025-12-03 10:50:59 +0530

    Time: 79 ms (55.04%) | Memory: 44.7 MB (7.03%) - LeetSync

--- Dynamic_Programming/PartitionDP/Hard/312-burst-balloons/burst-balloons.java
+++ Dynamic_Programming/PartitionDP/Hard/312-burst-balloons/burst-balloons.java
@@ -1,9 +1,9 @@
 class Solution {
     /**
-     * Approach II : Using Memoization Approach
+     * Approach III : Using Tabulation Approach
      *
      * TC: O(N x N x N)
-     * SC: O(N x N) + O(N)
+     * SC: O(N x N)
      *
      * Accepted (73 / 73 testcases passed)
      */
@@ -11,7 +11,42 @@ class Solution {
         int n = nums.length;
         List<Integer> numsList = new ArrayList<Integer>();
         numsList.add(1);
-        for (int num : nums) {
+        for (int num : nums) { // TC: O(N)
+            numsList.add(num);
+        }
+        numsList.add(1);
+        int[][] dp = new int[n + 2][n + 2]; // SC: O(N x N)
+        for (int i = n; i >= 1; i--) { // TC: O(N)
+            for (int j = 1; j <= n; j++) { // TC: O(N)
+                if (i > j) {
+                    continue;
+                }
+                int maximumVal = Integer.MIN_VALUE;
+                for (int k = i; k <= j; k++) { // TC: O(N)
+                    // kth index can be considered to be the last balloon to be bursted
+                    int currentCost = numsList.get(k) * numsList.get(i - 1) * numsList.get(j + 1);
+                    maximumVal = Math.max(maximumVal, 
+                        currentCost + dp[i][k - 1] + dp[k + 1][j]);
+                }
+                dp[i][j] = Math.max(dp[i][j], maximumVal);
+            }
+        }
+        return dp[1][n];
+    }
+
+    /**
+     * Approach II : Using Memoization Approach
+     *
+     * TC: O(N x N x N)
+     * SC: O(N x N) + O(N)
+     *
+     * Accepted (73 / 73 testcases passed)
+     */
+    public int maxCoinsMemoization(int[] nums) {
+        int n = nums.length;
+        List<Integer> numsList = new ArrayList<Integer>();
+        numsList.add(1);
+        for (int num : nums) { // TC: O(N)
             numsList.add(num);
         }
         numsList.add(1);
@@ -63,7 +98,7 @@ class Solution {
         int n = nums.length;
         List<Integer> numsList = new ArrayList<Integer>();
         numsList.add(1);
-        for (int num : nums) {
+        for (int num : nums) { // TC: O(N)
             numsList.add(num);
         }
         numsList.add(1);
