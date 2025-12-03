e0a696d414e751f4b3412a9d99932db48d206f08
Surajit Rana <surajit1602@gmail.com>
2025-12-03 10:41:58 +0530

    Time: 231 ms (5.11%) | Memory: 44.3 MB (41.89%) - LeetSync

---
+++ Dynamic_Programming/PartitionDP/Hard/312-burst-balloons/burst-balloons.java
@@ -0,0 +1,97 @@
+class Solution {
+    /**
+     * Approach II : Using Memoization Approach
+     *
+     * TC: O(N x N x N)
+     * SC: O(N x N) + O(N)
+     *
+     * Accepted (73 / 73 testcases passed)
+     */
+    public int maxCoins(int[] nums) {
+        int n = nums.length;
+        List<Integer> numsList = new ArrayList<Integer>();
+        numsList.add(1);
+        for (int num : nums) {
+            numsList.add(num);
+        }
+        numsList.add(1);
+        int[][] memo = new int[n + 2][n + 2]; // SC: O(N x N)
+        for (int[] mem : memo) {
+            Arrays.fill(mem, -1);
+        }
+        return solveMemoization(1, n, numsList, memo);
+    }
+
+    /**
+     * Using Memoization Approach
+     *
+     * TC: O(N x N x N)
+     * SC: O(N)
+     */
+    private int solveMemoization(int i, int j, List<Integer> numsList, int[][] memo) {
+        // Base Case
+        if (i > j) {
+            // if i == j we still have a balloon to burst which will contribute to coins
+            return 0;
+        }
+        // Memoization Check
+        if (memo[i][j] != -1) {
+            return memo[i][j];
+        }
+        // Recursion Calls
+        int maximumVal = Integer.MIN_VALUE;
+        for (int k = i; k <= j; k++) { // TC: O(N)
+            // kth index can be considered to be the last balloon to be bursted
+            int currentCost = numsList.get(k) * numsList.get(i - 1) * numsList.get(j + 1);
+            maximumVal = Math.max(maximumVal, 
+                currentCost + solveMemoization(i, k - 1, numsList, memo) + 
+                solveMemoization(k + 1, j, numsList, memo)
+            );
+        }
+        return memo[i][j] = maximumVal;
+    }
+
+    /**
+     * Approach I : Using Recursion Approach
+     *
+     * TC: Exponential
+     * SC: O(N)
+     *
+     * Time Limit Exceeded (30 / 73 testcases passed)
+     */
+    public int maxCoinsRecursion(int[] nums) {
+        int n = nums.length;
+        List<Integer> numsList = new ArrayList<Integer>();
+        numsList.add(1);
+        for (int num : nums) {
+            numsList.add(num);
+        }
+        numsList.add(1);
+        return solveRecursion(1, n, numsList);
+    }
+
+    /**
+     * Using Recursion Approach
+     *
+     * TC: Exponential
+     * SC: O(N)
+     */
+    private int solveRecursion(int i, int j, List<Integer> numsList) {
+        // Base Case
+        if (i > j) {
+            // if i == j we still have a balloon to burst which will contribute to coins
+            return 0;
+        }
+        // Recursion Calls
+        int maximumVal = Integer.MIN_VALUE;
+        for (int k = i; k <= j; k++) { // TC: O(N)
+            // kth index can be considered to be the last balloon to be bursted
+            int currentCost = numsList.get(k) * numsList.get(i - 1) * numsList.get(j + 1);
+            maximumVal = Math.max(maximumVal, 
+                currentCost + solveRecursion(i, k - 1, numsList) + 
+                solveRecursion(k + 1, j, numsList)
+            );
+        }
+        return maximumVal;
+    }
+}
