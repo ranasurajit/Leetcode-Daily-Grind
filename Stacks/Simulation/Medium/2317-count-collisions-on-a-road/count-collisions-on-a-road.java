9258fc82928ac64c8a7ad911630659b0caef0442
Surajit Rana <surajit1602@gmail.com>
2025-12-04 21:36:38 +0530

    Time: 97 ms (20.16%) | Memory: 48.2 MB (7.26%) - LeetSync

--- Stacks/Simulation/Medium/2317-count-collisions-on-a-road/count-collisions-on-a-road.java
+++ Stacks/Simulation/Medium/2317-count-collisions-on-a-road/count-collisions-on-a-road.java
@@ -1,26 +1,66 @@
 class Solution {
     /**
-     * Approach : Using Stack + String Simulation Approach
+     * Approach I : Using Stack + String Simulation Approach
      *
      * TC: O(N)
      * SC: O(1)
      */
     public int countCollisions(String directions) {
         int n = directions.length();
-        int i = 0;
-        int j = n - 1;
-        while (i < n && directions.charAt(i) == 'L') {
-            i++;
-        }
-        while (j >= 0 && directions.charAt(j) == 'R') {
-            j--;
-        }
+        Stack<Character> st = new Stack<Character>(); // SC: O(N)
         int collisions = 0;
-        for (int k = i; k <= j; k++) { // TC: O(N)
-            if (directions.charAt(k) == 'L' || directions.charAt(k) == 'R') {
-                collisions++;
+        for (char ch : directions.toCharArray()) { // TC: O(N)
+            if (ch == 'R') {
+                st.push('R');
+            } else if (ch == 'S') {
+                while (!st.isEmpty() && st.peek() == 'R') {
+                    collisions += 1;
+                    st.pop();
+                }
+                st.push('S');
+            } else {
+                if (!st.isEmpty() && st.peek() == 'R') {
+                    collisions += 2;
+                    st.pop();
+                    ch = 'S';
+                    while (!st.isEmpty() && st.peek() == 'R') {
+                        collisions += 1;
+                        st.pop();
+                    }
+                    st.push('S');
+                } else if (!st.isEmpty() && st.peek() == 'S') {
+                    collisions += 1;
+                    st.push('S');
+                } else {
+                    st.push('L');
+                }
             }
         }
         return collisions;
     }
+
+    /**
+     * Approach : Using Stack + String Simulation Approach
+     *
+     * TC: O(N)
+     * SC: O(1)
+     */
+    // public int countCollisions(String directions) {
+    //     int n = directions.length();
+    //     int i = 0;
+    //     int j = n - 1;
+    //     while (i < n && directions.charAt(i) == 'L') {
+    //         i++;
+    //     }
+    //     while (j >= 0 && directions.charAt(j) == 'R') {
+    //         j--;
+    //     }
+    //     int collisions = 0;
+    //     for (int k = i; k <= j; k++) { // TC: O(N)
+    //         if (directions.charAt(k) == 'L' || directions.charAt(k) == 'R') {
+    //             collisions++;
+    //         }
+    //     }
+    //     return collisions;
+    // }
 }
