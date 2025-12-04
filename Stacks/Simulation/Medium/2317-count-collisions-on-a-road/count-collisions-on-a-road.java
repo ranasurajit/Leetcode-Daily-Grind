6316aee58dbffc3aa9724f1f7afb92752cd5ce77
Surajit Rana <surajit1602@gmail.com>
2025-12-04 21:38:20 +0530

    Time: 17 ms (47.18%) | Memory: 47.9 MB (7.26%) - LeetSync

--- Stacks/Simulation/Medium/2317-count-collisions-on-a-road/count-collisions-on-a-road.java
+++ Stacks/Simulation/Medium/2317-count-collisions-on-a-road/count-collisions-on-a-road.java
@@ -1,11 +1,36 @@
 class Solution {
     /**
-     * Approach I : Using Stack + String Simulation Approach
+     * Approach II : Using Two Pointers + String Simulation Approach
      *
      * TC: O(N)
      * SC: O(1)
      */
     public int countCollisions(String directions) {
+        int n = directions.length();
+        int i = 0;
+        int j = n - 1;
+        while (i < n && directions.charAt(i) == 'L') {
+            i++;
+        }
+        while (j >= 0 && directions.charAt(j) == 'R') {
+            j--;
+        }
+        int collisions = 0;
+        for (int k = i; k <= j; k++) { // TC: O(N)
+            if (directions.charAt(k) == 'L' || directions.charAt(k) == 'R') {
+                collisions++;
+            }
+        }
+        return collisions;
+    }
+
+    /**
+     * Approach I : Using Stack + String Simulation Approach
+     *
+     * TC: O(N)
+     * SC: O(N)
+     */
+    public int countCollisionsUsingStack(String directions) {
         int n = directions.length();
         Stack<Character> st = new Stack<Character>(); // SC: O(N)
         int collisions = 0;
@@ -38,29 +63,4 @@ class Solution {
         }
         return collisions;
     }
-
-    /**
-     * Approach : Using Stack + String Simulation Approach
-     *
-     * TC: O(N)
-     * SC: O(1)
-     */
-    // public int countCollisions(String directions) {
-    //     int n = directions.length();
-    //     int i = 0;
-    //     int j = n - 1;
-    //     while (i < n && directions.charAt(i) == 'L') {
-    //         i++;
-    //     }
-    //     while (j >= 0 && directions.charAt(j) == 'R') {
-    //         j--;
-    //     }
-    //     int collisions = 0;
-    //     for (int k = i; k <= j; k++) { // TC: O(N)
-    //         if (directions.charAt(k) == 'L' || directions.charAt(k) == 'R') {
-    //             collisions++;
-    //         }
-    //     }
-    //     return collisions;
-    // }
 }
