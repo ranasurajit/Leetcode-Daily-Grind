f3a5d722cdd8298a88d183dd2738b9e2ea64ac3a
Surajit Rana <surajit1602@gmail.com>
2025-12-06 00:40:44 +0530

    Time: 17 ms (33.33%) | Memory: 48.1 MB (42.31%) - LeetSync

--- Stacks/MonotonicStacks/Medium/503-next-greater-element-ii/next-greater-element-ii.java
+++ Stacks/MonotonicStacks/Medium/503-next-greater-element-ii/next-greater-element-ii.java
@@ -1,11 +1,31 @@
 class Solution {
     /**
-     * Approach : Using Monotonic Stack (Monotonic Decreasing) Approach
+     * Approach II : Using Monotonic Stack (Monotonic Decreasing) (Space Optimized) Approach
      *
      * TC: O(N) + O(2 x N) + O(N) ~ O(N)
      * SC: O(2 x N) + O(2 x N) + O(2 x N) ~ O(N)
      */
     public int[] nextGreaterElements(int[] nums) {
+        int n = nums.length;
+        Stack<Integer> st = new Stack<Integer>(); // SC: O(N)
+        int[] nge = new int[n]; // SC: O(2 x N)
+        for (int i = 2 * n - 1; i >= 0; i--) { // TC: O(2 x N)
+            while (!st.isEmpty() && st.peek() <= nums[i % n]) {
+                st.pop();
+            }
+            nge[i % n] = st.isEmpty() ? -1 : st.peek();
+            st.push(nums[i % n]);
+        }
+        return nge;
+    }
+
+    /**
+     * Approach I : Using Monotonic Stack (Monotonic Decreasing) Approach
+     *
+     * TC: O(N) + O(2 x N) + O(N) ~ O(N)
+     * SC: O(2 x N) + O(2 x N) + O(2 x N) ~ O(N)
+     */
+    public int[] nextGreaterElementsMonotonicStack(int[] nums) {
         int n = nums.length;
         int[] cirNums = new int[2 * n]; // SC: O(2 x N)
         for (int i = 0; i < n; i++) {   // TC: O(N)
