0fecaf4bd4fe194f9336a0f5c51986f04d099828
Surajit Rana <surajit1602@gmail.com>
2025-12-06 00:13:11 +0530

    Time: 4 ms (54.81%) | Memory: 45.2 MB (25.07%) - LeetSync

--- Stacks/MonotonicStacks/Easy/496-next-greater-element-i/next-greater-element-i.java
+++ Stacks/MonotonicStacks/Easy/496-next-greater-element-i/next-greater-element-i.java
@@ -11,18 +11,16 @@ class Solution {
         Map<Integer, Integer> map = new HashMap<Integer, Integer>(); // SC: O(N)
         int[] nge = new int[n];       // SC: O(N)
         Stack<Integer> st = new Stack<Integer>(); // SC: O(N)
-        int i = n - 1;
-        while (i >= 0) { // TC: O(N)
+        for (int i = n - 1; i >= 0; i--) { // TC: O(N)
             while (!st.isEmpty() && st.peek() <= nums2[i]) {
                 st.pop();
             }
             nge[i] = st.isEmpty() ? -1 : st.peek();
             st.push(nums2[i]);
             map.put(nums2[i], i);
-            i--;
         }
         int[] result = new int[m];
-        for (i = 0; i < m; i++) { // TC: O(M)
+        for (int i = 0; i < m; i++) { // TC: O(M)
             result[i] = nge[map.get(nums1[i])];
         }
         return result;
