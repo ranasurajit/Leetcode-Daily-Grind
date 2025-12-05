f324da311575eb7ee90e7136578b207f5c18fdb5
Surajit Rana <surajit1602@gmail.com>
2025-12-05 10:09:33 +0530

    Time: 0 ms (100.00%) | Memory: 43.5 MB (31.42%) - LeetSync

--- Arrays/PrefixSuffix/Easy/3704-count-partitions-with-even-sum-difference/count-partitions-with-even-sum-difference.java
+++ Arrays/PrefixSuffix/Easy/3704-count-partitions-with-even-sum-difference/count-partitions-with-even-sum-difference.java
@@ -1,11 +1,36 @@
 class Solution {
     /**
-     * Approach : Using Array Pre-Processing (Prefix-Suffix) Approach
+     * Approach II : Using Simulation (No Extra Space) Approach
+     *
+     * TC: O(N) + O(N) ~ O(N)
+     * SC: O(1)
+     */
+    public int countPartitions(int[] nums) {
+        int n = nums.length;
+        int sum = 0;
+        for (int num : nums) { // TC: O(N)
+            sum += num;
+        }
+        int partitions = 0;
+        int leftSum = 0;
+        int rightSum = sum;
+        for (int i = 0; i < n - 1; i++) { // TC: O(N)
+            leftSum += nums[i];
+            rightSum -= nums[i];
+            if (((rightSum - leftSum) & 1) == 0) {
+                partitions++;
+            }
+        }
+        return partitions;
+    }
+
+    /**
+     * Approach I : Using Array Pre-Processing (Prefix-Suffix) Approach
      *
      * TC: O(N) + O(N) ~ O(N)
      * SC: O(N)
      */
-    public int countPartitions(int[] nums) {
+    public int countPartitionsUsingSpace(int[] nums) {
         int n = nums.length;
         int[] suffixSum = new int[n]; // SC: O(N)
         for (int i = n - 1; i >= 0; i--) { // TC: O(N)
