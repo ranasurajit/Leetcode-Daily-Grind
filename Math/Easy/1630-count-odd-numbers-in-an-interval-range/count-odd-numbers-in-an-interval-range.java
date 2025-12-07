cd4364cc7c713cd067f43172320c14cb5be61b5a
Surajit Rana <surajit1602@gmail.com>
2025-12-07 15:00:05 +0530

    Time: 0 ms (100.00%) | Memory: 41.9 MB (29.32%) - LeetSync

--- Math/Easy/1630-count-odd-numbers-in-an-interval-range/count-odd-numbers-in-an-interval-range.java
+++ Math/Easy/1630-count-odd-numbers-in-an-interval-range/count-odd-numbers-in-an-interval-range.java
@@ -1,11 +1,27 @@
 class Solution {
     /**
-     * Approach I: Using Simulation Approach
+     * Approach II : Using Math Approach
+     *
+     * TC: O(1)
+     * SC: O(1)
+     */
+    public int countOdds(int low, int high) {
+        boolean isStartOdd = (low & 1) == 1;
+        boolean isEndOdd = (high & 1) == 1;
+        int range = high - low + 1;
+        if (isStartOdd && isEndOdd) {
+            return (range / 2) + 1;
+        }
+        return range / 2;
+    }
+
+    /**
+     * Approach I : Using Simulation Approach
      *
      * TC: O(R), where R is the range = (high - low + 1)
      * SC: O(1)
      */
-    public int countOdds(int low, int high) {
+    public int countOddsBruteForce(int low, int high) {
         int count = 0;
         for (int i = low; i <= high; i++) {
             if ((i & 1) != 0) {
