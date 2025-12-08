59dba30d14e928b2ec21ec129aacba33aed4ce2c
Surajit Rana <surajit1602@gmail.com>
2025-12-08 09:57:38 +0530

    Time: 13 ms (56.16%) | Memory: 42 MB (59.50%) - LeetSync

--- Math/Easy/2037-count-square-sum-triples/count-square-sum-triples.java
+++ Math/Easy/2037-count-square-sum-triples/count-square-sum-triples.java
@@ -1,13 +1,33 @@
 class Solution {
     /**
-     * Approach I : Using Enumeration + Hashing Approach
+     * Approach II : Using Enumeration + Math Approach
      *
      * TC: O(N x N)
-     * SC: O(N)
+     * SC: O(1)
      */
     public int countTriples(int n) {
         int count = 0;
-        Map<Integer, ArrayList<int[]>> map = new HashMap<Integer, ArrayList<int[]>>(); // SC: O(N)
+        for (int a = 1; a <= n; a++) {     // TC: O(N)
+            for (int b = 1; b <= n; b++) { // TC: O(N)
+                int squares = a * a + b * b;
+                int c = (int) Math.sqrt(squares);
+                if (c * c == squares && c <= n) {
+                    count++;
+                }
+            }
+        }
+        return count;
+    }
+
+    /**
+     * Approach I : Using Brute-Force (Enumeration + Hashing) Approach
+     *
+     * TC: O(N x N)
+     * SC: O(N x N)
+     */
+    public int countTriplesBruteForce(int n) {
+        int count = 0;
+        Map<Integer, ArrayList<int[]>> map = new HashMap<Integer, ArrayList<int[]>>(); // SC: O(N x N)
         for (int i = 1; i <= n; i++) {     // TC: O(N)
             for (int j = 1; j <= n; j++) { // TC: O(N)
                 map.computeIfAbsent((i * i + j * j), k -> new ArrayList<int[]>()).add(new int[] { i, j });
