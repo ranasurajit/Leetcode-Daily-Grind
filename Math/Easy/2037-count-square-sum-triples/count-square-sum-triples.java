1d30b20a0851676c258e150ff263d06acaae0e15
Surajit Rana <surajit1602@gmail.com>
2025-12-08 10:08:45 +0530

    Time: 7 ms (88.94%) | Memory: 42.1 MB (37.58%) - LeetSync

--- Math/Easy/2037-count-square-sum-triples/count-square-sum-triples.java
+++ Math/Easy/2037-count-square-sum-triples/count-square-sum-triples.java
@@ -8,15 +8,16 @@ class Solution {
     public int countTriples(int n) {
         int count = 0;
         for (int a = 1; a <= n; a++) {     // TC: O(N)
-            for (int b = 1; b <= n; b++) { // TC: O(N)
+            for (int b = a + 1; b <= n; b++) { // TC: O(N)
                 int squares = a * a + b * b;
                 int c = (int) Math.sqrt(squares);
-                if (c <= n) {
-                    if (c * c == squares) {
-                        count++;
-                    }
-                } else {
+                if (c > n) {
+                    // out of range
                     break;
+                }
+                if (c * c == squares) {
+                    // as pairs can be swappable
+                    count += 2;
                 }
             }
         }
