960d2d5c27535b990b6c32dc5836a072c03c6334
Surajit Rana <surajit1602@gmail.com>
2025-12-12 00:51:43 +0530

    Time: 4 ms (37.96%) | Memory: 43 MB (61.27%) - LeetSync

--- Stacks/Simulation/Easy/20-valid-parentheses/valid-parentheses.java
+++ Stacks/Simulation/Easy/20-valid-parentheses/valid-parentheses.java
@@ -8,8 +8,7 @@ class Solution {
     public boolean isValid(String s) {
         int n = s.length();
         Deque<Character> deque = new ArrayDeque<Character>(); // SC: O(N)
-        for (int i = 0; i < n; i++) { // TC: O(N)
-            char ch = s.charAt(i);
+        for (char ch : s.toCharArray()) { // TC: O(N)
             if (deque.isEmpty() && !isOpenBracket(ch)) {
                 return false;
             }
