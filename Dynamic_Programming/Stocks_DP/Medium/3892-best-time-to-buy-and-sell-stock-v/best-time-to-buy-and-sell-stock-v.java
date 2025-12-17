09a44eb16cdfc1984adf24895f8d87923ae857a1
Surajit Rana <surajit1602@gmail.com>
2025-12-17 16:49:13 +0530

    Time: 44 ms (67.73%) | Memory: 100.1 MB (50.36%) - LeetSync

--- Dynamic_Programming/Stocks_DP/Medium/3892-best-time-to-buy-and-sell-stock-v/best-time-to-buy-and-sell-stock-v.java
+++ Dynamic_Programming/Stocks_DP/Medium/3892-best-time-to-buy-and-sell-stock-v/best-time-to-buy-and-sell-stock-v.java
@@ -4,6 +4,60 @@ class Solution {
     private int[] prices;
 
     /**
+     * Approach III : Using Tabulation Approach
+     *
+     * TC: O(N x 3 x K) + O(3 x K) ~ O(N x K)
+     * SC: O(N x 3 x K) ~ O(N x K)
+     * - O(N x K) - dp array
+     *
+     * Accepted (776 / 776 testcases passed)
+     */
+    public long maximumProfit(int[] prices, int k) {
+        this.n = prices.length;
+        this.k = k;
+        this.prices = prices;
+        // holdingState -> 0: no positions, 1: bought, 2: short-sold
+        // Initialization
+        long[][][] dp = new long[n + 1][3][k + 1]; // SC: O(N x K)
+        for (int j = 1; j <= 2; j++) { // TC: O(3)
+            for (int p = 0; p <= k; p++) { // TC: O(K)
+                dp[n][j][p] = Long.MIN_VALUE / 2;
+            }
+        }
+        // Iterative Calls
+        for (int i = n - 1; i >= 0; i--) {     // TC: O(N)
+            for (int j = 2; j >= 0; j--) {     // TC: O(3)
+                for (int p = k; p >= 0; p--) { // TC: O(K)
+                    long skip = dp[i + 1][j][p];
+                    long maxProfit = skip;
+                    if (j == 0) {
+                        if (p < k) {
+                            // pick buy
+                            long pickBuy = -1 * prices[i] + dp[i + 1][1][p];
+                            // pick short sell
+                            long pickShortSell = prices[i] + dp[i + 1][2][p];
+                            maxProfit = Math.max(skip, Math.max(pickBuy, pickShortSell));
+                        }
+                    } else if (j == 1) {
+                        // pick sell
+                        if (p + 1 <= k) {
+                            long pickSell = prices[i] + dp[i + 1][0][p + 1];
+                            maxProfit = Math.max(skip, pickSell);
+                        }
+                    } else {
+                        if (p + 1 <= k) {
+                            long pickShortBuy = -1 * prices[i] + dp[i + 1][0][p + 1];
+                            maxProfit = Math.max(skip, pickShortBuy);
+                        }
+                    }
+                    dp[i][j][p] = maxProfit;
+                }
+            }
+        }
+        return dp[0][0][0];
+    }
+
+    /**
      * Approach II : Using Memoization Approach
      *
      * TC: O(N x 3 x K) ~ O(N x K)
@@ -13,12 +67,12 @@ class Solution {
      *
      * Accepted (776 / 776 testcases passed)
      */
-    public long maximumProfit(int[] prices, int k) {
+    public long maximumProfitMemoization(int[] prices, int k) {
         this.n = prices.length;
         this.k = k;
         this.prices = prices;
         // holdingState -> 0: no positions, 1: bought, 2: short-sold
-        long[][][] memo = new long[n][3][k + 1];
+        long[][][] memo = new long[n][3][k + 1]; // SC: O(N x K)
         for (long[][] mem : memo) {
             for (long[] m : mem) {
                 Arrays.fill(m, -1L);
