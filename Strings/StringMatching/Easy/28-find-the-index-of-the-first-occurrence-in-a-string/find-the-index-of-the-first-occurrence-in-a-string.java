e4e753f0164396698661bd32ce3c0b1e64e55525
Surajit Rana <surajit1602@gmail.com>
2025-12-20 00:27:54 +0530

    Time: 2 ms (7.14%) | Memory: 43.1 MB (12.04%) - LeetSync

--- Strings/StringMatching/Easy/28-find-the-index-of-the-first-occurrence-in-a-string/find-the-index-of-the-first-occurrence-in-a-string.java
+++ Strings/StringMatching/Easy/28-find-the-index-of-the-first-occurrence-in-a-string/find-the-index-of-the-first-occurrence-in-a-string.java
@@ -1,11 +1,68 @@
 class Solution {
     /**
-     * Approach : Using Two Pointers Approach
+     * Approach II : Using KMP Algorithm + Two Pointers Approach
+     *
+     * TC: O(M + N)
+     * SC: O(M)
+     */
+    public int strStr(String haystack, String needle) {
+        int n = haystack.length();
+        int m = needle.length();
+        // construct lps array from pattern i.e. needle
+        int[] lps = new int[m]; // SC: O(M)
+        constructLPS(needle, lps, m); // TC: O(M)
+        int i = 0; // pointer at the start of String 'haystack'
+        int j = 0; // pointer at the start of String 'needle'
+        while (i < n) { // TC: O(N)
+            if (haystack.charAt(i) == needle.charAt(j)) {
+                i++;
+                j++;
+                if (j == m) {
+                    return i - j;
+                }
+            } else {
+                if (j > 0) {
+                    j = lps[j - 1];
+                } else {
+                    i++;
+                }
+            }
+        }
+        return -1;
+    }
+
+    /**
+     * Using KMP Algorithm to compute LPS Approach
+     *
+     * TC: O(M)
+     * SC: O(1)
+     */
+    private void constructLPS(String pattern, int[] lps, int m) {
+        int i = 1;
+        int len = 0;
+        while (i < m) { // TC: O(M)
+            if (pattern.charAt(i) == pattern.charAt(len)) {
+                len++;
+                lps[i] = len;
+                i++;
+            } else {
+                if (len > 0) {
+                    len = lps[len - 1];
+                } else {
+                    lps[i] = 0;
+                    i++;
+                }
+            }
+        }
+    }
+
+    /**
+     * Approach I : Using Two Pointers Approach
      *
      * TC: O(N x M)
      * SC: O(1)
      */
-    public int strStr(String haystack, String needle) {
+    public int strStrUsingTwoPointers(String haystack, String needle) {
         int n = haystack.length();
         int m = needle.length();
         int i = 0; // pointer at the start of String 'haystack'
