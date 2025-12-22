6b51da279ee04f12497a0e8d7dc1fdf313f7958f
Surajit Rana <surajit1602@gmail.com>
2025-12-22 15:59:02 +0530

    Time: 7 ms (69.23%) | Memory: 44.2 MB (47.44%) - LeetSync

--- Dynamic_Programming/LIS/Hard/1000-delete-columns-to-make-sorted-iii/delete-columns-to-make-sorted-iii.java
+++ Dynamic_Programming/LIS/Hard/1000-delete-columns-to-make-sorted-iii/delete-columns-to-make-sorted-iii.java
@@ -3,29 +3,55 @@ class Solution {
     private int n;
 
     /**
-     * Approach II : Using Memoization Approach
+     * Approach III : Using Optimized Tabulation Approach
      *
-     * TC: O(N x N)
-     * SC: O(N x N) + O(N)
-     * - O(N x N) - memoization memory
-     * - O(N) - recursion stack memory (reused)
+     * TC: O(N x N x M)
+     * SC: O(N)
+     * - O(N) - dp array memory
      *
      * Accepted (131 / 131 testcases passed)
      */
     public int minDeletionSize(String[] strs) {
         this.m = strs.length;
         this.n = strs[0].length();
+        int[] dp = new int[n]; // SC: O(N)
+        Arrays.fill(dp, 1); // all elements are LIS of length 1 by-default
+        int maxLIS = 1;
+        for (int idx = 1; idx < n; idx++) { // TC: O(N)
+            for (int prevIdx = 0; prevIdx < idx; prevIdx++) { // TC: O(N)
+                if (canBeIncluded(idx, prevIdx, strs) && dp[idx] < dp[prevIdx] + 1) { // TC: O(M)
+                    dp[idx] = dp[prevIdx] + 1;
+                }
+            }
+            maxLIS = Math.max(maxLIS, dp[idx]);
+        }
+        return n - maxLIS;
+    }
+
+    /**
+     * Approach II : Using Memoization Approach
+     *
+     * TC: O(N x N x M)
+     * SC: O(N x N) + O(N)
+     * - O(N x N) - memoization memory
+     * - O(N) - recursion stack memory (reused)
+     *
+     * Accepted (131 / 131 testcases passed)
+     */
+    public int minDeletionSizeMemoization(String[] strs) {
+        this.m = strs.length;
+        this.n = strs[0].length();
         int[][] memo = new int[n][n + 1]; // SC: O(N x N)
         for (int[] mem : memo) {
             Arrays.fill(mem, -1);
         }
-        return n - lisMemoization(0, -1, strs, memo); // TC: O(N x N), SC: O(N)
+        return n - lisMemoization(0, -1, strs, memo); // TC: O(N x N x M), SC: O(N)
     }
 
     /**
      * Using Memoization Approach
      *
-     * TC: O(N x N)
+     * TC: O(N x N x M)
      * SC: O(N)
      */
     private int lisMemoization(int idx, int prevIdx, String[] strs, int[][] memo) {
@@ -41,7 +67,7 @@ class Solution {
         // pick or skip
         int skip = lisMemoization(idx + 1, prevIdx, strs, memo);
         int pick = 0;
-        if (prevIdx == -1 || canBeIncluded(idx, prevIdx, strs)) {
+        if (prevIdx == -1 || canBeIncluded(idx, prevIdx, strs)) { // TC: O(M)
             pick = 1 + lisMemoization(idx + 1, idx, strs, memo);
         }
         return memo[idx][prevIdx + 1] = Math.max(pick, skip);
@@ -77,7 +103,7 @@ class Solution {
         // pick or skip
         int skip = lisRecursion(idx + 1, prevIdx, strs);
         int pick = 0;
-        if (prevIdx == -1 || canBeIncluded(idx, prevIdx, strs)) {
+        if (prevIdx == -1 || canBeIncluded(idx, prevIdx, strs)) { // TC: O(M)
             pick = 1 + lisRecursion(idx + 1, idx, strs);
         }
         return Math.max(pick, skip);
