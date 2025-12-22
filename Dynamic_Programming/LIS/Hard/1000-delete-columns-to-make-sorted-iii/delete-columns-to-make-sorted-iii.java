7c8af6340dc98c0f91df808a16dfcd5facb1c79c
Surajit Rana <surajit1602@gmail.com>
2025-12-22 15:52:18 +0530

    Time: 7 ms (69.23%) | Memory: 46.2 MB (7.69%) - LeetSync

--- Dynamic_Programming/LIS/Hard/1000-delete-columns-to-make-sorted-iii/delete-columns-to-make-sorted-iii.java
+++ Dynamic_Programming/LIS/Hard/1000-delete-columns-to-make-sorted-iii/delete-columns-to-make-sorted-iii.java
@@ -3,70 +3,23 @@ class Solution {
     private int n;
 
     /**
-     * Approach III : Using Tabulation (Optimal) Approach
+     * Approach II : Using Memoization Approach
      *
-     * TC: O(N x N x N)
-     * SC: O(N x N)
-     * - O(N x N) - tabulation dp memory
+     * TC: O(N x N)
+     * SC: O(N x N) + O(N)
+     * - O(N x N) - memoization memory
+     * - O(N) - recursion stack memory (reused)
      *
      * Accepted (131 / 131 testcases passed)
      */
     public int minDeletionSize(String[] strs) {
         this.m = strs.length;
         this.n = strs[0].length();
-        int maxLIS = 0;
-        for (int j = 0; j < n; j++) { // TC: O(N)
-            maxLIS = Math.max(maxLIS, lisOptimalTabulation(strs, n)); // TC: O(N x N), SC: O(N)
+        int[][] memo = new int[n][n + 1]; // SC: O(N x N)
+        for (int[] mem : memo) {
+            Arrays.fill(mem, -1);
         }
-        return n - maxLIS;
-    }
-
-    /**
-     * Using Tabulation (Optimal) Approach
-     *
-     * TC: O(N x N)
-     * SC: O(N)
-     */
-    private int lisOptimalTabulation(String[] strs, int n) {
-        int[] dp = new int[n]; // SC: O(N)
-        Arrays.fill(dp, 1); // all elements are LIS of length 1 by-default
-        int maxLength = 1;
-        for (int idx = 1; idx < n; idx++) { // TC: O(N)
-            // prevIdx cannot exceed idx
-            for (int prevIdx = 0; prevIdx < idx; prevIdx++) { // TC: O(N)
-                if (canBeIncluded(idx, prevIdx, strs)) {
-                    if (dp[idx] < dp[prevIdx] + 1) {
-                        dp[idx] = dp[prevIdx] + 1;
-                    }
-                }
-            }
-            maxLength = Math.max(maxLength, dp[idx]);
-        }
-        return maxLength;
-    }
-
-    /**
-     * Approach II : Using Memoization Approach
-     *
-     * TC: O(N x N x N)
-     * SC: O(N x N x N) + O(N)
-     * - O(N x N x N) - memoization memory
-     * - O(N) - recursion stack memory (reused)
-     *
-     * Accepted (131 / 131 testcases passed)
-     */
-    public int minDeletionSizeMemoization(String[] strs) {
-        this.m = strs.length;
-        this.n = strs[0].length();
-        int maxLIS = 0;
-        for (int j = 0; j < n; j++) { // TC: O(N)
-            int[][] memo = new int[n][n + 1]; // SC: O(N x N)
-            for (int[] mem : memo) {
-                Arrays.fill(mem, -1);
-            }
-            maxLIS = Math.max(maxLIS, lisMemoization(j, -1, strs, memo)); // TC: O(N x N), SC: O(N)
-        }
-        return n - maxLIS;
+        return n - lisMemoization(0, -1, strs, memo); // TC: O(N x N), SC: O(N)
     }
 
     /**
@@ -106,11 +59,7 @@ class Solution {
     public int minDeletionSizeRecursion(String[] strs) {
         this.m = strs.length;
         this.n = strs[0].length();
-        int maxLIS = 0;
-        for (int j = 0; j < n; j++) { // TC: O(N)
-            maxLIS = Math.max(maxLIS, lisRecursion(j, -1, strs)); // TC: O(Exponential), SC: O(N)
-        }
-        return n - maxLIS;
+        return n - lisRecursion(0, -1, strs); // TC: O(Exponential), SC: O(N)
     }
 
     /**
