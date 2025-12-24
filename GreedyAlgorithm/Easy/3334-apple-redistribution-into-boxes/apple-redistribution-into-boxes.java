cd5e24f6df57b1f45787dc4122ee9e3b80903b5d
Surajit Rana <surajit1602@gmail.com>
2025-12-24 12:33:37 +0530

    Time: 1 ms (98.81%) | Memory: 44.4 MB (59.94%) - LeetSync

--- GreedyAlgorithm/Easy/3334-apple-redistribution-into-boxes/apple-redistribution-into-boxes.java
+++ GreedyAlgorithm/Easy/3334-apple-redistribution-into-boxes/apple-redistribution-into-boxes.java
@@ -1,11 +1,54 @@
 class Solution {
     /**
-     * Approach : Using Greedy + Sorting Approach
+     * Approach II : Using Greedy Approach
+     *
+     * TC: O(N) + O(M) + O(Max(capacity)) ~ O(N + M + Max(capacity))
+     * SC: O(Max(capacity))
+     */
+    public int minimumBoxes(int[] apple, int[] capacity) {
+        int n = apple.length;
+        int m = capacity.length;
+        int totalApples = 0;
+        int maxCapacity = 0;
+        for (int i = 0; i < n; i++) { // TC: O(N)
+            totalApples += apple[i];
+        }
+        for (int i = 0; i < m; i++) { // TC: O(M)
+            maxCapacity = Math.max(maxCapacity, capacity[i]);
+        }
+        /**
+         * Greedily we can choose a box with higher capacity
+         * to fill it full so that we can minimize the 
+         * number of box usages, we can build a frequency
+         * of array of capacity to back fill it
+         *
+         * also no need to check if we have enough capacity
+         * to fill all apples as in constraint it is mentioned
+         * that 'The input is generated such that it's possible 
+         * to redistribute packs of apples into boxes.'
+         */
+        int[] freqCap = new int[maxCapacity + 1]; // SC: O(Max(capacity))
+        for (int i = 0; i < m; i++) { // TC: O(M)
+            freqCap[capacity[i]]++;
+        }
+        int boxesUsed = 0;
+        for (int i = maxCapacity; i >= 0; i--) { // TC: O(Max(capacity))
+            while (freqCap[i] > 0 && totalApples > 0) {
+                totalApples -= i;
+                freqCap[i]--;
+                boxesUsed++;
+            }
+        }
+        return boxesUsed;
+    }
+
+    /**
+     * Approach I : Using Greedy + Sorting Approach
      *
      * TC: O(N) + O(M x log(M)) + O(M) ~ O(N + M x log(M))
      * SC: O(1)
      */
-    public int minimumBoxes(int[] apple, int[] capacity) {
+    public int minimumBoxesUsingSorting(int[] apple, int[] capacity) {
         int n = apple.length;
         int m = capacity.length;
         int totalApples = 0;
