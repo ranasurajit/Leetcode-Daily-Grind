a3522859f404aa0b61db98cf72b4a5530494baca
Surajit Rana <surajit1602@gmail.com>
2025-12-26 13:47:54 +0530

    Time: 16 ms (11.21%) | Memory: 47.2 MB (20.69%) - LeetSync

---
+++ Strings/PrefixSuffix/Medium/2576-minimum-penalty-for-a-shop/minimum-penalty-for-a-shop.java
@@ -0,0 +1,67 @@
+class Solution {
+    /**
+     * Approach II : Using Optimal (Prefix-Sum) Approach
+     *
+     * TC: O(N) + O(N) ~ O(N)
+     * SC: O(N) + O(N) ~ O(N)
+     *
+     * Accepted (42 / 42 testcases passed)
+     */
+    public int bestClosingTime(String customers) {
+        int n = customers.length();
+        /**
+         * We need to compute prefix sum of count of N's from 
+         * right to left and pre-fill in an extra array
+         */
+        int[] countY = new int[n + 1]; // SC: O(N)
+        int[] countN = new int[n + 1]; // SC: O(N)
+        for (int i = 0; i <= n; i++) { // TC: O(N)
+            countY[i] = (i > 0 ? countY[i - 1] : 0) + (i < n && customers.charAt(i) == 'Y' ? 1 : 0);
+            countN[i] = (i > 0 ? countN[i - 1] : 0) + (i < n && customers.charAt(i) == 'N' ? 1 : 0);
+        }
+        int minPenalty = n;
+        int minHour = n;
+        for (int i = 0; i <= n; i++) { // TC: O(N) - i is the closing time index
+            // penalty at any index i = countYs[i...(n - 1)] + countNs[0 to (i - 1)]
+            int countYi = countY[n - 1] - (i > 0 ? countY[i - 1] : 0);
+            int countNi = (i > 0 ? countN[i - 1] : 0);
+            int currentPenalty = countYi + countNi;
+            if (minPenalty > currentPenalty) {
+                minPenalty = currentPenalty;
+                minHour = i;
+            }
+        }
+        return minHour;
+    }
+
+    /**
+     * Approach I : Using Brute-Force (String Simulation) Approach
+     *
+     * TC: O(N x N)
+     * SC: O(1)
+     *
+     * Time Limit Exceeded (33 / 42 testcases passed)
+     */
+    public int bestClosingTimeBruteForce(String customers) {
+        int n = customers.length();
+        int minPenalty = n;
+        int minHour = n;
+        int index = 0; // shop closed time index
+        while (index <= n) { // TC: O(N)
+            int currentPenalty = 0;
+            for (int i = 0; i < n; i++) { // TC: O(N)
+                if (i >= index && customers.charAt(i) == 'Y') {
+                    currentPenalty++;
+                } else if (i < index && customers.charAt(i) == 'N') {
+                    currentPenalty++;
+                }
+            }
+            if (minPenalty > currentPenalty) {
+                minPenalty = currentPenalty;
+                minHour = index;
+            }
+            index++;
+        }
+        return minHour;
+    }
+}
