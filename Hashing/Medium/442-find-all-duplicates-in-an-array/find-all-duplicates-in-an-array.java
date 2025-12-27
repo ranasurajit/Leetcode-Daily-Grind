7cd4bcccffb985c4c0f235bcc5b11ddd877f72ad
Surajit Rana <surajit1602@gmail.com>
2025-12-27 23:22:20 +0530

    Time: 6 ms (89.09%) | Memory: 57.4 MB (84.81%) - LeetSync

--- Hashing/Medium/442-find-all-duplicates-in-an-array/find-all-duplicates-in-an-array.java
+++ Hashing/Medium/442-find-all-duplicates-in-an-array/find-all-duplicates-in-an-array.java
@@ -1,11 +1,31 @@
 class Solution {
     /**
-     * Approach : Using Hashing Approach
+     * Approach II : Using Simulation Approach
+     *
+     * TC: O(N)
+     * SC: O(1)
+     */
+    public List<Integer> findDuplicates(int[] nums) {
+        int n = nums.length;
+        List<Integer> duplicates = new ArrayList<Integer>();
+        for (int i = 0; i < n; i++) { // TC: O(N)
+            int index = Math.abs(nums[i]) - 1;
+            if (nums[index] < 0) {
+                duplicates.add(Math.abs(nums[i]));
+            } else {
+                nums[index] = -nums[index];
+            }
+        }
+        return duplicates;
+    }
+
+    /**
+     * Approach I : Using Hashing Approach
      *
      * TC: O(N)
      * SC: O(N)
      */
-    public List<Integer> findDuplicates(int[] nums) {
+    public List<Integer> findDuplicatesHashingApproach(int[] nums) {
         List<Integer> duplicates = new ArrayList<Integer>();
         Set<Integer> set = new HashSet<Integer>(); // SC: O(N)
         for (int num : nums) { // TC: O(N)
