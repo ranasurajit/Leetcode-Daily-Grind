88952a078fae60bcddb01a3029e01be8651db573
Surajit Rana <surajit1602@gmail.com>
2025-12-28 12:31:45 +0530

    Time: 0 ms (100.00%) | Memory: 47.2 MB (7.96%) - LeetSync

--- BinarySearch/2DArrays/Easy/1476-count-negative-numbers-in-a-sorted-matrix/count-negative-numbers-in-a-sorted-matrix.java
+++ BinarySearch/2DArrays/Easy/1476-count-negative-numbers-in-a-sorted-matrix/count-negative-numbers-in-a-sorted-matrix.java
@@ -1,11 +1,48 @@
 class Solution {
     /**
-     * Approach I : Using 2D Array Simulation Approach
+     * Approach II : Using Binary Search Approach
+     *
+     * TC: O(M x log(N))
+     * SC: O(1)
+     */
+    public int countNegatives(int[][] grid) {
+        int m = grid.length;
+        int n = grid[0].length;
+        int count = 0;
+        for (int i = 0; i < m; i++) {           // TC: O(M)
+            count += lowerBound(grid[i], n, 0); // TC: O(log(N))
+        }
+        return count;
+    }
+
+    /**
+     * Using Binary Search (To Find Lower Bound) Approach
+     *
+     * TC: O(log(N))
+     * SC: O(1)
+     */
+    private int lowerBound(int[] row, int n, int target) {
+        int low = 0;
+        int high = n - 1;
+        while (low <= high) { // TC: O(log(N))
+            int mid = low + (high - low) / 2;
+            if (row[mid] >= target) {
+                low = mid + 1;
+            } else {
+                // move to the left to find least index with negative value
+                high = mid - 1;
+            }
+        }
+        return n - low;
+    }
+
+    /**
+     * Approach I : Using Brute-Force (2D Array Simulation) Approach
      *
      * TC: O(M x N)
      * SC: O(1)
      */
-    public int countNegatives(int[][] grid) {
+    public int countNegativesBruteForce(int[][] grid) {
         int m = grid.length;
         int n = grid[0].length;
         int count = 0;
