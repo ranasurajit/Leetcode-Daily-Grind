75c358e6f4e163afdc3eb787745d6245d3839ba9
Surajit Rana <surajit1602@gmail.com>
2026-01-01 07:54:47 +0530

    Time: 0 ms (100.00%) | Memory: 43.7 MB (7.44%) - LeetSync

--- Arrays/Math/Easy/66-plus-one/plus-one.java
+++ Arrays/Math/Easy/66-plus-one/plus-one.java
@@ -1,11 +1,32 @@
 class Solution {
     /**
-     * Approach : Using Brute-Force Approach
+     * Approach II : Using Inplace Substitution Approach
+     *
+     * TC: O(N)
+     * SC: O(1)
+     */
+    public int[] plusOne(int[] digits) {
+        int n = digits.length;
+        for (int i = n - 1; i >= 0; i--) { // TC: O(N)
+            digits[i]++;
+            if (digits[i] == 10) {
+                digits[i] = 0;
+            } else {
+                return digits;
+            }
+        }
+        digits = new int[n + 1];
+        digits[0] = 1;
+        return digits;
+    }
+
+    /**
+     * Approach I : Using Brute-Force Approach
      *
      * TC: O(N) + O(N) ~ O(N)
      * SC: O(N)
      */
-    public int[] plusOne(int[] digits) {
+    public int[] plusOneBruteForce(int[] digits) {
         int n = digits.length;
         List<Integer> onePlusList = new ArrayList<Integer>(); // SC: O(N)
         int carry = 1; // adding 1 to digits
