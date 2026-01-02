07dfee61c81bc826ca03fb56c86b3ea840f74231
Surajit Rana <surajit1602@gmail.com>
2026-01-02 09:13:13 +0530

    Time: 9 ms (45.11%) | Memory: 47.8 MB (6.11%) - LeetSync

---
+++ Hashing/Easy/1001-n-repeated-element-in-size-2n-array/n-repeated-element-in-size-2n-array.java
@@ -0,0 +1,22 @@
+class Solution {
+    /**
+     * Approach I : Using Hashing Approach
+     *
+     * TC: O(M) ~ O(2 x N) ~ O(N)
+     * SC: O(N)
+     */
+    public int repeatedNTimes(int[] nums) {
+        int m = nums.length;
+        Set<Integer> set = new HashSet<Integer>(); // SC: O(N)
+        int countAddFailures = 0;
+        int repeated = -1;
+        for (int num : nums) { // TC: O(M)
+            if (!set.add(num)) {
+                countAddFailures++;
+                repeated = num;
+            }
+        }
+        // as Set will already be having 1 occurence of repeated
+        return countAddFailures == (m / 2) - 1 ? repeated : -1;
+    }
+}
