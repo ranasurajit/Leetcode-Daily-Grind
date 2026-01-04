2abef0dcb58f0dce1f091092fd183366fa020199
Surajit Rana <surajit1602@gmail.com>
2026-01-04 13:01:16 +0530

    Time: 13 ms (60.35%) | Memory: 46.3 MB (68.18%) - LeetSync

--- Arrays/Math/Medium/1284-four-divisors/four-divisors.java
+++ Arrays/Math/Medium/1284-four-divisors/four-divisors.java
@@ -1,5 +1,49 @@
 class Solution {
     /**
+     * Approach II : Using Better (Simulation) Approach
+     *
+     * TC: O(N x Sqrt(M)), where M = max(nums)
+     * SC: O(1)
+     *
+     * Accepted (18 / 18 testcases passed)
+     */
+    public int sumFourDivisors(int[] nums) {
+        int n = nums.length;
+        int sum = 0;
+        for (int i = 0; i < n; i++) { // TC: O(N)
+            sum += sumOfAllDivisorsBetter(nums[i]); // TC: O(Sqrt(M))
+        }
+        return sum;
+    }
+
+    /**
+     * Using Simulation Approach
+     *
+     * TC: O(Sqrt(M))
+     * SC: O(1)
+     */
+    private int sumOfAllDivisorsBetter(int num) {
+        int sum = 1 + num;
+        int count = 0;
+        for (int i = 2; i <= (int) Math.sqrt(num); i++) { // TC: O(Sqrt(M))
+            if (num % i == 0) {
+                sum += i;
+                count++;
+                if (i != (num / i)) {
+                    sum += (num / i);
+                    count++;
+                }
+                if (count > 2) {
+                    // early break as num will be having more than 4 divisors
+                    break;
+                }
+            }
+        }
+        // we did not add 1 and number itself
+        return count == 2 ? sum : 0;
+    }
+
+    /**
      * Approach I : Using Brute-Force (Simulation) Approach
      *
      * TC: O(N x M), where M = max(nums)
@@ -7,7 +51,7 @@ class Solution {
      *
      * Accepted (18 / 18 testcases passed)
      */
-    public int sumFourDivisors(int[] nums) {
+    public int sumFourDivisorsBruteForce(int[] nums) {
         int n = nums.length;
         int sum = 0;
         for (int i = 0; i < n; i++) { // TC: O(N)
