dce1b76ce845a57522bc1ae999ba506c9c1cdc8a
Surajit Rana <surajit1602@gmail.com>
2026-01-06 09:57:28 +0530

    Time: 7 ms (94.13%) | Memory: 49.4 MB (17.48%) - LeetSync

--- BinaryTrees/BFS_DFS/Medium/1116-maximum-level-sum-of-a-binary-tree/maximum-level-sum-of-a-binary-tree.java
+++ BinaryTrees/BFS_DFS/Medium/1116-maximum-level-sum-of-a-binary-tree/maximum-level-sum-of-a-binary-tree.java
@@ -15,13 +15,53 @@
  */
 class Solution {
     /**
+     * Approach II : Using DFS Approach
+     *
+     * TC: O(N) + O(H) ~ O(N)
+     * SC: O(N) + O(H) - recursion stack
+     * where N = number of nodes of the tree, H = height of tree = log(N) ~ N (in worst case)
+     */
+    public int maxLevelSum(TreeNode root) {
+        List<Long> sumLevel = new ArrayList<Long>();
+        dfsTree(root, 0, sumLevel); // TC: O(N), SC: O(H)
+        long maxSum = Long.MIN_VALUE;
+        int maxLevel = 0;
+        for (int i = 0; i < sumLevel.size(); i++) { // TC: O(H)
+            if (sumLevel.get(i) > maxSum) {
+                maxSum = sumLevel.get(i);
+                maxLevel = i + 1;
+            }
+        }
+        return maxLevel;
+    }
+
+    /**
+     * Using DFS Approach
+     *
+     * TC: O(N)
+     * SC: O(log(N)) - recursion stack
+     * where N = number of nodes of the tree
+     */
+    private void dfsTree(TreeNode node, int level, List<Long> sumLevel) {
+        if (node == null) {
+            return;
+        }
+        if (sumLevel.size() == level) {
+            sumLevel.add(0L);
+        }
+        sumLevel.set(level, sumLevel.get(level) + node.val); // adding current node's value
+        dfsTree(node.left, level + 1, sumLevel);
+        dfsTree(node.right, level + 1, sumLevel);
+    }
+
+    /**
      * Approach I : Using BFS Approach
      *
      * TC: O(N)
      * SC: O(N)
      * where N = number of nodes of the tree
      */
-    public int maxLevelSum(TreeNode root) {
+    public int maxLevelSumBFS(TreeNode root) {
         if (root == null) {
             return 1;
         }
