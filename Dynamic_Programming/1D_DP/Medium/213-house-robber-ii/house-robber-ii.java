e2f10c0a672e014fb9c5f276531b45848d403eaa
Surajit Rana <surajit1602@gmail.com>
2026-01-10 01:53:48 +0530

    Time: 0 ms (100.00%) | Memory: 42.8 MB (43.19%) - LeetSync

---
+++ Dynamic_Programming/1D_DP/Medium/213-house-robber-ii/house-robber-ii.java
@@ -0,0 +1,112 @@
+class Solution {
+    /**
+     * Approach II : Using Memoization Approach
+     *
+     * TC: O(N) + O(N) + O(N) ~ O(N)
+     * SC: O(N) + O(N) + O(N) + O(N) + O(N) ~ O(N) + O(N)
+     * - O(N) - memoization space
+     * - O(N) - recursion stack space
+     *
+     * Accepted (75 / 75 testcases passed)
+     */
+    public int rob(int[] nums) {
+        int n = nums.length;
+        if (n < 2) {
+            return nums[0];
+        }
+        int[] nums1 = new int[n - 1]; // SC: O(N)
+        int[] nums2 = new int[n - 1]; // SC: O(N)
+        for (int i = 0; i < n - 1; i++) { // TC: O(N)
+            nums1[i] = nums[i];
+            nums2[i] = nums[i + 1];
+        }
+        int[] memo1 = new int[n - 1]; // SC: O(N)
+        int[] memo2 = new int[n - 1]; // SC: O(N)
+        Arrays.fill(memo1, -1);
+        Arrays.fill(memo2, -1);
+        return Math.max(
+            solveMemoization(nums1, n - 2, memo1),
+            solveMemoization(nums2, n - 2, memo2)
+        );
+    }
+
+    /**
+     * Using Memoization Approach
+     *
+     * TC: O(N)
+     * SC: O(N) - recursion stack space
+     */
+    private int solveMemoization(int[] nums, int idx, int[] memo) {
+        // Base Case
+        if (idx < 0) {
+            return Integer.MIN_VALUE;
+        }
+        // Memoization Check
+        if (memo[idx] != -1) {
+            return memo[idx];
+        }
+        // Recursion Calls
+        // if index 'idx' is skipped
+        int skip = Integer.MIN_VALUE;
+        if (idx > 0) {
+            skip = solveMemoization(nums, idx - 1, memo);
+        }
+        // if index 'idx' is picked
+        int pick = nums[idx];
+        if (idx > 1) {
+            pick += solveMemoization(nums, idx - 2, memo);
+        }
+        return memo[idx] = Math.max(pick, skip);
+    }
+
+    /**
+     * Approach I : Using Recursion Approach
+     *
+     * TC: O(N) + O(2 ^ N) ~ O(2 ^ N)
+     * SC: O(N) + O(N) + O(N) - recursion stack space
+     * - O(N) - recursion stack space
+     *
+     * Time Limit Exceeded (62 / 75 testcases passed)
+     */
+    public int robRecursion(int[] nums) {
+        int n = nums.length;
+        if (n < 2) {
+            return nums[0];
+        }
+        int[] nums1 = new int[n - 1]; // SC: O(N)
+        int[] nums2 = new int[n - 1]; // SC: O(N)
+        for (int i = 0; i < n - 1; i++) { // TC: O(N)
+            nums1[i] = nums[i];
+            nums2[i] = nums[i + 1];
+        }
+        return Math.max(
+            solveRecursion(nums1, n - 2),
+            solveRecursion(nums2, n - 2)
+        );
+    }
+
+    /**
+     * Using Recursion Approach
+     *
+     * TC: O(2 ^ N)
+     * SC: O(N) - recursion stack space
+     */
+    private int solveRecursion(int[] nums, int idx) {
+        // Base Case
+        if (idx < 0) {
+            return Integer.MIN_VALUE;
+        }
+        // Recursion Calls
+        // if index 'idx' is skipped
+        int skip = Integer.MIN_VALUE;
+        if (idx > 0) {
+            skip = solveRecursion(nums, idx - 1);
+        }
+        // if index 'idx' is picked
+        int pick = nums[idx];
+        if (idx > 1) {
+            pick += solveRecursion(nums, idx - 2);
+        }
+        return Math.max(pick, skip);
+    }
+}
