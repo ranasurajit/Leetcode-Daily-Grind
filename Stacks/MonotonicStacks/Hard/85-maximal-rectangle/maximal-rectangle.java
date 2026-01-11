32e86e7ccf1a6dceb93610b8fa4cfa06c9d94fd3
Surajit Rana <surajit1602@gmail.com>
2026-01-11 13:03:12 +0530

    Time: 14 ms (76.52%) | Memory: 49.8 MB (44.09%) - LeetSync

--- Stacks/MonotonicStacks/Hard/85-maximal-rectangle/maximal-rectangle.java
+++ Stacks/MonotonicStacks/Hard/85-maximal-rectangle/maximal-rectangle.java
@@ -1,11 +1,92 @@
 class Solution {
     /**
-     * Approach : Using Monotonic Stack Approach
+     * Approach II : Using Monotonic Deque Approach
      *
-     * TC: O(M x  2 x N) ~ O(M x N)
-     * SC: O(N) + O(N) (reused M times) ~ O(N)
+     * TC: O(M x 2 x N) ~ O(M x N)
+     * SC: O(N) + O(M x N) ~ O(M x N)
      */
     public int maximalRectangle(char[][] matrix) {
+        int m = matrix.length;
+        int n = matrix[0].length;
+        int[] height = new int[n]; // SC: O(N)
+        int maxArea = 0;
+        for (int i = 0; i < m; i++) {     // TC: O(M)
+            for (int j = 0; j < n; j++) { // TC: O(N)
+                if (matrix[i][j] == '1') {
+                    height[j] += 1;
+                } else {
+                    height[j] = 0;
+                }
+            }
+            maxArea = Math.max(maxArea, maxAreaHistogramOptimal(height, n)); // TC: O(N), SC: O(N)
+        }
+        return maxArea;
+    }
+
+    /**
+     * Using Monotonic Stack Approach
+     *
+     * TC: O(N) + O(N) + O(N) ~ O(N)
+     * SC: O(N) + O(N) + O(N) ~ O(N)
+     */
+    private int maxAreaHistogramOptimal(int[] height, int n) {
+        int maxRowArea = 0;
+        Deque<Integer> deque = new LinkedList<Integer>();                 // SC: O(N)
+        int[] pse = previousSmallerElementIndexOptimal(height, n, deque); // TC: O(N), SC: O(N)
+        deque.clear();
+        int[] nse = nextSmallerElementIndexOptimal(height, n, deque);     // TC: O(N), SC: O(N)
+        for (int i = 0; i < n; i++) { // TC: O(N)
+            int endIndex = nse[i] - 1;
+            int startIndex = pse[i] + 1;
+            int currentArea = (endIndex - startIndex + 1) * height[i];
+            maxRowArea = Math.max(maxRowArea, currentArea);
+        }
+        return maxRowArea;
+    }
+
+    /**
+     * Using Monotonic Stack Approach
+     *
+     * TC: O(N)
+     * SC: O(N)
+     */
+    private int[] previousSmallerElementIndexOptimal(int[] row, int n, Deque<Integer> deque) {
+        int[] pse = new int[n];
+        for (int i = 0; i < n; i++) {
+            while (!deque.isEmpty() && row[i] <= row[deque.peek()]) {
+                deque.pop();
+            }
+            pse[i] = deque.isEmpty() ? -1 : deque.peek();
+            deque.push(i);
+        }
+        return pse;
+    }
+
+    /**
+     * Using Monotonic Stack Approach
+     *
+     * TC: O(N)
+     * SC: O(N)
+     */
+    private int[] nextSmallerElementIndexOptimal(int[] row, int n, Deque<Integer> deque) {
+        int[] nse = new int[n];
+        for (int i = n - 1; i >= 0; i--) { // TC: O(N)
+            while (!deque.isEmpty() && row[i] <= row[deque.peek()]) {
+                deque.pop();
+            }
+            nse[i] = deque.isEmpty() ? n : deque.peek();
+            deque.push(i);
+        }
+        return nse;
+    }
+
+    /**
+     * Approach I : Using Monotonic Stack Approach
+     *
+     * TC: O(M x 2 x N) ~ O(M x N)
+     * SC: O(N) + O(M x N) ~ O(M x N)
+     */
+    public int maximalRectangleUsingStack(char[][] matrix) {
         int m = matrix.length;
         int n = matrix[0].length;
         int[] height = new int[n]; // SC: O(N)
