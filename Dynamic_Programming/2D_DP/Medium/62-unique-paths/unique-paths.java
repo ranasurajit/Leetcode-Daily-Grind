8e008fc0c6f78bde382f1c1af4a8a4cb41efcb6c
Surajit Rana <surajit1602@gmail.com>
2026-01-12 23:35:41 +0530

    Time: 0 ms (100.00%) | Memory: 42.5 MB (6.35%) - LeetSync

---
+++ Dynamic_Programming/2D_DP/Medium/62-unique-paths/unique-paths.java
@@ -0,0 +1,80 @@
+class Solution {
+    /**
+     * Approach II : Using Memoization Approach
+     *
+     * TC: O(M x N)
+     * SC: O(M x N) + O(M + N)
+     * - O(M x N) - memoization memory
+     * - O(M + N) - recursion stack space
+     *
+     * Accepted (64 / 64 testcases passed)
+     */
+    public int uniquePaths(int m, int n) {
+        int[][] memo = new int[m][n]; // SC: O(M x N)
+        for (int[] mem : memo) {
+            Arrays.fill(mem, -1);
+        }
+        return solveMemoization(m - 1, n - 1, memo);
+    }
+
+    /**
+     * Using Memoization (Top-Down) Approach
+     *
+     * TC: O(M x N)
+     * SC: O(M + N) - recursion stack space
+     */
+    private int solveMemoization(int i, int j, int[][] memo) {
+        // Base Case
+        if (i < 0 || j < 0) {
+            return 0;
+        }
+        if (i == 0 && j == 0) {
+            // when robot reaches cell (0, 0), we get 1 unique path
+            return 1;
+        }
+        // Memoization Check
+        if (memo[i][j] != -1) {
+            return memo[i][j];
+        }
+        // Recursion Calls
+        // here we are moving in opposite direction bottom-right to top-left
+        int topWays = solveMemoization(i, j - 1, memo);
+        int leftWays = solveMemoization(i - 1, j, memo);
+        return memo[i][j] = topWays + leftWays;
+    }
+
+    /**
+     * Approach I : Using Recursion Approach
+     *
+     * TC: O(2 ^ (M + N))
+     * SC: O(M + N)
+     * O(M + N) - recursion stack space
+     *
+     * Time Limit Exceeded (39 / 64 testcases passed)
+     */
+    public int uniquePathsRecursion(int m, int n) {
+        return solveRecursion(m - 1, n - 1);
+    }
+
+    /**
+     * Using Recursion Approach
+     *
+     * TC: O(2 ^ (M + N))
+     * SC: O(M + N) - recursion stack space
+     */
+    private int solveRecursion(int i, int j) {
+        // Base Case
+        if (i < 0 || j < 0) {
+            return 0;
+        }
+        if (i == 0 && j == 0) {
+            // when robot reaches cell (0, 0), we get 1 unique path
+            return 1;
+        }
+        // Recursion Calls
+        // here we are moving in opposite direction bottom-right to top-left
+        int topWays = solveRecursion(i, j - 1);
+        int leftWays = solveRecursion(i - 1, j);
+        return topWays + leftWays;
+    }
+}
