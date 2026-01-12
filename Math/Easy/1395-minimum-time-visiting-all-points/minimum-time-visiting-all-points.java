79b019f6d8d4687b4b4ee02e80b0f36f777a67cf
Surajit Rana <surajit1602@gmail.com>
2026-01-12 10:29:33 +0530

    Time: 1 ms (95.28%) | Memory: 44.9 MB (30.73%) - LeetSync

--- Math/Easy/1395-minimum-time-visiting-all-points/minimum-time-visiting-all-points.java
+++ Math/Easy/1395-minimum-time-visiting-all-points/minimum-time-visiting-all-points.java
@@ -1,11 +1,33 @@
 class Solution {
     /**
-     * Approach : Using Greedy + Math Approach
+     * Approach II : Using Greedy + Math (Optimal) Approach
      *
      * TC: O(N)
      * SC: O(1)
      */
     public int minTimeToVisitAllPoints(int[][] points) {
+        int n = points.length;
+        int[] last = { points[0][0], points[0][1] };
+        int time = 0;
+        /**
+         * to visit in mininum time, we should try to go via diagonal
+         * as much as possible and rest vertically or horizontally
+         */
+        for (int i = 1; i < n; i++) { // TC: O(N)
+            int[] current = points[i];
+            time += Math.max(Math.abs(current[0] - last[0]), Math.abs(current[1] - last[1]));
+            last = current;
+        }
+        return time;
+    }
+
+    /**
+     * Approach I : Using Greedy + Math (Better) Approach
+     *
+     * TC: O(N)
+     * SC: O(1)
+     */
+    public int minTimeToVisitAllPointsBetter(int[][] points) {
         int n = points.length;
         int[] last = { points[0][0], points[0][1] };
         int time = 0;
