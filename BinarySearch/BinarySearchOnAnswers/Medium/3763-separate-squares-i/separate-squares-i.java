5be397efdbd501515ab8c849194c3c31e956e3ab
Surajit Rana <surajit1602@gmail.com>
2026-01-13 11:12:03 +0530

    Time: 115 ms (70.00%) | Memory: 146.1 MB (67.37%) - LeetSync

---
+++ BinarySearch/BinarySearchOnAnswers/Medium/3763-separate-squares-i/separate-squares-i.java
@@ -0,0 +1,59 @@
+class Solution {
+    /**
+     * Approach : Using Binary Search on Answers Approach
+     *
+     * TC: O(N) + O(N x log(N)) ~ O(N x log(N))
+     * SC: O(1)
+     */
+    public double separateSquares(int[][] squares) {
+        int n = squares.length;
+        double low = Double.MAX_VALUE;
+        double high = Double.MIN_VALUE;
+        double highDelta = 0;
+        for (int[] sq : squares) { // TC: O(N)
+            low = Math.min(low, sq[1]);
+            high = Math.max(high, sq[1] + sq[2]);
+        }
+        double delta = 1e-6;
+        // this is a floating pointer binary search
+        while (high - low > delta) { // TC: O(log(N))
+            double mid = low + (high - low) / 2;
+            double[] totalArea = getAreaOfSquares(squares, mid); // TC: O(N)
+            if (totalArea[0] >= totalArea[1]) {
+                // we need to decrease the mid towards low
+                high = mid;
+            } else {
+                // we need to increase the mid towards high
+                low = mid;
+            }
+        }
+        return low;
+    }
+
+    /**
+     * Using Array Simulation + Math Approach
+     *
+     * TC: O(N)
+     * SC: O(1)
+     */
+    private double[] getAreaOfSquares(int[][] squares, double mid) {
+        double belowYAreas = 0d;
+        double aboveYAreas = 0d;
+        for (int[] sq : squares) { // TC: O(N)
+            double yHigh = (double) sq[1] + (double) sq[2];
+            double yLow = (double) sq[1];
+            double currentArea = (double) sq[2] * (double) sq[2];
+            if (mid > yLow && mid < yHigh) {
+                double heightBelow = (mid - yLow);
+                double heightAbove = (yHigh - mid);
+                belowYAreas += heightBelow * sq[2];
+                aboveYAreas += heightAbove * sq[2];
+            } else if (yHigh <= mid) {
+                belowYAreas += currentArea;
+            } else if (yLow >= mid) {
+                aboveYAreas += currentArea;
+            }
+        }
+        return new double[] { belowYAreas, aboveYAreas };
+    }
+}
