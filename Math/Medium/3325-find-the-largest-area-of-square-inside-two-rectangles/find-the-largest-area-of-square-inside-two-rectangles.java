ec8f694c88a3f5598856fba6138c9c4e3492c3ce
Surajit Rana <surajit1602@gmail.com>
2026-01-17 14:36:21 +0530

    Time: 59 ms (77.08%) | Memory: 47.8 MB (43.75%) - LeetSync

--- Math/Medium/3325-find-the-largest-area-of-square-inside-two-rectangles/find-the-largest-area-of-square-inside-two-rectangles.java
+++ Math/Medium/3325-find-the-largest-area-of-square-inside-two-rectangles/find-the-largest-area-of-square-inside-two-rectangles.java
@@ -8,27 +8,17 @@ class Solution {
     public long largestSquareArea(int[][] bottomLeft, int[][] topRight) {
         int n = bottomLeft.length;
         long maxSide = 0L;
-        for (int i = 0; i < n - 1; i++) { // TC: O(N)
+        for (int i = 0; i < n - 1; i++) {     // TC: O(N)
             for (int j = i + 1; j < n; j++) { // TC: O(N)
-                maxSide = Math.max(maxSide, getSquareSide(bottomLeft, topRight, i, j));
+                long leftMax = Math.max(bottomLeft[i][0], bottomLeft[j][0]);
+                long rightMin = Math.min(topRight[i][0], topRight[j][0]);
+                long bottomMax = Math.max(bottomLeft[i][1], bottomLeft[j][1]);
+                long topMin = Math.min(topRight[i][1], topRight[j][1]);
+                long width = rightMin - leftMax;
+                long height = topMin - bottomMax;
+                maxSide = Math.max(maxSide, Math.min(width, height));
             }
         }
         return maxSide * maxSide;
-    }
-
-    /**
-     * Using Enumeration Approach
-     *
-     * TC: O(1)
-     * SC: O(1)
-     */
-    private long getSquareSide(int[][] bottomLeft, int[][] topRight, int i, int j) {
-        long leftMax = Math.max(bottomLeft[i][0], bottomLeft[j][0]);
-        long rightMin = Math.min(topRight[i][0], topRight[j][0]);
-        long bottomMax = Math.max(bottomLeft[i][1], bottomLeft[j][1]);
-        long topMin = Math.min(topRight[i][1], topRight[j][1]);
-        long width = rightMin - leftMax;
-        long height = topMin - bottomMax;
-        return Math.min(width, height);
     }
 }
