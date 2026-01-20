c94be2a38ed56ff08817d2fe468d9fd9253d8461
Surajit Rana <surajit1602@gmail.com>
2026-01-20 14:33:45 +0530

    Time: 1 ms (100.00%) | Memory: 46.9 MB (15.79%) - LeetSync

--- BitManipulation/Easy/3605-construct-the-minimum-bitwise-array-i/construct-the-minimum-bitwise-array-i.java
+++ BitManipulation/Easy/3605-construct-the-minimum-bitwise-array-i/construct-the-minimum-bitwise-array-i.java
@@ -1,11 +1,48 @@
 class Solution {
     /**
+     * Approach II : Using Optimal (Bit-Manipulation) Approach
+     *
+     * TC: O(N)
+     * SC: O(1)
+     */
+    public int[] minBitwiseArray(List<Integer> nums) {
+        int n = nums.size();
+        int[] result = new int[n];
+        for (int i = 0; i < n; i++) { // TC: O(N)
+            result[i] = computeFromNum(nums.get(i)); // TC: O(1)
+        }
+        return result;
+    }
+
+    /**
+     * Using Bit-Manipulation Approach
+     *
+     * TC: O(32) ~ O(1)
+     * SC: O(1)
+     */
+    private int computeFromNum(int num) {
+        int index = 0;
+        for (int i = 0; i < 32; i++) { // TC: O(32)
+            if (i == 0 && ((num >> i) & 1) == 0) {
+                return -1;
+            }
+            if (((num >> i) & 1) == 1) {
+                continue;
+            }
+            index = i;
+            break;
+        }
+        // we need to flip (index - 1)th bit to 0
+        return num - (1 << (index - 1));
+    }
+
+    /**
      * Approach I : Using Brute-Force (Array Simulation) Approach
      *
      * TC: O(N x Max(nums))
      * SC: O(1)
      */
-    public int[] minBitwiseArray(List<Integer> nums) {
+    public int[] minBitwiseArrayBruteForce(List<Integer> nums) {
         int n = nums.size();
         int[] result = new int[n];
         for (int i = 0; i < n; i++) { // TC: O(N)
