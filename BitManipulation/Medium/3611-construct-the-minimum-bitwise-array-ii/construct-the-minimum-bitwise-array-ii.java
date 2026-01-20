eb21a9008b31f589a874b3d900457f76334b3f0b
Surajit Rana <surajit1602@gmail.com>
2026-01-20 14:54:49 +0530

    Time: 1 ms (100.00%) | Memory: 47.3 MB (6.06%) - LeetSync

---
+++ BitManipulation/Medium/3611-construct-the-minimum-bitwise-array-ii/construct-the-minimum-bitwise-array-ii.java
@@ -0,0 +1,78 @@
+class Solution {
+    /**
+     * Approach II : Using Optimal (Bit-Manipulation) Approach
+     **
+     * Intuition:
+     * When computing (x | (x + 1)), adding 1 flips all trailing 1s to 0
+     * and turns the first 0 (from the right) into 1. As a result, all bits
+     * below that first 0-bit become 1 in the OR result.
+     *
+     * To obtain nums[i] while keeping x minimal, we locate the lowest
+     * 0-bit in nums[i] and clear the bit immediately below it.
+     *
+     * If nums[i] is even, no such x exists because (x | (x + 1)) is always odd.
+     *
+     *
+     * TC: O(N)
+     * SC: O(1)
+     */
+    public int[] minBitwiseArray(List<Integer> nums) {
+        int n = nums.size();
+        int[] result = new int[n];
+        for (int i = 0; i < n; i++) { // TC: O(N)
+            result[i] = computeFromNum(nums.get(i)); // TC: O(1)
+        }
+        return result;
+    }
+
+    /**
+     * Using Bit-Manipulation Approach
+     *
+     * TC: O(32) ~ O(1)
+     * SC: O(1)
+     */
+    private int computeFromNum(int num) {
+        int index = 0;
+        for (int i = 0; i < 32; i++) { // TC: O(32)
+            if (i == 0 && ((num >> i) & 1) == 0) {
+                return -1;
+            }
+            if (((num >> i) & 1) == 1) {
+                continue;
+            }
+            index = i;
+            break;
+        }
+        // we need to flip (index - 1)th bit to 0 i.e we can subtract 2 ^ (index - 1) from num
+        return num - (1 << (index - 1));
+    }
+
+    /**
+     * Approach I : Using Brute-Force (Array Simulation) Approach
+     *
+     * TC: O(N x Max(nums))
+     * SC: O(1)
+     */
+    public int[] minBitwiseArrayBruteForce(List<Integer> nums) {
+        int n = nums.size();
+        int[] result = new int[n];
+        for (int i = 0; i < n; i++) { // TC: O(N)
+            /**
+             * To satisfy the condition of ans[i] OR ans[i] + 1 = nums.get(i),
+             * ans[i] cannot exceed nums.get(i)
+             */
+            boolean found = false;
+            for (int j = 0; j < nums.get(i); j++) { // TC: O(Max(nums))
+                if ((j | (j + 1)) == nums.get(i)) {
+                    result[i] = j;
+                    found = true;
+                    break;
+                }
+            }
+            if (!found) {
+                result[i] = -1;
+            }
+        }
+        return result;
+    }
+}
