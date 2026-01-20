5b8854580684225b08a93596eb333b6f12d9e7ac
Surajit Rana <surajit1602@gmail.com>
2026-01-20 05:58:03 +0530

    Time: 28 ms (11.40%) | Memory: 47 MB (15.79%) - LeetSync

---
+++ BitManipulation/Easy/3605-construct-the-minimum-bitwise-array-i/construct-the-minimum-bitwise-array-i.java
@@ -0,0 +1,30 @@
+class Solution {
+    /**
+     * Approach I : Using Brute-Force (Array Simulation) Approach
+     *
+     * TC: O(N²)
+     * SC: O(1)
+     */
+    public int[] minBitwiseArray(List<Integer> nums) {
+        int n = nums.size();
+        int[] result = new int[n];
+        for (int i = 0; i < n; i++) { // TC: O(N)
+            /**
+             * To satisfy the condition of ans[i] OR ans[i] + 1 = nums.get(i),
+             * ans[i] cannot exceed nums.get(i)
+             */
+            boolean found = false;
+            for (int j = 0; j <= nums.get(i); j++) { // TC: O(N)
+                if ((j | (j + 1)) == nums.get(i)) {
+                    result[i] = j;
+                    found = true;
+                    break;
+                }
+                if (!found) {
+                    result[i] = -1;
+                }
+            }
+        }
+        return result;
+    }
+}
