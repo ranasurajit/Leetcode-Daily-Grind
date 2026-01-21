ecc85bf3808b506ad89ebcb7b89857709f6cdf79
Surajit Rana <surajit1602@gmail.com>
2026-01-22 01:09:36 +0530

    Time: 606 ms (5.01%) | Memory: 42.7 MB (62.53%) - LeetSync

---
+++ Backtracking/Medium/1418-fair-distribution-of-cookies/fair-distribution-of-cookies.java
@@ -0,0 +1,47 @@
+class Solution {
+    private int n;
+    private int k;
+    private int result = Integer.MAX_VALUE;
+
+    /**
+     * Approach : Using Backtracking Approach
+     *
+     * TC: O(K ^ N)
+     * SC: O(N) + O(N) ~ O(N)
+     * - O(N) - dist array memory
+     * - O(N) - recursion stack
+     */
+    public int distributeCookies(int[] cookies, int k) {
+        this.n = cookies.length;
+        this.k = k;
+        int[] dist = new int[k]; // SC: O(K)
+        backtrack(0, cookies, dist);
+        return result;
+    }
+
+    /**
+     * Using Backtracking Approach
+     *
+     * TC: O(K ^ N)
+     * SC: O(N) - recursion stack
+     */
+    private void backtrack(int idx, int[] cookies, int[] dist) {
+        // Base Case
+        if (idx == n) {
+            // cookies bag exhaused
+            int unfairness = 0;
+            for (int i = 0; i < k; i++) { // TC: O(K)
+                unfairness = Math.max(unfairness, dist[i]);
+            }
+            result = Math.min(result, unfairness);
+        }
+        // Recursion Calls
+        for (int j = 0; j < k; j++) { // TC: O(K)
+            if (idx < n) {
+                dist[j] += cookies[idx]; // modify
+                backtrack(idx + 1, cookies, dist); // explore
+                dist[j] -= cookies[idx]; // backtrack
+            }
+        }
+    }
+}
