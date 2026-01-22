b5f078906579acdef5238c811407fe7f6165be44
Surajit Rana <surajit1602@gmail.com>
2026-01-22 08:53:54 +0530

    Time: 26 ms (14.02%) | Memory: 98.1 MB (36.06%) - LeetSync

--- Backtracking/Medium/77-combinations/combinations.java
+++ Backtracking/Medium/77-combinations/combinations.java
@@ -3,6 +3,42 @@ class Solution {
     private List<List<Integer>> result = new ArrayList<List<Integer>>();
 
     /**
+     * Approach II : Using Backtracking Approach
+     *
+     * TC: O(2 ^ N)
+     * SC: O(N) + O(K)
+     * - O(N) - recursion stack
+     * - O(K) - current array memory
+     */
+    public List<List<Integer>> combine(int n, int k) {
+        this.k = k;
+        List<Integer> current = new ArrayList<Integer>(); // SC: O(K)
+        solveBetter(1, n, k, current); // TC: O(2 ^ N), SC: O(N)
+        return result;
+    }
+
+    /**
+     * Using Backtracking Approach
+     *
+     * TC: O(2 ^ N)
+     * SC: O(N) - recursion stack
+     */
+    private void solveBetter(int start, int n, int k, List<Integer> current) {
+        // Base Case
+        if (start > n) {
+            if (k == 0) {
+                result.add(new ArrayList<Integer>(current));
+            }
+            return;
+        }
+        // Recursion Calls
+        current.add(start); // modify
+        solveBetter(start + 1, n, k - 1, current); // explore - pick
+        current.remove(current.size() - 1); // backtrack
+        solveBetter(start + 1, n, k, current); // explore - skip
+    }
+
+    /**
      * Approach I : Using Backtracking Approach
      *
      * TC: O(2 ^ N)
@@ -10,7 +46,7 @@ class Solution {
      * - O(N) - recursion stack
      * - O(2 ^ N) - current array memory
      */
-    public List<List<Integer>> combine(int n, int k) {
+    public List<List<Integer>> combineBruteForce(int n, int k) {
         this.k = k;
         List<Integer> current = new ArrayList<Integer>(); // SC: O(2 ^ N)
         solve(1, n, current); // TC: O(2 ^ N), SC: O(N)
