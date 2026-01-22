41b47c96ac234efdd47deb8b5e9e2edacbcf7adb
Surajit Rana <surajit1602@gmail.com>
2026-01-22 08:10:45 +0530

    Time: 17 ms (56.41%) | Memory: 43 MB (80.13%) - LeetSync

---
+++ Backtracking/Hard/1723-maximum-number-of-achievable-transfer-requests/maximum-number-of-achievable-transfer-requests.java
@@ -0,0 +1,55 @@
+class Solution {
+    private int m;
+    private int n;
+    private int maxTransfers = 0;
+
+    /**
+     * Approach : Using Backtracking Approach
+     *
+     * TC: O(N x 2 ^ M)
+     * SC: O(M + N)
+     * - O(N) - movement array memory
+     * - O(M) - recursion stack
+     */
+    public int maximumRequests(int n, int[][] requests) {
+        m = requests.length;
+        this.n = n;
+        int[] movement = new int[n]; // SC: O(N)
+        backtrack(0, 0, requests, movement); // TC: O(N x 2 ^ M), SC: O(M)
+        return maxTransfers;
+    }
+
+    /**
+     * Using Backtracking Approach
+     *
+     * TC: O(N x 2 ^ M)
+     * SC: O(M) - recursion stack
+     */
+    private void backtrack(int idx, int count, int[][] requests, int[] movement) {
+        // Base Case
+        if (idx == m) {
+            boolean isValid = true;
+            for (int i = 0; i < n; i++) { // TC: O(N)
+                if (movement[i] != 0) {
+                    isValid = false;
+                    break;
+                }
+            }
+            if (isValid) {
+                maxTransfers = Math.max(maxTransfers, count);
+            }
+            return;
+        }
+        // Recursion Calls
+        int from = requests[idx][0];
+        int to = requests[idx][1];
+        // modify
+        movement[from]--;
+        movement[to]++;
+        backtrack(idx + 1, count + 1, requests, movement); // explore - pick
+        // backtrack - undo
+        movement[from]++;
+        movement[to]--;
+        backtrack(idx + 1, count, requests, movement); // explore - skip
+    }
+}
