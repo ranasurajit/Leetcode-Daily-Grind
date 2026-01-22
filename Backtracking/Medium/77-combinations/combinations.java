94c29436964d746eb80a48027275ba7e354e6227
Surajit Rana <surajit1602@gmail.com>
2026-01-22 08:44:56 +0530

    Time: 28 ms (10.64%) | Memory: 98.1 MB (56.25%) - LeetSync

--- Backtracking/Medium/77-combinations/combinations.java
+++ Backtracking/Medium/77-combinations/combinations.java
@@ -1,8 +1,9 @@
 class Solution {
     private int k;
+    private List<List<Integer>> result = new ArrayList<List<Integer>>();
 
     /**
-     * Approach : Using Backtracking Approach
+     * Approach I : Using Backtracking Approach
      *
      * TC: O(2 ^ N)
      * SC: O(N) + O(2 ^ N)
@@ -11,9 +12,8 @@ class Solution {
      */
     public List<List<Integer>> combine(int n, int k) {
         this.k = k;
-        List<List<Integer>> result = new ArrayList<List<Integer>>();
         List<Integer> current = new ArrayList<Integer>(); // SC: O(2 ^ N)
-        solve(1, n, current, result); // TC: O(2 ^ N), SC: O(N)
+        solve(1, n, current); // TC: O(2 ^ N), SC: O(N)
         return result;
     }
 
@@ -23,7 +23,7 @@ class Solution {
      * TC: O(2 ^ N)
      * SC: O(N) - recursion stack
      */
-    private void solve(int i, int n, List<Integer> current, List<List<Integer>> result) {
+    private void solve(int i, int n, List<Integer> current) {
         // Base Case
         if (i > n) {
             if (current.size() == k) {
@@ -33,8 +33,8 @@ class Solution {
         }
         // Recursion
         current.add(i); // modify - pick
-        solve(i + 1, n, current, result); // explore - pick
+        solve(i + 1, n, current); // explore - pick
         current.remove(current.size() - 1); // backtrack
-        solve(i + 1, n, current, result); // explore - skip
+        solve(i + 1, n, current); // explore - skip
     }
 }
