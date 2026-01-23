ddea8692d1cce435d2fca37baca1e520c090160c
Surajit Rana <surajit1602@gmail.com>
2026-01-23 21:48:02 +0530

    Time: 0 ms (100.00%) | Memory: 45.8 MB (15.56%) - LeetSync

--- Backtracking/Medium/46-permutations/permutations.java
+++ Backtracking/Medium/46-permutations/permutations.java
@@ -1,11 +1,54 @@
 class Solution {
     /**
-     * Approach : Using Backtracking Approach
+     * Approach II : Using Backtracking (Template 1) Approach
      *
      * TC: O(N x N!)
      * SC: O(N!)
      */
     public List<List<Integer>> permute(int[] nums) {
+        int n = nums.length;
+        List<List<Integer>> result = new ArrayList<List<Integer>>();
+        solveBetter(0, n, nums, result);
+        return result;
+    }
+
+    /**
+     * Using Backtracking Approach
+     *
+     * TC: O(N x N!)
+     * SC: O(N!)
+     */
+    private void solveBetter(int idx, int n, int[] nums, List<List<Integer>> result) {
+        // Base Case
+        if (idx == n) {
+            List<Integer> current = new ArrayList<Integer>();
+            for (int i = 0; i < n; i++) { // TC: O(N)
+                current.add(nums[i]);
+            }
+            result.add(current);
+            return;
+        }
+        // Recursion Calls
+        for (int i = idx; i < n; i++) { // TC: O(N)
+            swap(nums, idx, i); // modify
+            solveBetter(idx + 1, n, nums, result); // explore
+            swap(nums, idx, i); // backtrack
+        }
+    }
+
+    private void swap(int[] nums, int a, int b) {
+        int temp = nums[a];
+        nums[a] = nums[b];
+        nums[b] = temp;
+    }
+
+    /**
+     * Approach I : Using Backtracking (Template 1) Approach
+     *
+     * TC: O(N x N!)
+     * SC: O(N!)
+     */
+    public List<List<Integer>> permuteTemplate1(int[] nums) {
         int n = nums.length;
         List<List<Integer>> result = new ArrayList<List<Integer>>();
         List<Integer> current = new ArrayList<Integer>();
