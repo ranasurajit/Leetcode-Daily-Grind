36e99e3432d7c863907c9a60971609afac9f6267
Surajit Rana <surajit1602@gmail.com>
2026-01-25 13:07:44 +0530

    Time: 9 ms (26.64%) | Memory: 46.9 MB (43.99%) - LeetSync

--- SlidingWindow/FixedSize/Easy/2112-minimum-difference-between-highest-and-lowest-of-k-scores/minimum-difference-between-highest-and-lowest-of-k-scores.java
+++ SlidingWindow/FixedSize/Easy/2112-minimum-difference-between-highest-and-lowest-of-k-scores/minimum-difference-between-highest-and-lowest-of-k-scores.java
@@ -1,11 +1,41 @@
 class Solution {
     /**
-     * Approach I : Using Sorting + Array Simulation Approach
+     * Approach II : Using Sorting + Sliding Window (Fixed Size) Approach
      *
      * TC: O((N - K) x K) + O(N x log(N)) ~ O(N x (K + log(N)))
      * SC: O(1)
      */
     public int minimumDifference(int[] nums, int k) {
+        int n = nums.length;
+        if (n == 1) {
+            return 0;
+        }
+        // sorting the nums so that we can compare it in k windows
+        Arrays.sort(nums); // TC: O(N x log(N))
+        int minDiff = Integer.MAX_VALUE;
+        int i = 0; // start pointer of sliding window
+        int j = 0; // end pointer of sliding window
+        while (j < n) {
+            if (j - i + 1 == k) {
+                minDiff = Math.min(minDiff, nums[j] - nums[i]);
+                /**
+                 * remove computation from index 'i' for next sliding window 
+                 * and slide to next window
+                 */
+                i++;
+            }
+            j++;
+        }
+        return minDiff;
+    }
+
+    /**
+     * Approach I : Using Sorting + Array Simulation Approach
+     *
+     * TC: O((N - K) x K) + O(N x log(N)) ~ O(N x (K + log(N)))
+     * SC: O(1)
+     */
+    public int minimumDifferenceUsingSortingSimulation(int[] nums, int k) {
         int n = nums.length;
         if (n == 1) {
             return 0;
