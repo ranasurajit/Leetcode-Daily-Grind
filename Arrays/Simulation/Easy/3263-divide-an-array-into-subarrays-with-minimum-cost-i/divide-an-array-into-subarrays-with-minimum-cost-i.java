139348d54186c245530bc001abf7270b22bda076
Surajit Rana <surajit1602@gmail.com>
2026-02-01 11:53:24 +0530

    Time: 1 ms (99.20%) | Memory: 45.3 MB (72.80%) - LeetSync

---
+++ Arrays/Simulation/Easy/3263-divide-an-array-into-subarrays-with-minimum-cost-i/divide-an-array-into-subarrays-with-minimum-cost-i.java
@@ -0,0 +1,23 @@
+class Solution {
+    /**
+     * Approach : Using Array Simulation Approach
+     *
+     * TC: O(N²)
+     * SC: O(1)
+     */
+    public int minimumCost(int[] nums) {
+        int n = nums.length;
+        int minCost = Integer.MAX_VALUE;
+        /*
+         * we need not think about index of the 1st partition and cost as
+         * it will start from index 0, so cost = nums[0]
+         */
+        for (int j = 1; j < n - 1; j++) {     // TC: O(N)
+            for (int k = j + 1; k < n; k++) { // TC: O(N)
+                int currentCost = nums[0] + nums[j] + nums[k];
+                minCost = Math.min(minCost, currentCost);
+            }
+        }
+        return minCost;
+    }
+}
