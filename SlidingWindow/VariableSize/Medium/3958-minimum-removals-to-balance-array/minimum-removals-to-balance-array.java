57750dbd579a8e4e1c634f87842358132c3343fe
Surajit Rana <surajit1602@gmail.com>
2026-02-06 18:38:38 +0530

    Time: 27 ms (14.06%) | Memory: 86.2 MB (60.24%) - LeetSync

---
+++ SlidingWindow/VariableSize/Medium/3958-minimum-removals-to-balance-array/minimum-removals-to-balance-array.java
@@ -0,0 +1,32 @@
+class Solution {
+    /**
+     * Approach : Using Sorting + Sliding Window (Variable Size) Approach
+     *
+     * TC: O(N x log(N)) + O(N) ~ O(N x log(N))
+     * SC: O(1)
+     */
+    public int minRemoval(int[] nums, int k) {
+        int n = nums.length;
+        if (n == 1) {
+            // array of size 1 is balanced
+            return 0;
+        }
+        Arrays.sort(nums); // TC: O(N x log(N))
+        int i = 0; // start pointer
+        int j = 1; // end pointer as array of size 1 is always balanced
+        int maxWindowSize = 1;
+        while (j < n) {    // TC: O(N)
+            while (i < n && nums[j] > (long) nums[i] * k) {
+                // we need to shrink the ith index
+                i++;
+            }
+            maxWindowSize = Math.max(maxWindowSize, j - i + 1);
+            j++;
+        }
+        /**
+         * the maximum window size of 'maxWindowSize' satisfies the 
+         * Balanced Array condition, so minimum removals = n - maxWindowSize
+         */
+        return n - maxWindowSize;
+    }
+}
