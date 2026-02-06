1e624ac4b0409c99c0c0b90f8632e73f0d86cb6a
Surajit Rana <surajit1602@gmail.com>
2026-02-06 15:15:53 +0530

    Time: 24 ms (93.17%) | Memory: 86.2 MB (36.95%) - LeetSync

---
+++ Arrays/Sorting/Medium/3958-minimum-removals-to-balance-array/minimum-removals-to-balance-array.java
@@ -0,0 +1,28 @@
+class Solution {
+    /**
+     * Approach : Using Sorting + Two Pointers Approach
+     *
+     * TC: O(N x log(N)) + O(N) ~ O(N x log(N))
+     * SC: O(1)
+     */
+    public int minRemoval(int[] nums, int k) {
+        int n = nums.length;
+        if (n == 1) {
+            // array of size 1 is balanced
+            return 0;
+        }
+        Arrays.sort(nums); // TC: O(N x log(N))
+        int p = 0; // start pointer
+        int q = 1; // end pointer
+        int maxWindowSize = 1;
+        while (q < n) { // TC: O(N)
+            while (q < n && nums[q] > (long) k * nums[p]) {
+                // increase start index
+                p++;
+            }
+            maxWindowSize = Math.max(maxWindowSize, q - p + 1);
+            q++;
+        }
+        return n - maxWindowSize;
+    }
+}
