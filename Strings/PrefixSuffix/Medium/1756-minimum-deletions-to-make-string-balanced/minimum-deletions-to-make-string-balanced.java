a67e1b496c0853fe48e2b9fd4ccd59d2b4c877b9
Surajit Rana <surajit1602@gmail.com>
2026-02-07 12:03:08 +0530

    Time: 62 ms (16.78%) | Memory: 48.1 MB (16.28%) - LeetSync

---
+++ Strings/PrefixSuffix/Medium/1756-minimum-deletions-to-make-string-balanced/minimum-deletions-to-make-string-balanced.java
@@ -0,0 +1,43 @@
+class Solution {
+    /**
+     * Approach : Using Array Prefix-Suffix Approach
+     *
+     * TC: O(N) + O(N) + O(N) ~ O(N)
+     * SC: O(N) + O(N) ~ O(N)
+     */
+    public int minimumDeletions(String s) {
+        int n = s.length();
+        if (n <= 1) {
+            return 0;
+        }
+        /**
+         * we need to make String balanced such that 
+         * leftPart has all a's and rightPart has all b's
+         * we will count number of bad 'b's on the left
+         * and also count of bad 'a's from any index to 
+         * its right
+         */
+        int[] leftBad = new int[n]; // SC: O(N)
+        int[] rightBad = new int[n]; // SC: O(N)
+        leftBad[0] = s.charAt(0) == 'b' ? 1 : 0;
+        int countBs = s.charAt(0) == 'b' ? 1 : 0;
+        for (int i = 1; i < n; i++) { // TC: O(N)
+            leftBad[i] = leftBad[i - 1] + (s.charAt(i) == 'b' ? 1 : 0);
+            countBs += (s.charAt(i) == 'b' ? 1 : 0);
+        }
+        rightBad[n - 1] = 0;
+        int countAs = s.charAt(0) == 'a' ? 1 : 0;
+        for (int i = n - 2; i >= 0; i--) { // TC: O(N)
+            rightBad[i] = rightBad[i + 1] + (s.charAt(i + 1) == 'a' ? 1 : 0);
+            countAs += (s.charAt(i) == 'a' ? 1 : 0);
+        }
+        if (countAs == 0 || countBs == 0) {
+            return 0;
+        }
+        int minDeletion = Math.min(leftBad[n - 1], rightBad[0]);
+        for (int i = 0; i < n; i++) { // TC: O(N)
+            minDeletion = Math.min(minDeletion, leftBad[i] + rightBad[i]);
+        }
+        return minDeletion;
+    }
+}
