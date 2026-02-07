310908edc8849c5875ad5001652582a9a6317df3
Surajit Rana <surajit1602@gmail.com>
2026-02-07 14:01:32 +0530

    Time: 22 ms (67.11%) | Memory: 47.9 MB (45.39%) - LeetSync

--- Strings/PrefixSuffix/Medium/1756-minimum-deletions-to-make-string-balanced/minimum-deletions-to-make-string-balanced.java
+++ Strings/PrefixSuffix/Medium/1756-minimum-deletions-to-make-string-balanced/minimum-deletions-to-make-string-balanced.java
@@ -1,11 +1,33 @@
 class Solution {
     /**
-     * Approach : Using Array Prefix-Suffix Approach
+     * Approach II : Using Array Prefix-Suffix (Optimal) Approach
+     *
+     * TC: O(N)
+     * SC: O(1)
+     */
+    public int minimumDeletions(String s) {
+        int n = s.length();
+        int countB = 0;
+        int dp = 0; // minimum deletions needed to keep String s balanced till index 'i'
+        for (int i = 0; i < n; i++) { // TC: O(N)
+            char ch = s.charAt(i);
+            if (ch == 'b') {
+                countB++;
+            } else {
+                // ch = a
+                dp = Math.min(1 + dp, countB);
+            }
+        }
+        return dp;
+    }
+
+    /**
+     * Approach I : Using Array Prefix-Suffix Approach
      *
      * TC: O(N) + O(N) + O(N) ~ O(N)
      * SC: O(N) + O(N) ~ O(N)
      */
-    public int minimumDeletions(String s) {
+    public int minimumDeletionsPrefixArray(String s) {
         int n = s.length();
         if (n <= 1) {
             return 0;
@@ -17,7 +39,7 @@ class Solution {
          * and also count of bad 'a's from any index to 
          * its right
          */
-        int[] leftBad = new int[n]; // SC: O(N)
+        int[] leftBad = new int[n];  // SC: O(N)
         int[] rightBad = new int[n]; // SC: O(N)
         leftBad[0] = s.charAt(0) == 'b' ? 1 : 0;
         for (int i = 1; i < n; i++) { // TC: O(N)
