4e57ac25d0792550fb9e5acd030e7a64171c5607
Surajit Rana <surajit1602@gmail.com>
2026-02-08 11:50:23 +0530

    Time: 0 ms (100.00%) | Memory: 45.8 MB (38.96%) - LeetSync

--- BinaryTrees/DFS_Postorder_CollectInfo/Easy/110-balanced-binary-tree/balanced-binary-tree.java
+++ BinaryTrees/DFS_Postorder_CollectInfo/Easy/110-balanced-binary-tree/balanced-binary-tree.java
@@ -15,12 +15,57 @@
  */
 class Solution {
     /**
-     * Approach : Using Post-order Traversal Approach
+     * Approach II : Using Post-order Traversal Approach
      *
      * TC: O(N) - every node is visited once
      * SC: O(N) - recursion stack
      */
     public boolean isBalanced(TreeNode root) {
+        if (dfsTreeOptimal(root) == -1) {
+            // returns unbalanced
+            return false;
+        }
+        return true;
+    }
+
+    /**
+     * Using Post-order Traversal Approach
+     * To concise the below approach we will
+     * return actual node depth/height only if
+     * it is balanced and all invalid cases
+     * will be returned with -1
+     *
+     * TC: O(N) - every node is visited once
+     * SC: O(N) - recursion stack
+     */
+    private int dfsTreeOptimal(TreeNode root) {
+        // Base Case
+        if (root == null) {
+            return 0;
+        }
+        // Recursion Calls
+        int left = dfsTreeOptimal(root.left);
+        int right = dfsTreeOptimal(root.right);
+        if (Math.abs(left - right) > 1) {
+            // not balanced
+            return -1;
+        }
+        if (left == -1) {
+            return -1;
+        }
+        if (right == -1) {
+            return -1;
+        }
+        return 1 + Math.max(left, right);
+    }
+
+    /**
+     * Approach I : Using Post-order Traversal Approach
+     *
+     * TC: O(N) - every node is visited once
+     * SC: O(N) - recursion stack
+     */
+    public boolean isBalancedDFSTreeApproach(TreeNode root) {
         NodeInfo nodeInfo = dfsTree(root); // TC: O(N), SC: O(N)
         return nodeInfo.balanced;
     }
@@ -37,6 +82,7 @@ class Solution {
             // null node has height/depth 0
             return new NodeInfo(0, true);
         }
+        // Recursion Calls
         NodeInfo left = dfsTree(node.left);
         NodeInfo right = dfsTree(node.right);
         // check if TreeNode 'node' is balanced
