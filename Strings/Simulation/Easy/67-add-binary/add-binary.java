e1b7aeed7b0fa929c620330ce84cdd29274f8892
Surajit Rana <surajit1602@gmail.com>
2026-02-15 14:43:00 +0530

    Time: 2 ms (49.49%) | Memory: 43.4 MB (84.70%) - LeetSync

--- Strings/Simulation/Easy/67-add-binary/add-binary.java
+++ Strings/Simulation/Easy/67-add-binary/add-binary.java
@@ -34,9 +34,6 @@ class Solution {
             sb.append(carry);
         }
         sb = sb.reverse();
-        while (sb.length() > 1 && sb.charAt(0) == '0') {
-            sb.deleteCharAt(0);
-        }
         return sb.toString();
     }
 
