616b0b4546a6938c8d4a240491f2cb4bce12759a
Surajit Rana <surajit1602@gmail.com>
2026-02-15 14:46:08 +0530

    Time: 1 ms (99.65%) | Memory: 43.6 MB (70.73%) - LeetSync

--- Strings/Simulation/Easy/67-add-binary/add-binary.java
+++ Strings/Simulation/Easy/67-add-binary/add-binary.java
@@ -1,11 +1,39 @@
 class Solution {
     /**
-     * Approach : Using String Simulation Approach
+     * Approach : Using String Simulation (Concise) Approach
      *
      * TC: O(N + M)
      * SC: O(N + M)
      */
     public String addBinary(String a, String b) {
+        StringBuilder sb = new StringBuilder(); // SC: O(N + M)
+        int p = a.length() - 1;
+        int q = b.length() - 1;
+        int carry = 0;
+        while (p >= 0 || q >= 0 || carry > 0) { // TC: O(N + M)
+            int numA = 0;
+            int numB = 0;
+            if (p >= 0) {
+                numA = a.charAt(p) - '0';
+            }
+            if (q >= 0) {
+                numB = b.charAt(q) - '0';
+            }
+            int result = carry + numA + numB;
+            carry = computeAdd(result, sb, carry);
+            p--;
+            q--;
+        }
+        return sb.reverse().toString();
+    }
+
+    /**
+     * Approach I : Using String Simulation Approach
+     *
+     * TC: O(N + M)
+     * SC: O(N + M)
+     */
+    public String addBinaryOptimal(String a, String b) {
         StringBuilder sb = new StringBuilder(); // SC: O(N + M)
         int p = a.length() - 1;
         int q = b.length() - 1;
@@ -33,8 +61,7 @@ class Solution {
         if (carry > 0) {
             sb.append(carry);
         }
-        sb = sb.reverse();
-        return sb.toString();
+        return sb.reverse().toString();
     }
 
     /**
