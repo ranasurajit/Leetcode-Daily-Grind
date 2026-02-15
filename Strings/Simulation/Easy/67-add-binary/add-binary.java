61108a0848b2e93b03c4e7e559ce895ecd95b96f
Surajit Rana <surajit1602@gmail.com>
2026-02-15 14:34:21 +0530

    Time: 3 ms (17.52%) | Memory: 43.9 MB (25.29%) - LeetSync

---
+++ Strings/Simulation/Easy/67-add-binary/add-binary.java
@@ -0,0 +1,65 @@
+class Solution {
+    /**
+     * Approach : Using String Simulation Approach
+     *
+     * TC: O(N + M)
+     * SC: O(N + M)
+     */
+    public String addBinary(String a, String b) {
+        StringBuilder sb = new StringBuilder(); // SC: O(N + M)
+        int p = a.length() - 1;
+        int q = b.length() - 1;
+        int carry = 0;
+        while (p >= 0 && q >= 0) { // TC: O(N + M)
+            int numA = a.charAt(p) - '0';
+            int numB = b.charAt(q) - '0';
+            int result = carry + numA + numB;
+            carry = computeAdd(result, sb, carry);
+            p--;
+            q--;
+        }
+        while (p >= 0) {
+            int numA = a.charAt(p) - '0';
+            int result = carry + numA;
+            carry = computeAdd(result, sb, carry);
+            p--;
+        }
+        while (q >= 0) {
+            int numB = b.charAt(q) - '0';
+            int result = carry + numB;
+            carry = computeAdd(result, sb, carry);
+            q--;
+        }
+        if (carry > 0) {
+            sb.append(carry);
+        }
+        sb = sb.reverse();
+        while (sb.length() > 1 && sb.charAt(0) == '0') {
+            sb.deleteCharAt(0);
+        }
+        return sb.toString();
+    }
+
+    /**
+     * Using String Simulation Approach
+     *
+     * TC: O(1)
+     * SC: O(1)
+     */
+    private int computeAdd(int add, StringBuilder sb, int carry) {
+        if (add > 2) {
+            sb.append('1');
+            carry = 1;
+        } else if (add > 1) {
+            sb.append('0');
+            carry = 1;
+        } else if (add > 0) {
+            sb.append('1');
+            carry = 0;
+        } else {
+            sb.append('0');
+            carry = 0;
+        }
+        return carry;
+    }
+}
