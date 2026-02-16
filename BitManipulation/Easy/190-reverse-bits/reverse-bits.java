15d3e90ac78e8f91dd93ae9f02e513ed35922194
Surajit Rana <surajit1602@gmail.com>
2026-02-16 13:17:17 +0530

    Time: 0 ms (100.00%) | Memory: 42.5 MB (52.75%) - LeetSync

--- BitManipulation/Easy/190-reverse-bits/reverse-bits.java
+++ BitManipulation/Easy/190-reverse-bits/reverse-bits.java
@@ -1,6 +1,6 @@
 class Solution {
     /**
-     * Approach II : Using Bit-Manipulation (Cleaner) Approach
+     * Approach II : Using Bit-Manipulation Approach
      *
      * TC: O(32) ~ O(1)
      * SC: O(1)
@@ -8,26 +8,26 @@ class Solution {
     public int reverseBits(int n) {
         int result = 0;
         for (int i = 31; i >= 0; i--) { // TC: O(32)
-            // extract last bit and make empty space to right of result and make OR operation
-            result = ((result << 1) | (n & 1));
-            // finally right shift n
-            n = (n >> 1);
+            int currentBit = ((n >> i) & 1); // get the current bit
+            // multiply current bit with reversed (index)
+            result += (1 << (31 - i)) * currentBit;
         }
         return result;
     }
 
     /**
-     * Approach I : Using Bit-Manipulation Approach
+     * Approach I : Using Bit-Manipulation (Cleaner) Approach
      *
      * TC: O(32) ~ O(1)
      * SC: O(1)
      */
-    public int reverseBitsApproachI(int n) {
+    public int reverseBitsOptimal(int n) {
         int result = 0;
         for (int i = 31; i >= 0; i--) { // TC: O(32)
-            int currentBit = ((n >> i) & 1); // get the current bit
-            // multiply current bit with reversed (index)
-            result += (1 << (31 - i)) * currentBit;
+            // extract last bit and make empty space to right of result and make OR operation
+            result = ((result << 1) | (n & 1));
+            // finally right shift n
+            n = (n >> 1);
         }
         return result;
     }
