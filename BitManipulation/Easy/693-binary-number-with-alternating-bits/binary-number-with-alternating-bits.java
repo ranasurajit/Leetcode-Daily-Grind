4d39b2591885212f18cac074e2c7c6bfae1b9d94
Surajit Rana <surajit1602@gmail.com>
2026-02-18 12:27:00 +0530

    Time: 0 ms (100.00%) | Memory: 42.3 MB (53.63%) - LeetSync

--- BitManipulation/Easy/693-binary-number-with-alternating-bits/binary-number-with-alternating-bits.java
+++ BitManipulation/Easy/693-binary-number-with-alternating-bits/binary-number-with-alternating-bits.java
@@ -1,11 +1,24 @@
 class Solution {
     /**
+     * Approach II : Using Bit-Manipulation Approach
+     *
+     * TC: O(1)
+     * SC: O(1)
+     */
+    public boolean hasAlternatingBits(int n) {
+        // we need to check if n has ones if we do (n) ^ (n >> 1)
+        int x = (n ^ (n >> 1));
+        // if x has all 1111...1's so x & (x + 1) will certainly return 0
+        return (x & (x + 1)) == 0;
+    }
+
+    /**
      * Approach I : Using Bit-Manipulation Approach
      *
      * TC: O(32) ~ O(1)
      * SC: O(1)
      */
-    public boolean hasAlternatingBits(int n) {
+    public boolean hasAlternatingBitsApproachI(int n) {
         int i = 31;
         while (((n >> i) & 1) != 1) { // TC: O(N1)
             i--;
