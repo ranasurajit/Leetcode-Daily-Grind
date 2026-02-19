9d1a39dc5bc6d64162dc4281ab42c2d9850c0938
Surajit Rana <surajit1602@gmail.com>
2026-02-19 12:08:20 +0530

    Time: 8 ms (96.20%) | Memory: 46.5 MB (56.68%) - LeetSync

--- TwoPointers/Easy/696-count-binary-substrings/count-binary-substrings.java
+++ TwoPointers/Easy/696-count-binary-substrings/count-binary-substrings.java
@@ -1,13 +1,40 @@
 class Solution {
     /**
-     * Approach : Using Two Pointers Approach
+     * Approach II : Using Two Pointers (Space Optimized) Approach
+     *
+     * TC: O(N)
+     * SC: O(1)
+     */
+    public int countBinarySubstrings(String s) {
+        int n = s.length();
+        int i = 0; // start pointer
+        int j = 0; // end pointer
+        int prevGroupLength = -1;
+        int count = 0;
+        while (j < n) { // TC: O(N)
+            char ch = s.charAt(j);
+            while (j < n && s.charAt(j) == ch) {
+                j++;
+            }
+            int currentGroupLength = j - i;
+            if (prevGroupLength != -1) {
+                count += Math.min(prevGroupLength, currentGroupLength);
+            }
+            i = j;
+            prevGroupLength = currentGroupLength;
+        }
+        return count;
+    }
+
+    /**
+     * Approach I : Using Two Pointers Approach
      *
      * TC: O(N) + O(K) ~ O(N)
      * SC: O(K) ~ O(N)
      *
      * K ~ N, as K in worst case is N if maximum group size of 1s or 0s is 1
      */
-    public int countBinarySubstrings(String s) {
+    public int countBinarySubstringsWithExtraSpace(String s) {
         int n = s.length();
         List<Integer> groups = new ArrayList<Integer>(); // SC: O(K)
         int i = 0; // start pointer
@@ -17,10 +44,8 @@ class Solution {
             while (j < n && s.charAt(j) == ch) {
                 j++;
             }
-            j--;
-            groups.add(j - i + 1);
-            i = j + 1;
-            j++;
+            groups.add(j - i);
+            i = j;
         }
         int count = 0;
         for (i = 1; i < groups.size(); i++) { // TC: O(K)
