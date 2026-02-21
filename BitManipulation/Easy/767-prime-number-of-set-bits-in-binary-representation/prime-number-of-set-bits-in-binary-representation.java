bffcb84b8aae757eb681264b27aa4dfb4dfb88c7
Surajit Rana <surajit1602@gmail.com>
2026-02-21 14:02:59 +0530

    Time: 25 ms (28.82%) | Memory: 42.3 MB (79.37%) - LeetSync

--- BitManipulation/Easy/767-prime-number-of-set-bits-in-binary-representation/prime-number-of-set-bits-in-binary-representation.java
+++ BitManipulation/Easy/767-prime-number-of-set-bits-in-binary-representation/prime-number-of-set-bits-in-binary-representation.java
@@ -1,13 +1,71 @@
 class Solution {
+    private int[] sieve;
     /**
-     * Approach I : Using Bit-Manipulation + Math Approach
+     * Approach II : Using Bit-Manipulation + Sieve of Eratosthenes Approach
+     *
+     * TC: O(R) + O(32) + O(32 x log(log(32))) ~ O(R)
+     * SC: O(32) ~ O(1)
+     *
+     * where R = right - left + 1 ~ (Max - 10^4 as per constraint)
+     */
+    public int countPrimeSetBits(int left, int right) {
+        // we need to compute Sieve of primes numbers from 1 to 32
+        sieve = new int[33]; // TC: O(32)
+        computePrimeSieve(); // TC: O(32 x log(log(32)))
+        int count = 0;
+        for (int i = left; i <= right; i++) { // TC: O(R)
+            if (hasPrimeSetBitsOptimal(i)) {  // TC: O(1), SC: O(1)
+                count++;
+            }
+        }
+        return count;
+    }
+
+    /**
+     * Using Sieve of Eratosthenes Approach
+     *
+     * TC: O(32 x log(log(32)))
+     * SC: O(1)
+     */
+    private void computePrimeSieve() {
+        Arrays.fill(sieve, 1);
+        sieve[0] = 0;
+        sieve[1] = 0;
+        for (int i = 2; i * i <= 32; i++) { // TC: O(Sqrt(32))
+            if (sieve[i] == 1) {
+                for (int j = i * i; j <= 32; j += i) {
+                    sieve[j] = 0;
+                }
+            }
+        }
+    }
+
+    /**
+     * Using Math + Bit-Manipulation Approach
+     *
+     * TC: O(32) ~ O(1)
+     * SC: O(1)
+     */
+    private boolean hasPrimeSetBitsOptimal(int num) {
+        int countSetBits = 0;
+        for (int i = 31; i >= 0; i--) { // TC: O(32)
+            int bitValue = ((num >> i) & 1);
+            if (bitValue == 1) {
+                countSetBits++;
+            }
+        }
+        return sieve[countSetBits] == 1;
+    }
+
+    /**
+     * Approach I : Using Brute-Force (Bit-Manipulation + Math) Approach
      *
      * TC: O(R)
      * SC: O(1)
      *
      * where R = right - left + 1 ~ (Max - 10^4 as per constraint)
      */
-    public int countPrimeSetBits(int left, int right) {
+    public int countPrimeSetBitsBruteForce(int left, int right) {
         int count = 0;
         for (int i = left; i <= right; i++) { // TC: O(R)
             if (hasPrimeSetBits(i)) { // TC: O(1), SC: O(1)
