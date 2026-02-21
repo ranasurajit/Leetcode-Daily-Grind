a64e8fadfbe134bcc434c11b49829c454beafa59
Surajit Rana <surajit1602@gmail.com>
2026-02-21 13:45:32 +0530

    Time: 27 ms (26.96%) | Memory: 42.1 MB (90.65%) - LeetSync

---
+++ BitManipulation/Easy/767-prime-number-of-set-bits-in-binary-representation/prime-number-of-set-bits-in-binary-representation.java
@@ -0,0 +1,55 @@
+class Solution {
+    /**
+     * Approach I : Using Bit-Manipulation + Math Approach
+     *
+     * TC: O(R)
+     * SC: O(1)
+     *
+     * where R = right - left + 1 ~ (Max - 10^4 as per constraint)
+     */
+    public int countPrimeSetBits(int left, int right) {
+        int count = 0;
+        for (int i = left; i <= right; i++) { // TC: O(R)
+            if (hasPrimeSetBits(i)) { // TC: O(1), SC: O(1)
+                count++;
+            }
+        }
+        return count;
+    }
+
+    /**
+     * Using Math + Bit-Manipulation Approach
+     *
+     * TC: O(32) + O(Sqrt(32)) ~ O(1)
+     * SC: O(1)
+     */
+    private boolean hasPrimeSetBits(int num) {
+        int countSetBits = 0;
+        for (int i = 31; i >= 0; i--) { // TC: O(32)
+            int bitValue = ((num >> i) & 1);
+            if (bitValue == 1) {
+                countSetBits++;
+            }
+        }
+        // K is maximum 32 size (32 bits)
+        return isPrime(countSetBits); // TC: O(Sqrt(K)), SC: O(1)
+    }
+
+    /**
+     * Using Math + Simulation Approach
+     *
+     * TC: O(Sqrt(K))
+     * SC: O(1)
+     */
+    private boolean isPrime(int count) {
+        if (count == 1) {
+            return false;
+        }
+        for (int i = 2; i * i <= count; i++) { // TC: O(Sqrt(K))
+            if (count % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
