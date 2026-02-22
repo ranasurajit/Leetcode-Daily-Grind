623acd050c99aa27db648fb6dd6f1f0a254bafcc
Surajit Rana <surajit1602@gmail.com>
2026-02-22 14:14:31 +0530

    Time: 1 ms (40.87%) | Memory: 42.4 MB (48.75%) - LeetSync

--- BitManipulation/Easy/899-binary-gap/binary-gap.java
+++ BitManipulation/Easy/899-binary-gap/binary-gap.java
@@ -1,41 +1,20 @@
 class Solution {
     /**
-     * Approach II : Using Better (Bit by Bit Scanning + No Extra Space) Approach
-     *
-     * TC: O(32) ~ O(1)
-     * SC: O(1)
-     */
-    public int binaryGap(int n) {
-        int lastSetBit = -1;
-        int maxGap = 0;
-        for (int i = 31; i >= 0; i--) { // TC: O(32)
-            if (((n >> i) & 1) == 1) {
-                int currentSetBit = i;
-                if (lastSetBit != -1) {
-                    maxGap = Math.max(maxGap, lastSetBit - currentSetBit);
-                }
-                lastSetBit = currentSetBit;
-            }
-        }
-        return maxGap;
-    }
-
-    /**
      * Approach I : Using Brute-Force (Bit by Bit Scanning + Extra Space) Approach
      *
      * TC: O(32) + O(32) ~ O(1)
      * SC: O(32) ~ O(1)
      */
-    public int binaryGapBruteForce(int n) {
+    public int binaryGap(int n) {
         List<Integer> setBits = new ArrayList<Integer>(); // SC: O(32)
         for (int i = 31; i >= 0; i--) { // TC: O(32)
             if (((n >> i) & 1) == 1) {
-                setBits.add(32 - i);
+                setBits.add(i);
             }
         }
         int maxGap = 0;
         for (int i = 1; i < setBits.size(); i++) { // TC: O(32)
-            maxGap = Math.max(maxGap, setBits.get(i) - setBits.get(i - 1));
+            maxGap = Math.max(maxGap, setBits.get(i - 1) - setBits.get(i));
         }
         return maxGap;
     }
