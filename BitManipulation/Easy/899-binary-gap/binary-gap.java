bcd3cc0ebf049170e6ec1e2b616af50990ff6c77
Surajit Rana <surajit1602@gmail.com>
2026-02-22 14:09:09 +0530

    Time: 1 ms (40.87%) | Memory: 42.6 MB (14.06%) - LeetSync

---
+++ BitManipulation/Easy/899-binary-gap/binary-gap.java
@@ -0,0 +1,21 @@
+class Solution {
+    /**
+     * Approach I : Using Brute-Force (Bit by Bit Scanning + Extra Space) Approach
+     *
+     * TC: O(32) + O(32) ~ O(1)
+     * SC: O(32) ~ O(1)
+     */
+    public int binaryGap(int n) {
+        List<Integer> setBits = new ArrayList<Integer>(); // SC: O(32)
+        for (int i = 31; i >= 0; i--) { // TC: O(32)
+            if (((n >> i) & 1) == 1) {
+                setBits.add(32 - i);
+            }
+        }
+        int maxGap = 0;
+        for (int i = 1; i < setBits.size(); i++) { // TC: O(32)
+            maxGap = Math.max(maxGap, setBits.get(i) - setBits.get(i - 1));
+        }
+        return maxGap;
+    }
+}
