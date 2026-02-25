672116f7e968e4b2679100973b92abb0871a69d7
Surajit Rana <surajit1602@gmail.com>
2026-02-25 09:40:03 +0530

    Time: 8 ms (61.40%) | Memory: 46.2 MB (97.98%) - LeetSync

---
+++ Arrays/Sorting/Easy/1458-sort-integers-by-the-number-of-1-bits/sort-integers-by-the-number-of-1-bits.java
@@ -0,0 +1,48 @@
+class Solution {
+    /**
+     * Approach : Using Sorting + Bit-Manipulation Approach
+     *
+     * TC: O(N) + O(N x log(N)) + O(N) ~ O(N x log(N))
+     * SC: O(N)
+     */
+    public int[] sortByBits(int[] arr) {
+        int n = arr.length;
+        int[][] nums = new int[n][2]; // SC: O(N x 2) ~ O(N)
+        for (int i = 0; i < n; i++) { // TC: O(N)
+            nums[i][0] = arr[i];
+            nums[i][1] = countSetBits(arr[i]); // TC: O(1)
+        }
+        Arrays.sort(nums, (a, b) -> {
+            if (a[1] == b[1]) {
+                return a[0] - b[0];
+            }
+            return a[1] - b[1];
+        }); // TC: O(N x log(N))
+        int[] sorted = new int[n]; // SC: O(N) - can be neglected as this is the output
+        for (int i = 0; i < n; i++) { // TC: O(N)
+            sorted[i] = nums[i][0];
+        }
+        return sorted;
+    }
+
+    /**
+     * Using Bit-Manipulation Approach
+     *
+     * TC: O(15) ~ O(1)
+     * SC: O(1)
+     */
+    private int countSetBits(int num) {
+        int count = 0;
+        /**
+         * we don't have to compare all 32 / 64 bits here as
+         * per constraints, 0 <= arr[i] <= 10^4 so, 10^4
+         * can go upto holding set bit till maximum 15 bits
+         */
+        for (int i = 14; i >= 0; i--) { // TC: O(15)
+            if (((num >> i) & 1) == 1) {
+                count++;
+            }
+        }
+        return count;
+    }
+}
