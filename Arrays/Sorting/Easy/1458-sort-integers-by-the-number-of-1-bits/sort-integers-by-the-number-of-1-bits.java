694228b9c81d3d0f94927f999622881045290708
Surajit Rana <surajit1602@gmail.com>
2026-02-25 09:52:55 +0530

    Time: 8 ms (61.40%) | Memory: 46.5 MB (84.05%) - LeetSync

--- Arrays/Sorting/Easy/1458-sort-integers-by-the-number-of-1-bits/sort-integers-by-the-number-of-1-bits.java
+++ Arrays/Sorting/Easy/1458-sort-integers-by-the-number-of-1-bits/sort-integers-by-the-number-of-1-bits.java
@@ -1,11 +1,47 @@
 class Solution {
     /**
-     * Approach : Using Sorting + Bit-Manipulation Approach
+     * Approach II : Using Sorting on Running Bit Counts Approach
      *
      * TC: O(N) + O(N x log(N)) + O(N) ~ O(N x log(N))
      * SC: O(N)
      */
     public int[] sortByBits(int[] arr) {
+        int n = arr.length;
+        Integer[] nums = new Integer[n]; // SC: O(N)
+        for (int i = 0; i < n; i++) { // TC: O(N)
+            nums[i] = Integer.valueOf(arr[i]);
+        }
+        Arrays.sort(nums, (a, b) -> {
+            int aBits = countBitsOptimal(a);
+            int bBits = countBitsOptimal(b);
+            if (aBits == bBits) {
+                return a - b;
+            }
+            return aBits - bBits;
+        }); // TC: O(N x log(N))
+        int[] sorted = new int[n]; // SC: O(N) - can be neglected as this is the output
+        for (int i = 0; i < n; i++) { // TC: O(N)
+            sorted[i] = nums[i];
+        }
+        return sorted;
+    }
+
+    private int countBitsOptimal(int num) {
+        int count = 0;
+        while (num > 0) {
+            num = (num & (num - 1));
+            count++;
+        }
+        return count;
+    }
+
+    /**
+     * Approach I : Using Bit-By-Bit Count (Sorting + Bit-Manipulation) Approach
+     *
+     * TC: O(N) + O(N x log(N)) + O(N) ~ O(N x log(N))
+     * SC: O(N)
+     */
+    public int[] sortByBitsBitByBitCountApproach(int[] arr) {
         int n = arr.length;
         int[][] nums = new int[n][2]; // SC: O(N x 2) ~ O(N)
         for (int i = 0; i < n; i++) { // TC: O(N)
