fd742cd293eb8c1ef6046d024f522df7f8cb12dc
Surajit Rana <surajit1602@gmail.com>
2026-02-26 11:49:50 +0530

    Time: 6 ms (16.52%) | Memory: 46.2 MB (14.45%) - LeetSync

---
+++ BitManipulation/Medium/1520-number-of-steps-to-reduce-a-number-in-binary-representation-to-one/number-of-steps-to-reduce-a-number-in-binary-representation-to-one.java
@@ -0,0 +1,82 @@
+class Solution {
+    /**
+     * Approach II : Using Optimal (String Simulation + Bit-Manipulation) Approach
+     *
+     * TC: O(N²)
+     * SC: O(1) 
+     *
+     * Note: This approach will handle overflows
+     */
+    public int numSteps(String s) {
+        int steps = 0;
+        while (!s.equals("1")) { // TC: O(N)
+            int n = s.length();
+            int lastBit = (s.charAt(n - 1) - '0');
+            if (lastBit == 1) {
+                /**
+                 * s denotes an odd number, so to add 1 to it,
+                 * we need to find the 1st zero from right to left
+                 * and make it 1 and rest all towards right to zero
+                 */
+                int j = n - 1;
+                while (j > 0 && s.charAt(j) != '0') {
+                    j--;
+                }
+                if (j == 0) {
+                    s = "1";
+                } else {
+                    s = s.substring(0, j) + "1";
+                    j++;
+                }
+                while (j < n) { // TC: O(N)
+                    s += "0";
+                    j++;
+                }
+            } else {
+                // s denotes an even number
+                s = s.substring(0, s.length() - 1); // TC: O(N)
+            }
+            steps++;
+        }
+        return steps;
+    }
+
+    /**
+     * Approach I : Using Brute-Force (String Simulation + Bit-Manipulation) Approach
+     *
+     * TC: O(N)
+     * SC: O(1)
+     *
+     * Note: This approach will result in wrong results in case of overflow
+     */
+    public int numStepsBruteForce(String s) {
+        int n = s.length();
+        long num = getNumber(s, n); // TC: O(N), SC: O(1)
+        int steps = 0;
+        while (num > 1L) { // TC: O(N)
+            if ((num & 1) == 0) {
+                // even
+                num = num / 2;
+            } else {
+                // odd
+                num += 1;
+            }
+            steps++;
+        }
+        return steps;
+    }
+
+    /**
+     * Using String Simulation + Bit-Manipulation Approach
+     *
+     * TC: O(N)
+     * SC: O(1)
+     */
+    private long getNumber(String s, int n) {
+        long num = 0L;
+        for (int i = n - 1; i >= 0; i--) { // TC: O(N)
+            num += (s.charAt(i) - '0') * (long) (1 << (n - i - 1));
+        }
+        return num;
+    }
+}
