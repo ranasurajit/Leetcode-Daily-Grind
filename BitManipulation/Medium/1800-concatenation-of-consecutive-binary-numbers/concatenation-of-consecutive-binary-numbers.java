6719116070a017060c6d13738bcb88a309a21949
Surajit Rana <surajit1602@gmail.com>
2026-02-28 16:56:56 +0530

    Time: 42 ms (48.84%) | Memory: 42.6 MB (63.95%) - LeetSync

--- BitManipulation/Medium/1800-concatenation-of-consecutive-binary-numbers/concatenation-of-consecutive-binary-numbers.java
+++ BitManipulation/Medium/1800-concatenation-of-consecutive-binary-numbers/concatenation-of-consecutive-binary-numbers.java
@@ -1,5 +1,32 @@
 class Solution {
     private static final int MOD = (int) 1e9 + 7;
+    /**
+     * Approach III : Using Optimal (Bit-Manipulation) Approach
+     *
+     * TC: O(N)
+     * SC: O(1)
+     */
+    public int concatenatedBinary(int n) {
+        /**
+         * the bit length increase in every powers of 2
+         * for example: 
+         * 1 (power of 2) - 1,
+         * 2 (power of 2) - 10 (bit increased)
+         * 3 (not power of 2) - 11 (bits are same as previous)
+         * 4 (power of 2) - 100 (bit increased)
+         */
+        long result = 0L;
+        int bits = 0;
+        for (int i = 1; i <= n; i++) { // TC: O(N)
+            // check if 'i' is power of 2
+            if ((i & (i - 1)) == 0) {
+                // here 'i' is power of 2
+                bits++;
+            }
+            result = ((result << bits) % MOD + i) % MOD;
+        }
+        return (int) result;
+    }
 
     /**
      * Approach II : Using Better (String Simulation + Bit-Manipulation) Approach
@@ -10,16 +37,12 @@ class Solution {
      * Each number i, has (log(i) Base 2) bits 
      * so total String can become of length (N x log(N))
      */
-    public int concatenatedBinary(int n) {
-        /**
-         * as per constraints n can go upto 10⁵ so 
-         * maximum left most bit can go upto 17 digits
-         */
+    public int concatenatedBinaryBetter(int n) {
         StringBuilder sb = new StringBuilder(); // SC: O(N x log(N))
         for (int i = 1; i <= n; i++) { // TC: O(N)
             sb.append(Integer.toBinaryString(i)); // TC: O(1), SC: O(1)
         }
-        long result = 0;
+        long result = 0L;
         int m = sb.length();
         for (int i = 0; i < m; i++) { // TC: O(N x log(N))
             result = (result * 2 + (sb.charAt(i) - '0')) % MOD;
@@ -45,7 +68,7 @@ class Solution {
         for (int i = 1; i <= n; i++) { // TC: O(N)
             sb.append(convertNumberToBinary(i)); // TC: O(1), SC: O(1)
         }
-        long result = 0;
+        long result = 0L;
         int m = sb.length();
         for (int i = 0; i < m; i++) { // TC: O(N x log(N))
             result = (result * 2 + (sb.charAt(i) - '0')) % MOD;
