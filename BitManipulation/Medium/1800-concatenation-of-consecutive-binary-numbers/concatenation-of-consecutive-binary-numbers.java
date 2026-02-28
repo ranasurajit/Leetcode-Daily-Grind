30bec12b23b23ef8c89da3a6f132022d1f842866
Surajit Rana <surajit1602@gmail.com>
2026-02-28 16:36:12 +0530

    Time: 643 ms (18.60%) | Memory: 98.5 MB (8.14%) - LeetSync

--- BitManipulation/Medium/1800-concatenation-of-consecutive-binary-numbers/concatenation-of-consecutive-binary-numbers.java
+++ BitManipulation/Medium/1800-concatenation-of-consecutive-binary-numbers/concatenation-of-consecutive-binary-numbers.java
@@ -2,14 +2,41 @@ class Solution {
     private static final int MOD = (int) 1e9 + 7;
 
     /**
-     * Approach I : Using Brute-Force (String Simulation + Bit-Manipulation) Approach
+     * Approach II : Using Better (String Simulation + Bit-Manipulation) Approach
      *
      * TC: O(N) + O(N x log(N)) ~ O(N x log(N))
      * SC: O(N x log(N))
+     *
      * Each number i, has (log(i) Base 2) bits 
      * so total String can become of length (N x log(N))
      */
     public int concatenatedBinary(int n) {
+        /**
+         * as per constraints n can go upto 10⁵ so 
+         * maximum left most bit can go upto 17 digits
+         */
+        StringBuilder sb = new StringBuilder(); // SC: O(N x log(N))
+        for (int i = 1; i <= n; i++) { // TC: O(N)
+            sb.append(Integer.toBinaryString(i)); // TC: O(1), SC: O(1)
+        }
+        long result = 0;
+        int m = sb.length();
+        for (int i = 0; i < m; i++) { // TC: O(N x log(N))
+            result = (result * 2 + (sb.charAt(i) - '0')) % MOD;
+        }
+        return (int) result;
+    }
+
+    /**
+     * Approach I : Using Brute-Force (String Simulation + Bit-Manipulation) Approach
+     *
+     * TC: O(N) + O(N x log(N)) ~ O(N x log(N))
+     * SC: O(N x log(N))
+     *
+     * Each number i, has (log(i) Base 2) bits 
+     * so total String can become of length (N x log(N))
+     */
+    public int concatenatedBinaryBruteForce(int n) {
         /**
          * as per constraints n can go upto 10⁵ so 
          * maximum left most bit can go upto 17 digits
