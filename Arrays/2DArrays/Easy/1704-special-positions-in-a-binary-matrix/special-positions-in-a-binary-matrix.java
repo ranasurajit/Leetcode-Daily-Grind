3c840eff2da352fde27629b42e34a897dd803e7b
Surajit Rana <surajit1602@gmail.com>
2026-03-04 11:09:40 +0530

    Time: 2 ms (94.84%) | Memory: 47.4 MB (11.92%) - LeetSync

---
+++ Arrays/2DArrays/Easy/1704-special-positions-in-a-binary-matrix/special-positions-in-a-binary-matrix.java
@@ -0,0 +1,44 @@
+class Solution {
+    private int m;
+    private int n;
+
+    /**
+     * Approach : Using Brute-Force(2D-Array Simulation) Approach
+     *
+     * TC: O(M x N x (M + N))
+     * SC: O(1)
+     */
+    public int numSpecial(int[][] mat) {
+        this.m = mat.length;
+        this.n = mat[0].length;
+        int count = 0;
+        for (int i = 0; i < m; i++) {     // TC: O(M)
+            for (int j = 0; j < n; j++) { // TC: O(N)
+                if (mat[i][j] == 1 && isSpecial(mat, i, j)) { // TC: O(M + N)
+                    count++;
+                }
+            }
+        }
+        return count;
+    }
+
+    /**
+     * Using 2D-Array Simulation Approach
+     *
+     * TC: O(M + N)
+     * SC: O(1)
+     */
+    private boolean isSpecial(int[][] mat, int r, int c) {
+        for (int i = 0; i < m; i++) { // TC: O(M)
+            if (i != r && mat[i][c] == 1) {
+                return false;
+            }
+        }
+        for (int j = 0; j < n; j++) { // TC: O(N)
+            if (j != c && mat[r][j] == 1) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
