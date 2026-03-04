9462e35df2d5be26b98c29eb8b0abd067464082a
Surajit Rana <surajit1602@gmail.com>
2026-03-04 11:19:39 +0530

    Time: 8 ms (6.94%) | Memory: 46.9 MB (91.99%) - LeetSync

--- Arrays/2DArrays/Easy/1704-special-positions-in-a-binary-matrix/special-positions-in-a-binary-matrix.java
+++ Arrays/2DArrays/Easy/1704-special-positions-in-a-binary-matrix/special-positions-in-a-binary-matrix.java
@@ -3,12 +3,47 @@ class Solution {
     private int n;
 
     /**
-     * Approach : Using Brute-Force(2D-Array Simulation) Approach
+     * Approach II : Using Optimal(2D-Array Simulation) Approach
+     *
+     * TC: O(M x N) + O(M + N) ~ O(M x N)
+     * SC: O(Max(M, N))
+     */
+    public int numSpecial(int[][] mat) {
+        this.m = mat.length;
+        this.n = mat[0].length;
+        int count = 0;
+        Set<String> set = new HashSet<String>(); // SC: O(Max(M, N))
+        for (int i = 0; i < m; i++) {     // TC: O(M)
+            for (int j = 0; j < n; j++) { // TC: O(N)
+                if (mat[i][j] == 1) {
+                    String key = i + "_" + j;
+                    if (set.contains(key)) {
+                        // early pruning
+                        continue;
+                    }
+                    if (isSpecial(mat, i, j)) { // TC: O(M + N)
+                        count++;
+                    } else {
+                        /**
+                         * if the cell { i, j } is 1 and not special 
+                         * so any other cells with value 1 in that 
+                         * row and column cannot be special
+                         */
+                        set.add(key);
+                    }
+                }
+            }
+        }
+        return count;
+    }
+
+    /**
+     * Approach I : Using Brute-Force(2D-Array Simulation) Approach
      *
      * TC: O(M x N x (M + N))
      * SC: O(1)
      */
-    public int numSpecial(int[][] mat) {
+    public int numSpecialBruteForce(int[][] mat) {
         this.m = mat.length;
         this.n = mat[0].length;
         int count = 0;
