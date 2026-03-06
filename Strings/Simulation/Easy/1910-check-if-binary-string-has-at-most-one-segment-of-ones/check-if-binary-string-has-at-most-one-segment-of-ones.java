a0b9f43ee585bb4b0cb8488c8b1b144b86089d8a
Surajit Rana <surajit1602@gmail.com>
2026-03-06 09:15:47 +0530

    Time: 0 ms (100.00%) | Memory: 42.5 MB (96.42%) - LeetSync

---
+++ Strings/Simulation/Easy/1910-check-if-binary-string-has-at-most-one-segment-of-ones/check-if-binary-string-has-at-most-one-segment-of-ones.java
@@ -0,0 +1,31 @@
+class Solution {
+    /**
+     * Approach : Using String Simulation Approach
+     *
+     * TC: O(N) - entire String s is traversed once
+     * SC: O(1) - no extra space used
+     */
+    public boolean checkOnesSegment(String s) {
+        int n = s.length();
+        int i = 0;
+        int j = n - 1;
+        while (i < n && s.charAt(i) == '1') {
+            i++;
+        }
+        // here index 'i' points at a value '0'
+        while (j > 0 && s.charAt(j) == '0') {
+            j--;
+        }
+        // here index 'j' points at a value '1'
+        if (j < i) {
+            return true;
+        }
+        for (int p = i; p <= j; p++) {
+            if (s.charAt(p) == '1') {
+                // another contiguous segment of ones found here
+                return false;
+            }
+        }
+        return true;
+    }
+}
