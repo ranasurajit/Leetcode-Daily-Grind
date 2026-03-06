bb3409077f8f914a4a509d222fe33c78415421fa
Surajit Rana <surajit1602@gmail.com>
2026-03-06 09:20:09 +0530

    Time: 0 ms (100.00%) | Memory: 42.6 MB (96.42%) - LeetSync

--- Strings/TwoPointers/Easy/1910-check-if-binary-string-has-at-most-one-segment-of-ones/check-if-binary-string-has-at-most-one-segment-of-ones.java
+++ Strings/TwoPointers/Easy/1910-check-if-binary-string-has-at-most-one-segment-of-ones/check-if-binary-string-has-at-most-one-segment-of-ones.java
@@ -1,11 +1,35 @@
 class Solution {
     /**
-     * Approach : Using Two Pointers + String Simulation Approach
+     * Approach II : Using Two Pointers Approach
      *
      * TC: O(N) - entire String s is traversed once
      * SC: O(1) - no extra space used
      */
     public boolean checkOnesSegment(String s) {
+        int n = s.length();
+        int i = 0;
+        int j = n - 1;
+        while (i < n && s.charAt(i) == '1') {
+            i++;
+        }
+        // here index 'i' points at a value '0'
+        while (j > 0 && s.charAt(j) == '0') {
+            j--;
+        }
+        // here index 'j' points at a value '1'
+        if (j < i) {
+            return true;
+        }
+        return false;
+    }
+
+    /**
+     * Approach I : Using Two Pointers + String Simulation Approach
+     *
+     * TC: O(N) - entire String s is traversed once
+     * SC: O(1) - no extra space used
+     */
+    public boolean checkOnesSegmentTwoPointersStringSimulation(String s) {
         int n = s.length();
         int i = 0;
         int j = n - 1;
