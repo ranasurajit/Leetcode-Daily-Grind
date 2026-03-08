90c40d8b1a7c087aa33d83f42608b420092950c1
Surajit Rana <surajit1602@gmail.com>
2026-03-08 12:53:16 +0530

    Time: 275 ms (5.19%) | Memory: 46.9 MB (9.65%) - LeetSync

---
+++ Backtracking/Medium/2107-find-unique-binary-string/find-unique-binary-string.java
@@ -0,0 +1,42 @@
+class Solution {
+    private String result;
+
+    /**
+     * Approach : Using Backtracking Approach
+     *
+     * TC: O(2 ^ N)
+     * SC: O(N)
+     */
+    public String findDifferentBinaryString(String[] nums) {
+        int n = nums.length;
+        Set<String> set = new HashSet<>(); // SC: O(N)
+        for (String num : nums) { // TC: O(N)
+            set.add(num);
+        }
+        backtrack(0, n, new StringBuilder(), set);
+        return result;
+    }
+
+    /**
+     * Using Backtracking Approach
+     *
+     * TC: O(2 ^ N)
+     * SC: O(N)
+     */
+    private void backtrack(int idx, int n, StringBuilder sb, Set<String> set) {
+        // Base Case
+        if (idx == n) {
+            // sb will have a String of length = n
+            if (!set.contains(sb.toString())) {
+                result = sb.toString();
+            }
+            return;
+        }
+        // Recursion Calls
+        for (int j = 0; j <= 1; j++) {
+            sb.append(j);
+            backtrack(idx + 1, n, sb, set);
+            sb.setLength(sb.length() - 1);
+        }
+    }
+}
