b02b58de1191bc17b22a3b9380911627cc70b179
Surajit Rana <surajit1602@gmail.com>
2026-03-11 11:20:38 +0530

    Time: 0 ms (100.00%) | Memory: 40.9 MB (100.00%) - LeetSync

--- BitManipulation/Easy/1054-complement-of-base-10-integer/complement-of-base-10-integer.java
+++ BitManipulation/Easy/1054-complement-of-base-10-integer/complement-of-base-10-integer.java
@@ -1,11 +1,30 @@
 class Solution {
     /**
+     * Approach II : Using Inbuilt Method + Bit-Manipulation Approach
+     *
+     * TC: O(M)
+     * SC: O(M), where M = leftmost set bit
+     */
+    public int bitwiseComplement(int n) {
+        int result = 0;
+        String binaryStr = Integer.toBinaryString(n); // SC: O(M)
+        int m = binaryStr.length();
+        for (int i = m - 1; i >= 0; i--) { // TC: O(M)
+            int flipped = binaryStr.charAt(i) == '0' ? 1 : 0;
+            if (flipped == 1) {
+                result += (1 << (m - 1 - i));
+            }
+        }
+        return result;
+    }
+
+    /**
      * Approach I : Using Math + Bit-Manipulation Approach
      *
      * TC: O(log(N) Base 2)
      * SC: O(1)
      */
-    public int bitwiseComplement(int n) {
+    public int bitwiseComplementDivisionMethod(int n) {
         int result = 0;
         if (n == 0) {
             return 1; // flipped
