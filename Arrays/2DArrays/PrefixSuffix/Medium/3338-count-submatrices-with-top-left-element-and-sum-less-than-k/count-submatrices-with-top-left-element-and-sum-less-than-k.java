b20422e9bf82fc131f90f5179734f1d1e011e3ce
Surajit Rana <surajit1602@gmail.com>
2026-03-18 11:29:09 +0530

    Time: 8 ms (25.81%) | Memory: 168.5 MB (29.03%) - LeetSync

--- Arrays/2DArrays/PrefixSuffix/Medium/3338-count-submatrices-with-top-left-element-and-sum-less-than-k/count-submatrices-with-top-left-element-and-sum-less-than-k.java
+++ Arrays/2DArrays/PrefixSuffix/Medium/3338-count-submatrices-with-top-left-element-and-sum-less-than-k/count-submatrices-with-top-left-element-and-sum-less-than-k.java
@@ -1,11 +1,45 @@
 class Solution {
     /**
+     * Approach II : Using Better(Prefix-Sum - Single Pass) Approach
+     *
+     * TC: O(M x N)
+     * SC: O(M x N)
+     */
+    public int countSubmatrices(int[][] grid, int k) {
+        int m = grid.length;
+        int n = grid[0].length;
+        /**
+         * Intuition is to compute the prefix sum of
+         * the 2D matrix grid and then count the 
+         * number of cells greater than k)
+         *
+         * prefix[i][j] = grid[i][j] + prefix[i - 1][j] +
+         * prefix[i][j - 1] - prefix[i - 1][j - 1]
+         * compute count on-the-go
+         */
+        int[][] prefix = new int[m][n];   // SC: O(M x N)
+        int count = 0;
+        for (int i = 0; i < m; i++) {     // TC: O(M)
+            for (int j = 0; j < n; j++) { // TC: O(N)
+                prefix[i][j] = grid[i][j] + 
+                    (i > 0 ? prefix[i - 1][j] : 0) + 
+                    (j > 0 ? prefix[i][j - 1] : 0) - 
+                    (i > 0 && j > 0 ? prefix[i - 1][j - 1] : 0);
+                if (prefix[i][j] <= k) {
+                    count++;
+                }
+            }
+        }
+        return count;
+    }
+
+    /**
      * Approach I : Using Brute-Force(Prefix-Sum) Approach
      *
      * TC: O(M x N) + O(M x N) ~ O(M x N)
      * SC: O(M x N)
      */
-    public int countSubmatrices(int[][] grid, int k) {
+    public int countSubmatricesBruteForce(int[][] grid, int k) {
         int m = grid.length;
         int n = grid[0].length;
         /**
