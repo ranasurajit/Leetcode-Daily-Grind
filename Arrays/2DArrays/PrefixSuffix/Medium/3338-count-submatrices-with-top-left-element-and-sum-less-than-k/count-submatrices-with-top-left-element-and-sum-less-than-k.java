d8f1f71512ee8b331f871edc0158720bf1be3a80
Surajit Rana <surajit1602@gmail.com>
2026-03-18 11:46:19 +0530

    Time: 8 ms (25.81%) | Memory: 176.8 MB (8.06%) - LeetSync

--- Arrays/2DArrays/PrefixSuffix/Medium/3338-count-submatrices-with-top-left-element-and-sum-less-than-k/count-submatrices-with-top-left-element-and-sum-less-than-k.java
+++ Arrays/2DArrays/PrefixSuffix/Medium/3338-count-submatrices-with-top-left-element-and-sum-less-than-k/count-submatrices-with-top-left-element-and-sum-less-than-k.java
@@ -1,9 +1,9 @@
 class Solution {
     /**
-     * Approach III : Using Optimal(Prefix-Sum - Single Pass, Inplace Replacement) Approach
+     * Approach II : Using Better(Prefix-Sum - Single Pass) Approach
      *
      * TC: O(M x N)
-     * SC: O(1)
+     * SC: O(M x N)
      */
     public int countSubmatrices(int[][] grid, int k) {
         int m = grid.length;
@@ -11,53 +11,21 @@ class Solution {
         /**
          * Intuition is to compute the prefix sum of
          * the 2D matrix grid and then count the 
-         * number of cells greater than k)
-         *
-         * grid[i][j] = grid[i][j] + grid[i - 1][j] +
-         * grid[i][j - 1] - grid[i - 1][j - 1]
-         * compute count on-the-go
-         */
-        int count = 0;
-        for (int i = 0; i < m; i++) {     // TC: O(M)
-            for (int j = 0; j < n; j++) { // TC: O(N)
-                grid[i][j] = grid[i][j] + 
-                    (i > 0 ? grid[i - 1][j] : 0) + 
-                    (j > 0 ? grid[i][j - 1] : 0) - 
-                    (i > 0 && j > 0 ? grid[i - 1][j - 1] : 0);
-                if (grid[i][j] <= k) {
-                    count++;
-                }
-            }
-        }
-        return count;
-    }
-
-    /**
-     * Approach II : Using Better(Prefix-Sum - Single Pass) Approach
-     *
-     * TC: O(M x N)
-     * SC: O(M x N)
-     */
-    public int countSubmatricesBetter(int[][] grid, int k) {
-        int m = grid.length;
-        int n = grid[0].length;
-        /**
-         * Intuition is to compute the prefix sum of
-         * the 2D matrix grid and then count the 
-         * number of cells greater than k)
+         * number of prefixSum cells greater than k
+         * as grid[i][j] >= 0)
          *
          * prefix[i][j] = grid[i][j] + prefix[i - 1][j] +
          * prefix[i][j - 1] - prefix[i - 1][j - 1]
          * compute count on-the-go
          */
-        int[][] prefix = new int[m][n];   // SC: O(M x N)
+        long[][] prefix = new long[m][n];   // SC: O(M x N)
         int count = 0;
         for (int i = 0; i < m; i++) {     // TC: O(M)
             for (int j = 0; j < n; j++) { // TC: O(N)
-                prefix[i][j] = grid[i][j] + 
-                    (i > 0 ? prefix[i - 1][j] : 0) + 
-                    (j > 0 ? prefix[i][j - 1] : 0) - 
-                    (i > 0 && j > 0 ? prefix[i - 1][j - 1] : 0);
+                prefix[i][j] = (long) grid[i][j] + 
+                    (i > 0 ? prefix[i - 1][j] : 0L) + 
+                    (j > 0 ? prefix[i][j - 1] : 0L) - 
+                    (i > 0 && j > 0 ? prefix[i - 1][j - 1] : 0L);
                 if (prefix[i][j] <= k) {
                     count++;
                 }
@@ -78,18 +46,19 @@ class Solution {
         /**
          * Intuition is to compute the prefix sum of
          * the 2D matrix grid and then count the 
-         * number of cells greater than k)
+         * number of prefixSum cells greater than k
+         * as grid[i][j] >= 0)
          *
          * prefix[i][j] = grid[i][j] + prefix[i - 1][j] +
          * prefix[i][j - 1] - prefix[i - 1][j - 1]
          */
-        int[][] prefix = new int[m][n];   // SC: O(M x N)
+        long[][] prefix = new long[m][n];   // SC: O(M x N)
         for (int i = 0; i < m; i++) {     // TC: O(M)
             for (int j = 0; j < n; j++) { // TC: O(N)
-                prefix[i][j] = grid[i][j] + 
-                    (i > 0 ? prefix[i - 1][j] : 0) + 
-                    (j > 0 ? prefix[i][j - 1] : 0) - 
-                    (i > 0 && j > 0 ? prefix[i - 1][j - 1] : 0);
+                prefix[i][j] = (long) grid[i][j] + 
+                    (i > 0 ? prefix[i - 1][j] : 0L) + 
+                    (j > 0 ? prefix[i][j - 1] : 0L) - 
+                    (i > 0 && j > 0 ? prefix[i - 1][j - 1] : 0L);
             }
         }
         int count = 0;
