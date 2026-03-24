0ef19bd6ffb52ff0af5d580b1e524f78c404058a
Surajit Rana <surajit1602@gmail.com>
2026-03-24 14:05:41 +0530

    Time: 10 ms (84.31%) | Memory: 100.8 MB (55.88%) - LeetSync

--- Arrays/PrefixSuffix/Medium/3031-construct-product-matrix/construct-product-matrix.java
+++ Arrays/PrefixSuffix/Medium/3031-construct-product-matrix/construct-product-matrix.java
@@ -1,12 +1,40 @@
 class Solution {
     private static final int MOD = 12345;
     /**
-     * Approach : Using Brute-Force (Prefix-Suffix Array) Approach
+     * Approach II : Using Optimal (Prefix-Suffix Array) Approach
+     *
+     * TC: O(N x M) + O(N x M) ~ O(N x M)
+     * SC: O(1)
+     */
+    public int[][] constructProductMatrix(int[][] grid) {
+        int n = grid.length;
+        int m = grid[0].length;
+        long prefix = 1L;
+        long suffix = 1L;
+        int[][] p = new int[n][m];
+        for (int i = n - 1; i >= 0; i--) { // TC: O(N)
+            for (int j = m - 1; j >= 0; j--) { // TC: O(M)
+                p[i][j] = (int) suffix;
+                suffix = (suffix * (long) grid[i][j]) % MOD;
+            }
+        }
+        for (int i = 0; i < n; i++) { // TC: O(N)
+            for (int j = 0; j < m; j++) { // TC: O(M)
+                // multiplying with prefix suffix at (i, j) i.e. p[i][j]
+                p[i][j] = (int) ((prefix * p[i][j]) % MOD);
+                prefix = (prefix * grid[i][j]) % MOD;
+            }
+        }
+        return p;
+    }
+
+    /**
+     * Approach I : Using Brute-Force(Prefix-Suffix Array) Approach
      *
      * TC: O(N x M) + O(N x M) + O(N x M) ~ O(N x M)
      * SC: O(N x M) + O(N x M) ~ O(N x M)
      */
-    public int[][] constructProductMatrix(int[][] grid) {
+    public int[][] constructProductMatrixBruteForce(int[][] grid) {
         int n = grid.length;
         int m = grid[0].length;
         long[][] prefixProduct = new long[n][m]; // SC: O(N x M)
