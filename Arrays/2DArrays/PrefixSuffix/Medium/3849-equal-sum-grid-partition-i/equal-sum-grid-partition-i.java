808fdf2e3c0f3896cb35e6660bb2077a5ef5422e
Surajit Rana <surajit1602@gmail.com>
2026-03-25 11:03:17 +0530

    Time: 4 ms (97.20%) | Memory: 161.8 MB (26.57%) - LeetSync

--- Arrays/2DArrays/PrefixSuffix/Medium/3849-equal-sum-grid-partition-i/equal-sum-grid-partition-i.java
+++ Arrays/2DArrays/PrefixSuffix/Medium/3849-equal-sum-grid-partition-i/equal-sum-grid-partition-i.java
@@ -1,11 +1,72 @@
 class Solution {
     /**
-     * Approach : Using Array Prefix Sum Approach
+     * Approach II : Using Array Prefix Sum Approach
+     *
+     * TC: O(M x N) + O(M x N) + O(M x N) ~ O(M x N)
+     * SC: O(1)
+     */
+    public boolean canPartitionGrid(int[][] grid) {
+        int m = grid.length;
+        int n = grid[0].length;
+        /**
+         * Intuition is to find the total sum,
+         * if total sum is odd then it is not
+         * possible to divide it in any cut
+         *
+         * then we can try for both horizontal 
+         * and vertical cuts individually by 
+         * computing prefix rowSums and colSums
+         * respectively and comparing with total
+         * sum / 2 
+         */
+        long totalSum = 0L;
+        for (int i = 0; i < m; i++) {     // TC: O(M)
+            for (int j = 0; j < n; j++) { // TC: O(N)
+                totalSum += (long) grid[i][j];
+            }
+        }
+        if ((totalSum & 1) != 0) {
+            // odd sum cannot be made two equal partitions
+            return false;
+        }
+        long targetSum = totalSum / 2;
+        long cumulativeSum = 0L;
+        // let's try to check for horizontal cut
+        for (int i = 0; i < m - 1; i++) { // TC: O(M)
+            long rowSum = 0L;
+            for (int j = 0; j < n; j++) { // TC: O(N)
+                rowSum += (long) grid[i][j];
+            }
+            cumulativeSum += rowSum;
+            if (cumulativeSum == targetSum) {
+                // horizontal cut is possible
+                return true;
+            }
+        }
+        // let's try to check for vertical cut
+        cumulativeSum = 0L;
+        for (int j = 0; j < n - 1; j++) { // TC: O(M)
+            long colSum = 0L;
+            for (int i = 0; i < m; i++) { // TC: O(N)
+                colSum += (long) grid[i][j];
+            }
+            cumulativeSum += colSum;
+            if (cumulativeSum == targetSum) {
+                // vertical cut is possible
+                return true;
+            }
+        }
+        // none of an cuts is possible
+        return false;
+    }
+
+    /**
+     * Approach I : Using 2D Prefix Computation Approach
      *
      * TC: O(M x N) + O(M) + O(N) ~ O(M x N)
      * SC: O(M x N)
      */
-    public boolean canPartitionGrid(int[][] grid) {
+    public boolean canPartitionGridUsing2DPrefix(int[][] grid) {
         int m = grid.length;
         int n = grid[0].length;
         /**
