9041fe5e1403677b5fb576555b7f7e195af7f56c
Surajit Rana <surajit1602@gmail.com>
2026-03-27 20:49:23 +0530

    Time: 2 ms (10.13%) | Memory: 47.2 MB (23.73%) - LeetSync

---
+++ TwoPointers/Easy/3215-matrix-similarity-after-cyclic-shifts/matrix-similarity-after-cyclic-shifts.java
@@ -0,0 +1,80 @@
+class Solution {
+    /**
+     * Approach : Using Two Pointers Approach
+     *
+     * TC: O(M x N) + O(M x N) + O(M x N) ~ O(M x N)
+     * SC: O(M x N)
+     */
+    public boolean areSimilar(int[][] mat, int k) {
+        int m = mat.length;
+        int n = mat[0].length;
+        k = k % n; // as row is same when rotated n times
+        int[][] copy = new int[m][n]; // SC: O(M x N)
+        for (int i = 0; i < m; i++) { // TC: O(M)
+            for (int j = 0; j < n; j++) { // TC: O(N)
+                copy[i][j] = mat[i][j];
+            }
+        }
+        /**
+         * without incremental shifts we can directly shift
+         * even and odd rows k times left and right respectively
+         */
+        for (int i = 0; i < m; i++) { // TC: O(M)
+            if ((i & 1) == 0) {
+                // even-indexed row
+                leftShiftArray(copy[i], n, k); // TC: O(N)
+            } else {
+                // odd-indexed row
+                rightShiftArray(copy[i], n, k); // TC: O(N)
+            }
+        }
+        for (int i = 0; i < m; i++) { // TC: O(M)
+            for (int j = 0; j < n; j++) { // TC: O(N)
+                if (copy[i][j] != mat[i][j]) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+    /**
+     * Using Two Pointers Approach
+     *
+     * TC: O(N) + O(N) + O(N) ~ O(N)
+     * SC: O(1)
+     */
+    private void leftShiftArray(int[] row, int n, int k) {
+        reverseArray(row, 0, n - 1);     // TC: O(N)
+        reverseArray(row, 0, n - k - 1); // TC: O(N)
+        reverseArray(row, n - k, n - 1); // TC: O(N)
+    }
+
+    /**
+     * Using Two Pointers Approach
+     *
+     * TC: O(N) + O(N) + O(N) ~ O(N)
+     * SC: O(1)
+     */
+    private void rightShiftArray(int[] row, int n, int k) {
+        reverseArray(row, 0, n - k - 1); // TC: O(N)
+        reverseArray(row, n - k, n - 1); // TC: O(N)
+        reverseArray(row, 0, n - 1);     // TC: O(N)
+    }
+
+    /**
+     * Using Two Pointers Approach
+     *
+     * TC: O(N)
+     * SC: O(1)
+     */
+    private void reverseArray(int[] row, int left, int right) {
+        while (left < right) { // TC: O(N)
+            int temp = row[right];
+            row[right] = row[left];
+            row[left] = temp;
+            left++;
+            right--;
+        }
+    }
+}
