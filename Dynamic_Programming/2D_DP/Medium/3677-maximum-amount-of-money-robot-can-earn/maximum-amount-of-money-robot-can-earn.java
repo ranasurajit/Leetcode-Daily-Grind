c4252f361e8cabd2d17c2771850afa065e918553
Surajit Rana <surajit1602@gmail.com>
2026-04-02 12:13:43 +0530

    Time: 110 ms (57.30%) | Memory: 237.2 MB (87.64%) - LeetSync

--- Dynamic_Programming/2D_DP/Medium/3677-maximum-amount-of-money-robot-can-earn/maximum-amount-of-money-robot-can-earn.java
+++ Dynamic_Programming/2D_DP/Medium/3677-maximum-amount-of-money-robot-can-earn/maximum-amount-of-money-robot-can-earn.java
@@ -1,5 +1,61 @@
 class Solution {
     /**
+     * Approach III : Using Tabulation (Bottom-Up) Approach
+     *
+     * TC: O(m x n x 3) ~ O(m x n)
+     * SC: O(m x n)
+     * - O(m x n) - dp array memory
+     *
+     * Accepted (579 / 579 testcases passed)
+     */
+    public int maximumAmount(int[][] coins) {
+        int m = coins.length;
+        int n = coins[0].length;
+        int[][][] dp = new int[m][n][3]; // SC: O(m x n x 3) ~ O(m x n)
+        // Initialization
+        dp[0][0][0] = coins[0][0];
+        dp[0][0][1] = 0;
+        dp[0][0][2] = 0;
+
+        // Iterative Calls
+        for (int i = 0; i < m; i++) { // TC: O(m)
+            for (int j = 0; j < n; j++) { // TC: O(n)
+                if (i == 0 && j == 0) {
+                    continue;
+                }
+                for (int k = 2; k >= 0; k--) { // TC: O(3)
+                    int skipUp = Integer.MIN_VALUE;
+                    int skipLeft = Integer.MIN_VALUE;
+                    int pick = Integer.MIN_VALUE;
+                    if (coins[i][j] < 0 && k > 0) {
+                        // try to neutralize
+                        skipUp = (i > 0 && k > 0) ? 
+                            dp[i - 1][j][k - 1] : Integer.MIN_VALUE;
+                        skipLeft = (j > 0 && k > 0) ?
+                            dp[i][j - 1][k - 1] : Integer.MIN_VALUE;
+                        // pick and don't neutralize
+                        pick = coins[i][j] + 
+                            Math.max(
+                                i > 0 ? dp[i - 1][j][k] : Integer.MIN_VALUE,
+                                j > 0 ? dp[i][j - 1][k]: Integer.MIN_VALUE
+                            );
+                    } else {
+                        pick = coins[i][j] + 
+                            Math.max(
+                                i > 0 ? dp[i - 1][j][k] : Integer.MIN_VALUE,
+                                j > 0 ? dp[i][j - 1][k]: Integer.MIN_VALUE
+                            );
+                    }
+                    int skip = Math.max(skipUp, skipLeft);
+                    dp[i][j][k] = Math.max(skip, pick);
+                }
+            }
+        }
+        return Math.max(dp[m - 1][n - 1][0],
+            Math.max(dp[m - 1][n - 1][1], dp[m - 1][n - 1][2]));
+    }
+
+    /**
      * Approach II : Using Memoization (Top-Down) Approach
      *
      * TC: O(m x n x 3) ~ O(m x n)
@@ -9,7 +65,7 @@ class Solution {
      *
      * Accepted (579 / 579 testcases passed)
      */
-    public int maximumAmount(int[][] coins) {
+    public int maximumAmountMemoization(int[][] coins) {
         int m = coins.length;
         int n = coins[0].length;
         int[][][] memo = new int[m][n][3]; // SC: O(m x n x 3) ~ O(m x n)
