5b1f5d73e4275db8e16dd1b0bb76be0d4ea37158
Surajit Rana <surajit1602@gmail.com>
2026-04-04 22:51:33 +0530

    Time: 32 ms (36.69%) | Memory: 56.9 MB (95.27%) - LeetSync

--- Strings/TwoPointers/Medium/2197-decode-the-slanted-ciphertext/decode-the-slanted-ciphertext.java
+++ Strings/TwoPointers/Medium/2197-decode-the-slanted-ciphertext/decode-the-slanted-ciphertext.java
@@ -1,12 +1,46 @@
 class Solution {
     /**
-     * Approach I : Using Brute-Force (Two Pointers) Approach
+     * Approach II : Using Optimal (Two Pointers) Approach
      *
-     * TC: O(m x n) + O(n x (m + n)) + O(m x n) ~ O(m x n) ~ O(N)
+     * TC: O(n x (m + n)) + O(m x n) ~ O(m x n) ~ O(N)
      * SC: O(m x n) ~ O(N)
      * where N = len(encodedText) = m x n
      */
     public String decodeCiphertext(String encodedText, int rows) {
+        int n = encodedText.length() / rows;
+        /**
+         * we will not be generating matrix 'mat' at all
+         * but we will be using the indices instead
+         */
+        StringBuilder sb = new StringBuilder(); // SC: O(m x n)
+        int col = 0;
+        while (col < n) { // TC: O(n)
+            int p = 0;
+            int q = col;
+            while (p < rows && q < n) { // TC: O(m + n)
+                // mat[p][q] = encodedText.charAt(p * n + q);
+                sb.append(encodedText.charAt(p * n + q));
+                p++;
+                q++;
+            }
+            col++;
+        }
+        int idx = sb.length() - 1;
+        while (idx > 0 && sb.charAt(idx) == ' ') { // TC: O(m x n)
+            sb.setLength(sb.length() - 1);
+            idx--;
+        }
+        return sb.toString();
+    }
+
+    /**
+     * Approach I : Using Brute-Force (Two Pointers) Approach
+     *
+     * TC: O(m x n) + O(n x (m + n)) + O(m x n) ~ O(m x n) ~ O(N)
+     * SC: O(m x n) + O(m x n) ~ O(N)
+     * where N = len(encodedText) = m x n
+     */
+    public String decodeCiphertextBruteForce(String encodedText, int rows) {
         int m = rows;
         int n = encodedText.length() / m;
         char[][] mat = new char[m][n];    // SC: O(m x n)
@@ -15,7 +49,7 @@ class Solution {
                 mat[i][j] = encodedText.charAt(i * n + j);
             }
         }
-        StringBuilder sb = new StringBuilder();
+        StringBuilder sb = new StringBuilder(); // SC: O(m x n)
         int col = 0;
         while (col < n) { // TC: O(n)
             int p = 0;
