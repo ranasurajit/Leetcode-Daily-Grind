6c96007f738f7e9a803d2d1bc2bb54f9230b2b47
Surajit Rana <surajit1602@gmail.com>
2026-04-05 12:16:59 +0530

    Time: 5 ms (68.18%) | Memory: 45.6 MB (75.15%) - LeetSync

--- Strings/Simulation/Easy/657-robot-return-to-origin/robot-return-to-origin.java
+++ Strings/Simulation/Easy/657-robot-return-to-origin/robot-return-to-origin.java
@@ -6,12 +6,37 @@ class Solution {
     private static final char[] directions = { 'R', 'L', 'U', 'D' };
 
     /**
-     * Approach : Using String Simulation + Hashing Approach
+     * Approach II : Using Optimal (String Simulation) Approach
+     *
+     * TC: O(n)
+     * SC: O(1)
+     */
+    public boolean judgeCircle(String moves) {
+        int n = moves.length();
+        int x = 0;
+        int y = 0;
+        for (int i = 0; i < n; i++) { // TC: O(n)
+            char ch = moves.charAt(i);
+            if (ch == 'R') {
+                x++;
+            } else if (ch == 'L') {
+                x--;
+            } else if (ch == 'U') {
+                y++;
+            } else {
+                y--;
+            }
+        }
+        return x == 0 && y == 0;
+    }
+
+    /**
+     * Approach I : Using Brute-Force (String Simulation + Hashing) Approach
      *
      * TC: O(n) + O(4) ~ O(n)
      * SC: O(4) ~ O(1)
      */
-    public boolean judgeCircle(String moves) {
+    public boolean judgeCircleBruteForce(String moves) {
         int n = moves.length();
         Map<Character, int[]> dirMap = new HashMap<>(); // SC: O(4)
         for (int i = 0; i < 4; i++) { // TC: O(4)
