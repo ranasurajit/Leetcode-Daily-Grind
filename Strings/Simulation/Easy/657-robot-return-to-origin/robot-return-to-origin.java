f6202d76cb63343540475bc22edb14a6f7851d12
Surajit Rana <surajit1602@gmail.com>
2026-04-05 12:18:13 +0530

    Time: 10 ms (8.70%) | Memory: 45.7 MB (57.48%) - LeetSync

--- Strings/Simulation/Easy/657-robot-return-to-origin/robot-return-to-origin.java
+++ Strings/Simulation/Easy/657-robot-return-to-origin/robot-return-to-origin.java
@@ -1,43 +1,15 @@
 class Solution {
-    // Moves - 'R' (right), 'L' (left), 'U' (up), and 'D' (down).
-    private static final int[][] coordinates = {
-        { 0, 1 }, { 0, -1 }, { -1, 0 }, { 1, 0 }
-    };
-    private static final char[] directions = { 'R', 'L', 'U', 'D' };
-
-    /**
-     * Approach II : Using Optimal (String Simulation) Approach
-     *
-     * TC: O(n)
-     * SC: O(1)
-     */
-    public boolean judgeCircle(String moves) {
-        int n = moves.length();
-        int x = 0;
-        int y = 0;
-        for (int i = 0; i < n; i++) { // TC: O(n)
-            char ch = moves.charAt(i);
-            if (ch == 'R') {
-                x++;
-            } else if (ch == 'L') {
-                x--;
-            } else if (ch == 'U') {
-                y++;
-            } else {
-                y--;
-            }
-        }
-        return x == 0 && y == 0;
-    }
-
     /**
      * Approach I : Using Brute-Force (String Simulation + Hashing) Approach
      *
      * TC: O(n) + O(4) ~ O(n)
      * SC: O(4) ~ O(1)
      */
-    public boolean judgeCircleBruteForce(String moves) {
+    public boolean judgeCircle(String moves) {
         int n = moves.length();
+        // Moves - 'R' (right), 'L' (left), 'U' (up), and 'D' (down).
+        int[][] coordinates = {{ 0, 1 }, { 0, -1 }, { -1, 0 }, { 1, 0 }};
+        char[] directions = { 'R', 'L', 'U', 'D' };
         Map<Character, int[]> dirMap = new HashMap<>(); // SC: O(4)
         for (int i = 0; i < 4; i++) { // TC: O(4)
             dirMap.put(directions[i], coordinates[i]);
