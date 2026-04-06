072df9a9dcdbce862b41c89c99648de366c88a43
Surajit Rana <surajit1602@gmail.com>
2026-04-06 12:45:02 +0530

    Time: 35 ms (52.90%) | Memory: 52.2 MB (51.45%) - LeetSync

---
+++ Hashing/Medium/906-walking-robot-simulation/walking-robot-simulation.java
@@ -0,0 +1,48 @@
+class Solution {
+    // Store Directions for North, East, South and West
+    private static final int[][] directions = {
+        { 0, 1 }, { 1, 0 }, { 0, -1 }, { -1, 0 }
+    };
+    private int d = 0; // current direction is north
+
+    /**
+     * Approach : Using Hashing Approach
+     *
+     * TC: O(m) + O(k x n) ~ O(m + n) as per constraint (1 <= k <= 9)
+     * SC: O(m)
+     */
+    public int robotSim(int[] commands, int[][] obstacles) {
+        int n = commands.length;
+        Set<String> obs = new HashSet<>(); // SC: O(m)
+        for (int[] ob : obstacles) {       // TC: O(m)
+            obs.add(ob[0] + "_" + ob[1]);
+        }
+        int[] current = { 0, 0 };
+        int farthest = 0;
+        // execute commands
+        for (int k : commands) { // TC: O(n)
+            if (k == -1) {
+                // turn right 90 degrees
+                d = (d + 1) % 4;
+            } else if (k == -2) {
+                // turn left 90 degrees
+                d = (d + 3) % 4;
+            } else {
+                // move k units (one unit at a time)
+                int[] delta = directions[d];
+                for (int i = 1; i <= k; i++) { // TC: O(k)
+                    int x = current[0] + delta[0];
+                    int y = current[1] + delta[1];
+                    if (obs.contains(x + "_" + y)) {
+                        break;
+                    }
+                    current[0] = x;
+                    current[1] = y;
+                    farthest = Math.max(farthest,
+                        current[0] * current[0] + current[1] * current[1]);
+                }
+            }
+        }
+        return farthest;
+    }
+}
