31719599bf56c128ae4acae02857eae3157e13a1
Surajit Rana <surajit1602@gmail.com>
2026-04-10 13:11:03 +0530

    Time: 5 ms (51.45%) | Memory: 45.1 MB (18.97%) - LeetSync

--- Hashing/Easy/4115-minimum-distance-between-three-equal-elements-i/minimum-distance-between-three-equal-elements-i.java
+++ Hashing/Easy/4115-minimum-distance-between-three-equal-elements-i/minimum-distance-between-three-equal-elements-i.java
@@ -1,11 +1,43 @@
 class Solution {
     /**
+     * Approach II : Using Optimal (Hashing) Approach
+     *
+     * TC: O(n) + O(k x n) ~ O(n²)
+     * SC: O(n)
+     */
+    public int minimumDistance(int[] nums) {
+        int n = nums.length;
+        int minDist = Integer.MAX_VALUE;
+        Map<Integer, ArrayList<Integer>> map = new HashMap<>(); // SC: O(n)
+        for (int i = 0; i < n; i++) {         // TC: O(n)
+            map.computeIfAbsent(nums[i], k -> new ArrayList<>()).add(i);
+        }
+        for (Integer key : map.keySet()) { // TC: O(k)
+            ArrayList<Integer> indices = map.get(key);
+            int m = indices.size();
+            if (m >= 3) {
+                int k = m - 1;
+                while (k >= 2) { // TC: O(n)
+                    int j = k - 1;
+                    int i = k - 2;
+                    int current = (indices.get(j) - indices.get(i)) + 
+                        (indices.get(k) - indices.get(j)) +
+                        (indices.get(k) - indices.get(i));
+                    minDist = Math.min(minDist, current);
+                    k--;
+                }
+            }
+        }
+        return minDist == Integer.MAX_VALUE ? -1 : minDist;
+    }
+
+    /**
      * Approach I : Using Brute-Force (Array Simulation) Approach
      *
      * TC: O(n³)
      * SC: O(1)
      */
-    public int minimumDistance(int[] nums) {
+    public int minimumDistanceBruteForce(int[] nums) {
         int n = nums.length;
         int minDist = Integer.MAX_VALUE;
         for (int i = 0; i < n - 2; i++) {         // TC: O(n)
