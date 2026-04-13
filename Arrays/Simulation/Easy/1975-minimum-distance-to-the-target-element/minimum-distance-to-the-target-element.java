c56da0c73f583ff9c5625b5cc871dbc122de0109
Surajit Rana <surajit1602@gmail.com>
2026-04-13 12:25:59 +0530

    Time: 0 ms (100.00%) | Memory: 44.6 MB (93.42%) - LeetSync

--- Arrays/Simulation/Easy/1975-minimum-distance-to-the-target-element/minimum-distance-to-the-target-element.java
+++ Arrays/Simulation/Easy/1975-minimum-distance-to-the-target-element/minimum-distance-to-the-target-element.java
@@ -1,11 +1,33 @@
 class Solution {
     /**
-     * Approach : Using Array Simulation Approach
+     * Approach II : Using Optimal (Two Pointers) Approach
      *
      * TC: O(n)
      * SC: O(1)
      */
     public int getMinDistance(int[] nums, int target, int start) {
+        int n = nums.length;
+        int minDist = Integer.MAX_VALUE;
+        for (int i = 0; i < n; i++) { // TC: O(n)
+            int left = Math.abs(start - i);
+            int right = Math.abs(start + i);
+            if (left >= 0 && nums[left] == target) {
+                return i;
+            }
+            if (right < n && nums[right] == target) {
+                return i;
+            }
+        }
+        return 0;
+    }
+
+    /**
+     * Approach I : Using Brute-Force (Array Simulation) Approach
+     *
+     * TC: O(n)
+     * SC: O(1)
+     */
+    public int getMinDistanceBruteForce(int[] nums, int target, int start) {
         int n = nums.length;
         int minDist = Integer.MAX_VALUE;
         for (int i = 0; i < n; i++) { // TC: O(n)
