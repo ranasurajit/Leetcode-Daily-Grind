6e4e6d4d75559c969c2f6c2c233cc7ede53dafe3
Surajit Rana <surajit1602@gmail.com>
2026-04-15 10:27:25 +0530

    Time: 0 ms (100.00%) | Memory: 46.9 MB (55.19%) - LeetSync

--- Arrays/Simulation/Easy/2598-shortest-distance-to-target-string-in-a-circular-array/shortest-distance-to-target-string-in-a-circular-array.java
+++ Arrays/Simulation/Easy/2598-shortest-distance-to-target-string-in-a-circular-array/shortest-distance-to-target-string-in-a-circular-array.java
@@ -1,20 +1,39 @@
 class Solution {
     /**
-     * Approach : Using Array Simulation Approach
+     * Approach II : Using Array Simulation Approach
      *
-     * TC: O(Max(s, n - s))
+     * TC: O(n)
      * SC: O(1)
-     * where s = startIndex and n = size(words)
      */
     public int closestTarget(String[] words, String target, int startIndex) {
+        int n = words.length;
+        int minDist = Integer.MAX_VALUE;
+        for (int i = 0; i < n; i++) { // TC: O(n)
+            if (words[i].equals(target)) {
+                int right = Math.abs(i - startIndex);
+                int left = n - right;
+                minDist = Math.min(minDist, Math.min(left, right));
+            }
+        }
+        return minDist == Integer.MAX_VALUE ? -1 : minDist;
+    }
+
+    /**
+     * Approach I : Using Array Simulation Approach
+     *
+     * TC: O(n / 2) ~ O(n)
+     * SC: O(1)
+     */
+    public int closestTargetArraySimulation(String[] words, String target,
+        int startIndex) {
         int n = words.length;
         /**
          * we can offset a distance either side of the startIndex
          * and if we encounter words[i] = target then that guarantees
          * the shortest distance to the target String
          */
-        int maxOffset = Math.max(startIndex, n - 1 - startIndex);
-        for (int offset = 0; offset <= maxOffset; offset++) { // TC: O(Max(s, n - s))
+        int maxOffset = n / 2;
+        for (int offset = 0; offset <= maxOffset; offset++) { // TC: O(n / 2)
             int leftIdx = (startIndex - offset + n) % n;
             int rightIdx = (startIndex + offset) % n;
             if (words[leftIdx].equals(target)) {
