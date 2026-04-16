d1ce09c739c4bf97c22c62d47a5ca56f8b00882a
Surajit Rana <surajit1602@gmail.com>
2026-04-16 14:05:39 +0530

    Time: 53 ms (88.89%) | Memory: 188.8 MB (36.36%) - LeetSync

--- BinarySearch/1DArrays/Medium/3750-closest-equal-element-queries/closest-equal-element-queries.java
+++ BinarySearch/1DArrays/Medium/3750-closest-equal-element-queries/closest-equal-element-queries.java
@@ -1,13 +1,53 @@
 class Solution {
     /**
-     * Approach II : Using Optimal (Hashing + Binary Search) Approach
+     * Approach III : Using Optimal (Hashing) Approach
+     *
+     * TC: O(n) + O(n) + O(m) ~ O(n + m)
+     * SC: O(n) + O(n) ~ O(n)
+     *
+     * Accepted (614 / 614 testcases passed)
+     */
+    public List<Integer> solveQueries(int[] nums, int[] queries) {
+        int n = nums.length;
+        Map<Integer, ArrayList<Integer>> map = new HashMap<>(); // SC: O(n)
+        for (int i = 0; i < n; i++) { // TC: O(n)
+            map.computeIfAbsent(nums[i], k -> new ArrayList<>()).add(i);
+        }
+        int[] ans = new int[n]; // SC: O(n)
+        Arrays.fill(ans, -1);
+        for (ArrayList<Integer> list : map.values()) { // TC: O(k)
+            int size = list.size();
+            if (size == 1) {
+                // the key value is unique only
+                continue;
+            }
+            for (int j = 0; j < size; j++) { // TC: O(n / k)
+                int current = list.get(j);
+                int prev = list.get((j - 1 + size) % size);
+                int next = list.get((j + 1) % size);
+                int dist1 = Math.abs(current - prev);
+                dist1 = Math.min(dist1, n - dist1);
+                int dist2 = Math.abs(current - next);
+                dist2 = Math.min(dist2, n - dist2);
+                ans[current] = Math.min(dist1, dist2);
+            }
+        }
+        List<Integer> result = new ArrayList<Integer>();
+        for (int q : queries) { // TC: O(m)
+            result.add(ans[q]);
+        }
+        return result;
+    }
+
+    /**
+     * Approach II : Using Better (Hashing + Binary Search) Approach
      *
      * TC: O(n) + O(q x log(n)) ~ O(n + q x log(n))
      * SC: O(n)
      *
      * Accepted (614 / 614 testcases passed)
      */
-    public List<Integer> solveQueries(int[] nums, int[] queries) {
+    public List<Integer> solveQueriesBetter(int[] nums, int[] queries) {
         int n = nums.length;
         int m = queries.length;
         Map<Integer, ArrayList<Integer>> map = new HashMap<>(); // SC: O(n)
