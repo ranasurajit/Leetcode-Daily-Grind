c7a681d3ee71edbdd3eb144ae1adc2666e67394a
Surajit Rana <surajit1602@gmail.com>
2026-04-18 11:00:17 +0530

    Time: 1 ms (100.00%) | Memory: 42.6 MB (65.56%) - LeetSync

--- Math/Easy/4168-mirror-distance-of-an-integer/mirror-distance-of-an-integer.java
+++ Math/Easy/4168-mirror-distance-of-an-integer/mirror-distance-of-an-integer.java
@@ -1,6 +1,6 @@
 class Solution {
     /**
-     * Approach I :  Using Math + Simulation Approach
+     * Approach II :  Using Optimal (Math + Simulation) Approach
      *
      * TC: O(9) ~ O(1)
      * SC: O(1)
@@ -9,6 +9,30 @@ class Solution {
      * maximum 9 times the loop would run
      */
     public int mirrorDistance(int n) {
+        int d = (int) Math.log10(n); // compute highest power of 10
+        int rightPos = 1;
+        int leftPos = (int) Math.pow(10, d);
+        int dist = 0;
+        while (n > 0) { // TC: O(9)
+            int rem = n % 10;
+            dist += rem * (rightPos - leftPos);
+            rightPos *= 10;
+            leftPos /= 10;
+            n /= 10;
+        }
+        return Math.abs(dist);
+    }
+
+    /**
+     * Approach I :  Using Brute-Force (Math + Simulation) Approach
+     *
+     * TC: O(9) ~ O(1)
+     * SC: O(1)
+     *
+     * as per constraints: 1 <= n <= 10⁹, 
+     * maximum 9 times the loop would run
+     */
+    public int mirrorDistanceBruteForce(int n) {
         int original = n;
         int reverse = 0;
         while (n > 0) { // TC: O(9)
