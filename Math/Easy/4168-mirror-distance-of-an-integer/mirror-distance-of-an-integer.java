0966687b486336df47990767aa7b91735ba47084
Surajit Rana <surajit1602@gmail.com>
2026-04-18 10:29:51 +0530

    Time: 1 ms (100.00%) | Memory: 42.6 MB (48.72%) - LeetSync

---
+++ Math/Easy/4168-mirror-distance-of-an-integer/mirror-distance-of-an-integer.java
@@ -0,0 +1,22 @@
+class Solution {
+    /**
+     * Approach : Using Math + Simulation Approach
+     *
+     * TC: O(9) ~ O(1)
+     * SC: O(1)
+     *
+     * as per constraints: 1 <= n <= 10⁹, 
+     * maximum 9 times the loop would run
+     */
+    public int mirrorDistance(int n) {
+        int original = n;
+        int reverse = 0;
+        while (n > 0) { // TC: O(9)
+            int rem = n % 10;
+            n = n / 10;
+            reverse = reverse * 10 + rem;
+        }
+        int result = original - reverse;
+        return result < 0 ? -1 * result : result;
+    }
+}
