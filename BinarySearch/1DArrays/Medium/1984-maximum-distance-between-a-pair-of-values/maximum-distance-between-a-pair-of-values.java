449af128a4616cdd5222638c958d7d6f607dc59b
Surajit Rana <surajit1602@gmail.com>
2026-04-19 19:06:18 +0530

    Time: 20 ms (13.90%) | Memory: 89.9 MB (90.51%) - LeetSync

---
+++ BinarySearch/1DArrays/Medium/1984-maximum-distance-between-a-pair-of-values/maximum-distance-between-a-pair-of-values.java
@@ -0,0 +1,72 @@
+class Solution {
+    /**
+     * Approach II : Using Optimal approach (Binary Search) Approach
+     *
+     * TC : O(m x log(n))
+     * SC : O(1)
+     *
+     * Accepted (32 / 32 testcases passed)
+     */
+    public int maxDistance(int[] nums1, int[] nums2) {
+        int m = nums1.length;
+        int n = nums2.length;
+        int maxDist = 0;
+        for (int i = 0; i < m; i++) { // TC: O(m)
+            /**
+             * we need to find the farthest index 'j' in 'nums2'
+             * such that j >= i and  nums1[i] <= nums2[j] so
+             * we can perform Binary Search on 'nums2'
+             */
+            if (i < n) {
+                int j = findFarthestIndex(nums2, i,
+                    n - 1, nums1[i]); // TC : O(log(n))
+                maxDist = Math.max(maxDist, j - i);
+            }
+        }
+        return maxDist;
+    }
+
+    /**
+     * Using Binary Search Approach
+     *
+     * TC : O(log(n))
+     * SC : O(1)
+     */
+    private int findFarthestIndex(int[] nums, int low, int high, int target) {
+        int farthest = low;
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+            if (nums[mid] < target) {
+                // we need to move towards left
+                high = mid - 1;
+            } else {
+                // we need to move right to get farthest index
+                farthest = Math.max(farthest, mid);
+                low = mid + 1;
+            }
+        }
+        return farthest;
+    }
+
+    /**
+     * Approach I : Using Brute-Force (Array Simulation) Approach
+     *
+     * TC : O(m x n)
+     * SC : O(1)
+     *
+     * Time Limit Exceeded (22 / 32 testcases passed)
+     */
+    public int maxDistanceBruteForce(int[] nums1, int[] nums2) {
+        int m = nums1.length;
+        int n = nums2.length;
+        int maxDist = 0;
+        for (int i = 0; i < m; i++) {     // TC: O(m)
+            for (int j = i; j < n; j++) { // TC: O(n)
+                if (nums1[i] <= nums2[j]) {
+                    maxDist = Math.max(maxDist, j - i);
+                }
+            }
+        }
+        return maxDist;
+    }
+}
