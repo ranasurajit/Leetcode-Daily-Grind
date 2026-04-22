39fde759c310996ac8118300f13eda9eba83d40a
Surajit Rana <surajit1602@gmail.com>
2026-04-22 10:44:53 +0530

    Time: 13 ms (36.76%) | Memory: 44.2 MB (84.56%) - LeetSync

---
+++ Tries/Medium/2550-words-within-two-edits-of-dictionary/words-within-two-edits-of-dictionary.java
@@ -0,0 +1,31 @@
+class Solution {
+    /**
+     * Approach : Using Brute-Force Approach
+     *
+     * TC : O(q x n x n) ~ O(q x n²)
+     * SC : O(1)
+     */
+    public List<String> twoEditWords(String[] queries, String[] dictionary) {
+        List<String> result = new ArrayList<>();
+        for (String query : queries) {       // TC : O(q)
+            for (String word : dictionary) { // TC: O(n)
+                int p = 0; // pointer at the start of String 'query'
+                int q = 0; // pointer at the start of String 'word'
+                int n = word.length();
+                int edits = 0;
+                while (p < n && q < n) { // TC: O(2 x n)
+                    if (query.charAt(p) != word.charAt(q)) {
+                        edits++;
+                    }
+                    p++;
+                    q++;
+                }
+                if (edits <= 2) {
+                    result.add(query);
+                    break;
+                }
+            }
+        }
+        return result;
+    }
+}
