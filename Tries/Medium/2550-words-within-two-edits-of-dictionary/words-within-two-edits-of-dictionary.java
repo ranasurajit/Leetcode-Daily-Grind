173fe586dab3d814f5ae80dbc6d0a8002494c832
Surajit Rana <surajit1602@gmail.com>
2026-04-22 10:50:05 +0530

    Time: 5 ms (78.68%) | Memory: 43.9 MB (98.53%) - LeetSync

--- Tries/Medium/2550-words-within-two-edits-of-dictionary/words-within-two-edits-of-dictionary.java
+++ Tries/Medium/2550-words-within-two-edits-of-dictionary/words-within-two-edits-of-dictionary.java
@@ -2,26 +2,24 @@ class Solution {
     /**
      * Approach : Using Brute-Force Approach
      *
-     * TC : O(q x n x n) ~ O(q x n²)
+     * TC : O(q x d x n)
      * SC : O(1)
      */
     public List<String> twoEditWords(String[] queries, String[] dictionary) {
         List<String> result = new ArrayList<>();
         for (String query : queries) {       // TC : O(q)
-            for (String word : dictionary) { // TC: O(n)
+            for (String word : dictionary) { // TC : O(d)
                 int p = 0; // pointer at the start of String 'query'
                 int q = 0; // pointer at the start of String 'word'
                 int n = word.length();
                 int edits = 0;
-                while (p < n && q < n) { // TC: O(2 x n)
-                    if (query.charAt(p) != word.charAt(q)) {
+                for (int i = 0; i < n; i++) { // TC : O(n)
+                    if (query.charAt(i) != word.charAt(i)) {
                         edits++;
                         if (edits > 2) {
                             break;
                         }
                     }
-                    p++;
-                    q++;
                 }
                 if (edits <= 2) {
                     result.add(query);
