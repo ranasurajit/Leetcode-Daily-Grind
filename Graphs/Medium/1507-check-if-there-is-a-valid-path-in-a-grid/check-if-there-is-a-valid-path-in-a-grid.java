e930dae87a366a90180e8a30ecaba67db9b37ad6
Surajit Rana <surajit1602@gmail.com>
2026-04-27 16:37:17 +0530

    Time: 16 ms (83.37%) | Memory: 94.5 MB (62.80%) - LeetSync

---
+++ Graphs/Medium/1507-check-if-there-is-a-valid-path-in-a-grid/check-if-there-is-a-valid-path-in-a-grid.java
@@ -0,0 +1,75 @@
+class Solution {
+    private static final int[][][] directions = {
+        {{ 0, -1 }, { 0, 1 }}, // street 1
+        {{ -1, 0 }, { 1, 0 }}, // street 2
+        {{ 0, -1 }, { 1, 0 }}, // street 3
+        {{ 1, 0 }, { 0, 1 }}, // street 4
+        {{ 0, -1 }, { -1, 0 }}, // street 5
+        {{ -1, 0 }, { 0, 1 }} // street 6
+    };
+
+    private int m;
+    private int n;
+    private int[][] grid;
+
+    /**
+     * Approach : Using DFS Approach
+     *
+     * TC : O(m + n)
+     * SC : O(m + n) + O(m x n) ~ O(m x n)
+     */
+    public boolean hasValidPath(int[][] grid) {
+        this.m = grid.length;
+        this.n = grid[0].length;
+        this.grid = grid;
+        boolean[][] visited = new boolean[m][n]; // SC : O(m x n)
+        if (dfsGrid(0, 0, visited)) { // TC : O(m + n), SC : O(m + n) 
+            return true;
+        }
+        return false;
+    }
+
+    /**
+     * Using DFS Approach
+     *
+     * TC : O(4 x (m + n)) ~ O(m + n)
+     * SC : O(m + n)
+     */
+    private boolean dfsGrid(int i, int j, boolean[][] visited) {
+        if (i == m - 1 && j == n - 1) {
+            // reached destination cell
+            return true;
+        }
+        visited[i][j] = true;
+        int streetType = grid[i][j] - 1;
+        int[][] dir = directions[streetType];
+        for (int[] d : dir) { // TC : O(2)
+            int x = i + d[0];
+            int y = j + d[1];
+            if (x < 0 || x >= m || y < 0 || y >= n) {
+                // invalid cells
+                continue;
+            }
+            if (visited[x][y]) {
+                // already visited
+                continue;
+            }
+            int newStreetType = grid[x][y] - 1;
+            int[][] newDir = directions[newStreetType];
+            for (int[] nd : newDir) { // TC : O(2)
+                int effX = x + nd[0];
+                int effY = y + nd[1];
+                if (effX == i && effY == j) {
+                    /**
+                     * If we can go back from (x, y) to previous cell 
+                     * (i, j) from where we came then it forms a path
+                     */
+                    if (dfsGrid(x, y, visited)) {
+                        return true;
+                    }
+                }
+            }
+        }
+        return false;
+    }
+}
