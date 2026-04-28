f8c1a9c543106c8685f24864848e748daa0db2ea
Surajit Rana <surajit1602@gmail.com>
2026-04-28 11:55:00 +0530

    Time: 42 ms (70.99%) | Memory: 110 MB (74.81%) - LeetSync

--- Arrays/Sorting/Medium/2160-minimum-operations-to-make-a-uni-value-grid/minimum-operations-to-make-a-uni-value-grid.java
+++ Arrays/Sorting/Medium/2160-minimum-operations-to-make-a-uni-value-grid/minimum-operations-to-make-a-uni-value-grid.java
@@ -1,12 +1,54 @@
 class Solution {
     /**
-     * Approach : Using Sorting + Simulation Approach
+     * Approach II : Using Sorting + Simulation (Cleaner) Approach
      *
-     * TC : O(m x n) +O(m x n) + O((m x n) x log(m x n)) + O(m x n)
+     * TC : O(m x n) + O((m x n) x log(m x n)) + O(m x n)
      *      ~ O((m x n) x log(m x n))
      * SC : O(m x n)
      */
     public int minOperations(int[][] grid, int x) {
+        int m = grid.length;
+        int n = grid[0].length;
+        /**
+         * Now we need to sort the elements after making it 1D
+         */
+        int[] flat = new int[m * n]; // SC : O(m x n)
+        int index = 0;
+        for (int i = 0; i < m; i++) {     // TC : O(m)
+            for (int j = 0; j < n; j++) { // TC : O(n)
+                flat[index++] = grid[i][j];
+            }
+        }
+        Arrays.sort(flat); // TC : O((m x n) x log(m x n))
+        int median = flat[(m * n) / 2];
+        /**
+         * A grid can be operated to make uni-value 
+         * if and only if difference between any 2
+         * elements is divisible by x, so all elements
+         * should share the same remainder by x
+         *
+         * Now we need to compute operations for every
+         * element which is operations += abs(list.get(i) - median) / x
+         */
+        int operations = 0;
+        int rem = flat[0] % x;
+        for (int it : flat) { // TC : O(m x n)
+            if (it % x != rem) {
+                return -1;
+            }
+            operations += Math.abs(it - median) / x;
+        }
+        return operations;
+    }
+
+    /**
+     * Approach I : Using Sorting + Simulation Approach
+     *
+     * TC : O(m x n) + O(m x n) + O((m x n) x log(m x n)) + O(m x n)
+     *      ~ O((m x n) x log(m x n))
+     * SC : O(m x n)
+     */
+    public int minOperationsApproachI(int[][] grid, int x) {
         int m = grid.length;
         int n = grid[0].length;
         /**
