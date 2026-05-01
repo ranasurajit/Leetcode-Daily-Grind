d40f9e8bf209f2a4acf50825e22ac0323f7f0cb6
Surajit Rana <surajit1602@gmail.com>
2026-05-01 13:57:52 +0530

    Time: 4 ms (90.35%) | Memory: 90.6 MB (32.96%) - LeetSync

---
+++ Math/Medium/396-rotate-function/rotate-function.java
@@ -0,0 +1,54 @@
+class Solution {
+    /**
+     * Approach II : Using Optimal (Math + Array Simulation) Approach
+     *
+     * TC : O(n) + O(n) ~ O(n)
+     * SC : O(1)
+     *
+     * Accepted (58 / 58 testcases passed)
+     */
+    public int maxRotateFunction(int[] nums) {
+        int n = nums.length;
+        int maxFn = Integer.MIN_VALUE;
+        int currentFn = 0;
+        /**
+         * Firstly we can get the sum of F(0)
+         * so, currentFn = F(0)
+         */
+        int total = 0;
+        for (int i = 0; i < n; i++) { // TC : O(n)
+            currentFn += (nums[i] * i);
+            total += nums[i];
+        }
+        maxFn = Math.max(maxFn, currentFn);
+        for (int i = n - 1; i > 0; i--) { // TC : O(n)
+            // here element at index 'i' is the start of rotated array
+            currentFn += (total - nums[i]) - ((n - 1) * nums[i]);
+            maxFn = Math.max(maxFn, currentFn);
+        }
+        return maxFn == Integer.MIN_VALUE ? 0 : maxFn;
+    }
+
+    /**
+     * Approach I : Using Brute-Force (Array Simulation) Approach
+     *
+     * TC : O(n²)
+     * SC : O(1)
+     *
+     * Time Limit Exceeded (45 / 58 testcases passed)
+     */
+    public int maxRotateFunctionBruteForce(int[] nums) {
+        int n = nums.length;
+        int maxFn = Integer.MIN_VALUE;
+        for (int start = 0; start < n; start++) {     // TC : O(n)
+            int mult = 0;
+            int currentFn = 0;
+            for (int i = start; i < n + start; i++) { // TC : O(n)
+                currentFn += nums[i % n] * mult;
+                mult++;
+            }
+            maxFn = Math.max(maxFn, currentFn);
+        }
+        return maxFn;
+    }
+}
