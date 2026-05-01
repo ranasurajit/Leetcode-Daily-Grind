2f3cd6cfead03a5d1dc9422ce91f7fb860c0ab75
Surajit Rana <surajit1602@gmail.com>
2026-05-01 14:03:08 +0530

    Time: 4 ms (90.35%) | Memory: 90.4 MB (82.46%) - LeetSync

--- Math/Medium/396-rotate-function/rotate-function.java
+++ Math/Medium/396-rotate-function/rotate-function.java
@@ -9,7 +9,6 @@ class Solution {
      */
     public int maxRotateFunction(int[] nums) {
         int n = nums.length;
-        int maxFn = Integer.MIN_VALUE;
         int currentFn = 0;
         /**
          * Firstly we can get the sum of F(0)
@@ -20,7 +19,7 @@ class Solution {
             currentFn += (nums[i] * i);
             total += nums[i];
         }
-        maxFn = Math.max(maxFn, currentFn);
+        int maxFn = currentFn;
         for (int i = n - 1; i > 0; i--) { // TC : O(n)
             // here element at index 'i' is the start of rotated array
             currentFn += total - (n * nums[i]);
