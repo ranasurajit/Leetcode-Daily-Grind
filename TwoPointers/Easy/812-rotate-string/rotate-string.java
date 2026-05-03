aa729834f1a62bb64f54dd3fac44ed4c6fee5ff9
Surajit Rana <surajit1602@gmail.com>
2026-05-03 14:18:53 +0530

    Time: 0 ms (100.00%) | Memory: 42.9 MB (42.97%) - LeetSync

--- TwoPointers/Easy/812-rotate-string/rotate-string.java
+++ TwoPointers/Easy/812-rotate-string/rotate-string.java
@@ -1,11 +1,41 @@
 class Solution {
     /**
-     * Approach : Using Two Pointers Approach
+     * Approach II : Using Optimal Two Pointers Approach
+     *
+     * TC : O(2 x n) ~ O(n)
+     * SC : O(1)
+     */
+    public boolean rotateString(String s, String goal) {
+        int n = s.length();
+        int m = goal.length();
+        if (m != n) {
+            // not possible for any number of rotations
+            return false;
+        }
+        s = s + s;
+        int i = 0; // pointer at the start index of String 's'
+        int j = 0; // pointer at the start index of String 'goal'
+        while (i < 2 * n) { // TC : O(2 x n)
+            while (i < 2 * n && j < n && s.charAt(i) == goal.charAt(j)) {
+                i++;
+                j++;
+            }
+            if (j == n) {
+                return true;
+            }
+            i = i - j + 1;
+            j = 0;
+        }
+        return false;
+    }
+
+    /**
+     * Approach I : Using Brute-Force (Two Pointers) Approach
      *
      * TC : O(n²)
      * SC : O(1)
      */
-    public boolean rotateString(String s, String goal) {
+    public boolean rotateStringBruteForce(String s, String goal) {
         int m = s.length();
         int n = goal.length();
         if (m != n) {
