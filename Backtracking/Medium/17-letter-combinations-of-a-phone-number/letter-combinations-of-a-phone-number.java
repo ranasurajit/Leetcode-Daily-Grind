ccfac10d0bcb0e810489c587c1d526aeb2494c97
Surajit Rana <surajit1602@gmail.com>
2026-05-04 23:51:20 +0530

    Time: 0 ms (100.00%) | Memory: 43.5 MB (66.16%) - LeetSync

--- Backtracking/Medium/17-letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.java
+++ Backtracking/Medium/17-letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.java
@@ -1,51 +1,58 @@
 class Solution {
-    private static final String[] keys = 
-        { "", "", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz" };
-    private List<String> result = new ArrayList<String>();
+    private static final char[][] keys = {
+        { },
+        { },
+        { 'a', 'b', 'c' },
+        { 'd', 'e', 'f' },
+        { 'g', 'h', 'i' },
+        { 'j', 'k', 'l' },
+        { 'm', 'n', 'o' },
+        { 'p', 'q', 'r', 's' },
+        { 't', 'u', 'v' },
+        { 'w', 'x', 'y', 'z' }
+    };
+
+    private Map<Integer, char[]> keysMap;
 
     /**
-     * Approach : Using Backtracking Approach
+     * Using Backtracking Approach
      *
-     * TC: O(N x 4 ^ N) 
+     * TC : O(4ⁿ)
+     * SC : O(n) + O(n) ~ O(n)
      *
-     * as for a keypad key maximum 4 loops happen
-     * For example keypad 9 has 4 options (w, x, y and z)
-     *
-     * SC: O(N)
+     * - O(n) - recursion stack
      */
     public List<String> letterCombinations(String digits) {
         int n = digits.length();
-        StringBuilder sb = new StringBuilder();
-        solve(0, n, digits, sb);
+        this.keysMap = new HashMap<>(); // SC : O(10)
+        for (int i = 0; i < keys.length; i++) { // TC : O(10) ~ O(1)
+            keysMap.put(i, keys[i]);
+        }
+        List<String> result = new ArrayList<>();
+        StringBuilder sb = new StringBuilder(); // SC : O(n)
+        solve(0, n, digits, sb, result); // TC : O(4ⁿ), SC : O(n)
         return result;
     }
 
     /**
      * Using Backtracking Approach
      *
-     * TC: O(N x 4 ^ N) 
-     *
-     * as for a keypad key maximum 4 loops happen
-     * For example keypad 9 has 4 options (w, x, y and z)
-     *
-     * SC: O(N)
+     * TC : O(4ⁿ)
+     * SC : O(n)
      */
-    private void solve(int idx, int n, String digits, StringBuilder sb) {
+    private void solve(int idx, int n, String digits, 
+        StringBuilder sb, List<String> result) {
         // Base Case
         if (idx == n) {
             result.add(sb.toString());
             return;
         }
         // Recursion Calls
-        char c = digits.charAt(idx);
-        String currentKey = keys[c - '0'];
-        for (int i = 0; i < currentKey.length(); i++) { // TC: O(4)
-            // pick
-            char ch = currentKey.charAt(i);
-            int size = sb.length();
-            sb.append(ch);
-            solve(idx + 1, n, digits, sb); // explore
-            sb.setLength(size); // backtrack
-        }    
+        int currentKey = digits.charAt(idx) - '0';
+        for (char ch : keysMap.get(currentKey)) { // TC : O(4)
+            sb.append(ch); // modify
+            solve(idx + 1, n, digits, sb, result); // explore
+            sb.setLength(sb.length() - 1); // backtrack
+        }
     }
 }
