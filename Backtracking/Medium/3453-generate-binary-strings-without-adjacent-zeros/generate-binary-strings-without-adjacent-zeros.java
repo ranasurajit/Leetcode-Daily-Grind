114441ace21809937e08d0d6e1096760c3cc46e1
Surajit Rana <surajit1602@gmail.com>
2026-05-06 00:39:55 +0530

    Time: 2 ms (91.11%) | Memory: 47.8 MB (26.17%) - LeetSync

--- Backtracking/Medium/3453-generate-binary-strings-without-adjacent-zeros/generate-binary-strings-without-adjacent-zeros.java
+++ Backtracking/Medium/3453-generate-binary-strings-without-adjacent-zeros/generate-binary-strings-without-adjacent-zeros.java
@@ -1,11 +1,26 @@
 class Solution {
+    /**
+     * Using Backtracking Approach
+     *
+     * TC : O(2ⁿ)
+     * SC : O(n) + O(n) ~ O(n)
+     *
+     * - O(n) - recursion stack
+     * - O(n) - StringBuilder memory
+     */
     public List<String> validStrings(int n) {
         List<String> result = new ArrayList<>();
-        StringBuilder sb = new StringBuilder();
-        solve(n, sb, result);
+        StringBuilder sb = new StringBuilder(); // SC : O(n)
+        solve(n, sb, result); // TC : O(2ⁿ), SC : O(n)
         return result;
     }
 
+    /**
+     * Using Backtracking Approach
+     *
+     * TC : O(2 x 2ⁿ) ~ O(2ⁿ)
+     * SC : O(n)
+     */
     private void solve(int n, StringBuilder sb, List<String> result) {
         // Base Case
         if (sb.length() == n) {
@@ -13,7 +28,7 @@ class Solution {
             return;
         }
         // Recursion Calls
-        for (int i = 0; i <= 1; i++) {
+        for (int i = 0; i <= 1; i++) { // TC : O(2)
             // choice for 0
             if (i == 0) {
                 if (sb.isEmpty() || sb.charAt(sb.length() - 1) != '0') {
