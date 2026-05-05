43044a3a8fe850a0ce5f453037ae90d7cb4fe8af
Surajit Rana <surajit1602@gmail.com>
2026-05-06 02:02:51 +0530

    Time: 2 ms (37.80%) | Memory: 45.7 MB (17.32%) - LeetSync

--- Backtracking/Medium/46-permutations/permutations.java
+++ Backtracking/Medium/46-permutations/permutations.java
@@ -1,83 +1,43 @@
 class Solution {
     /**
-     * Approach II : Using Backtracking (Template 1) Approach
+     * Approach : Using Backtracking Approach
      *
-     * TC: O(N x N!)
-     * SC: O(N!)
+     * TC : O(n x n!)
+     * SC : O(n!) + O(n)
+     *
+     * - O(n!) - current arraylist memory
+     * - O(n) - recursion stack 
      */
     public List<List<Integer>> permute(int[] nums) {
         int n = nums.length;
-        List<List<Integer>> result = new ArrayList<List<Integer>>();
-        solveBetter(0, n, nums, result);
+        List<List<Integer>> result = new ArrayList<>();
+        List<Integer> current = new ArrayList<>(); // SC : O(n!)
+        Set<Integer> hs = new HashSet<>(); // SC : O(n)
+        solve(n, nums, current, hs, result);
         return result;
     }
 
     /**
      * Using Backtracking Approach
      *
-     * TC: O(N x N!)
-     * SC: O(N!)
+     * TC : O(n x n!)
+     * SC : O(n)
      */
-    private void solveBetter(int idx, int n, int[] nums, List<List<Integer>> result) {
-        // Base Case
-        if (idx == n) {
-            List<Integer> current = new ArrayList<Integer>();
-            for (int i = 0; i < n; i++) { // TC: O(N)
-                current.add(nums[i]);
-            }
-            result.add(current);
-            return;
-        }
-        // Recursion Calls
-        for (int i = idx; i < n; i++) { // TC: O(N)
-            swap(nums, idx, i); // modify
-            solveBetter(idx + 1, n, nums, result); // explore
-            swap(nums, idx, i); // backtrack
-        }
-    }
-
-    private void swap(int[] nums, int a, int b) {
-        int temp = nums[a];
-        nums[a] = nums[b];
-        nums[b] = temp;
-    }
-
-    /**
-     * Approach I : Using Backtracking (Template 1) Approach
-     *
-     * TC: O(N x N!)
-     * SC: O(N!)
-     */
-    public List<List<Integer>> permuteTemplate1(int[] nums) {
-        int n = nums.length;
-        List<List<Integer>> result = new ArrayList<List<Integer>>();
-        List<Integer> current = new ArrayList<Integer>();
-        Set<Integer> hs = new HashSet<Integer>();
-        solve(n, nums, current, result, hs);
-        return result;
-    }
-
-    /**
-     * Using Backtracking Approach
-     *
-     * TC: O(N x N!)
-     * SC: O(N!)
-     */
-    private void solve(int n, int[] nums, 
-        List<Integer> current, List<List<Integer>> result, Set<Integer> hs) {
+    private void solve(int n, int[] nums, List<Integer> current,
+        Set<Integer> hs, List<List<Integer>> result) {
         // Base Case
         if (current.size() == n) {
-            result.add(new ArrayList<Integer>(current));
+            result.add(new ArrayList<>(current));
             return;
         }
         // Recursion Calls
-        for (int i = 0; i < n; i++) { // TC: O(N)
-            if (hs.isEmpty() || !hs.contains(nums[i])) {
-                current.add(nums[i]); // modify
-                hs.add(nums[i]);      // modify
-                solve(n, nums, current, result, hs); // explore
+        for (int j = 0; j < n; j++) { // TC : O(n)
+            if (!hs.contains(nums[j])) {
+                current.add(nums[j]); // modify
+                hs.add(nums[j]); // modify
+                solve(n, nums, current, hs, result); // explore
                 current.remove(current.size() - 1); // backtrack
-                hs.remove(nums[i]);                 // backtrack
+                hs.remove(nums[j]); // backtrack
             }
         }
     }
