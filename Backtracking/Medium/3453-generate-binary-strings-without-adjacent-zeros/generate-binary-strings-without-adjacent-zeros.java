ec518626cb8edeaeaffbf5d47ea8c56d642c4be7
Surajit Rana <surajit1602@gmail.com>
2026-05-06 00:41:15 +0530

    Time: 2 ms (91.11%) | Memory: 47.9 MB (15.31%) - LeetSync

--- Backtracking/Medium/3453-generate-binary-strings-without-adjacent-zeros/generate-binary-strings-without-adjacent-zeros.java
+++ Backtracking/Medium/3453-generate-binary-strings-without-adjacent-zeros/generate-binary-strings-without-adjacent-zeros.java
@@ -18,7 +18,7 @@ class Solution {
     /**
      * Using Backtracking Approach
      *
-     * TC : O(2 x 2ⁿ) ~ O(2ⁿ)
+     * TC : O(2ⁿ)
      * SC : O(n)
      */
     private void solve(int n, StringBuilder sb, List<String> result) {
@@ -28,19 +28,14 @@ class Solution {
             return;
         }
         // Recursion Calls
-        for (int i = 0; i <= 1; i++) { // TC : O(2)
-            // choice for 0
-            if (i == 0) {
-                if (sb.isEmpty() || sb.charAt(sb.length() - 1) != '0') {
-                    sb.append(i); // modify
-                    solve(n, sb, result); // explore
-                    sb.setLength(sb.length() - 1); // backtrack
-                }
-            } else {
-                sb.append(i); // modify
-                solve(n, sb, result); // explore
-                sb.setLength(sb.length() - 1); // backtrack
-            }
+        // try using '1'
+        sb.append(1); // modify
+        solve(n, sb, result); // explore
+        sb.setLength(sb.length() - 1); // backtrack
+        if (sb.isEmpty() || sb.charAt(sb.length() - 1) != '0') {
+            sb.append(0); // modify
+            solve(n, sb, result); // explore
+            sb.setLength(sb.length() - 1); // backtrack
         }
     }
 }
