4e283df8a4b1791cb3f4fb8070e8d6dcc87e0327
Surajit Rana <surajit1602@gmail.com>
2026-05-06 01:27:02 +0530

    Time: 1 ms (88.85%) | Memory: 44 MB (87.10%) - LeetSync

--- Backtracking/Medium/78-subsets/subsets.java
+++ Backtracking/Medium/78-subsets/subsets.java
@@ -2,34 +2,39 @@ class Solution {
     /**
      * Approach : Using Backtracking Approach
      *
-     * TC: O(2 ^ N)
-     * SC: O(N) + O(2 ^ N)
+     * TC : O(2ⁿ)
+     * SC : O(n) + O(n) ~ O(n)
+     *
+     * - O(n) - recursion stack
+     * - O(n) - current arraylist memory
      */
     public List<List<Integer>> subsets(int[] nums) {
         int n = nums.length;
-        List<List<Integer>> result = new ArrayList<List<Integer>>();
-        List<Integer> current = new ArrayList<Integer>(); // SC: O(2 ^ N)
-        solve(0, n, nums, current, result); // TC: O(2 ^ N), SC: O(N)
+        List<List<Integer>> result = new ArrayList<>();
+        List<Integer> current = new ArrayList<>(); // SC : O(n)
+        solve(0, n, nums, current, result); // TC : O(2ⁿ), SC : O(n)
         return result;
     }
 
     /**
      * Using Backtracking Approach
      *
-     * TC: O(2 ^ N)
-     * SC: O(N)
+     * TC : O(2ⁿ)
+     * SC : O(n)
      */
-    private void solve(int idx, int n, int[] nums,
-        List<Integer> current, List<List<Integer>> result) {
+    private void solve(int idx, int n, int[] nums, List<Integer> current,
+        List<List<Integer>> result) {
         // Base Case
         if (idx == n) {
-            result.add(new ArrayList<Integer>(current));
+            result.add(new ArrayList<>(current));
             return;
         }
         // Recursion Calls
+        // skip element at index 'idx'
+        solve(idx + 1, n, nums, current, result); // explore
+        // pick element at index 'idx'
         current.add(nums[idx]); // modify
-        solve(idx + 1, n, nums, current, result); // explore - pick
+        solve(idx + 1, n, nums, current, result); // explore
         current.remove(current.size() - 1); // backtrack
-        solve(idx + 1, n, nums, current, result); // explore - skip
     }
 }
