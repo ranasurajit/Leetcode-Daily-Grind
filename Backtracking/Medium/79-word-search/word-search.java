40a487cf70022ee54fbe023a6bb64f1b59021764
Surajit Rana <surajit1602@gmail.com>
2026-05-09 00:26:51 +0530

    Time: 175 ms (24.90%) | Memory: 42.6 MB (93.90%) - LeetSync

--- Backtracking/Medium/79-word-search/word-search.java
+++ Backtracking/Medium/79-word-search/word-search.java
@@ -1,25 +1,30 @@
 class Solution {
+    private static final int[][] directions = {
+        { -1, 0 }, { 0, -1 }, { 1, 0 }, { 0, 1 }
+    };
+
     private int m;
     private int n;
-    private static final int[][] directions = { { -1, 0 }, { 0, -1 }, { 1, 0 }, { 0, 1 } };
 
     /**
      * Approach : Using Backtracking Approach
      *
-     * TC: O(M x N x 3 ^ L)
-     * from every cell we go to 3 directions 
-     * (1 direction is negated as we came from that)
-     * 
-     * SC: O(L), where L = size(word)
+     * TC : O(m x n x 3 ^ l))
+     * SC : O(l)
+     * - O(l) - recursion stack
+     *
+     * where l = size(word)
      */
     public boolean exist(char[][] board, String word) {
         this.m = board.length;
         this.n = board[0].length;
-        for (int i = 0; i < m; i++) {     // TC: O(M)
-            for (int j = 0; j < n; j++) { // TC: O(N)
-                if (board[i][j] == word.charAt(0) && 
-                    backtrack(i, j, 0, word, board)) { // TC: O(3 ^ L), SC: O(L)
-                    return true;
+        for (int i = 0; i < m; i++) {     // TC : O(m)
+            for (int j = 0; j < n; j++) { // TC : O(n)
+                if (board[i][j] == word.charAt(0)) {
+                    // we have the 1st match so we can explore if further
+                    if (backtrack(i, j, board, 0, word)) {
+                        return true;
+                    }
                 }
             }
         }
@@ -29,31 +34,27 @@ class Solution {
     /**
      * Using Backtracking Approach
      *
-     * TC: O(M x N x 3 ^ L)
-     * from every cell we go to 3 directions 
-     * (1 direction is negated as we came from that)
-     * 
-     * SC: O(L), where L = size(word)
+     * TC : O(3 ^ l)
+     * - at most we explore 3 directions from any cell
+     *   (excluded the direction it came from)
+     * SC : O(l)
      */
-    private boolean backtrack(int i, int j, int idx, String word, char[][] board) {
+    private boolean backtrack(int i, int j, char[][] board, int idx, String word) {
         // Base Case
         if (idx == word.length()) {
-            // we reached the end of word so all characters matches
             return true;
         }
-        if (i < 0 || i >= m || j < 0 || j >= n || board[i][j] == '$' || 
+        if (i < 0 || i >= m || j < 0 || j >= n || board[i][j] == '$' ||
             board[i][j] != word.charAt(idx)) {
-            // invalid cases (out of bound, already visited cell or no match)
             return false;
         }
         // Recursion Calls
-        // here board[i][j] matches word.charAt(idx)
-        char temp = board[i][j]; // storing the value to backtrack later
-        board[i][j] = '$'; // marking cell as visited
-        for (int[] dir : directions) { // TC: O(4)
-            int effX = i + dir[0];
-            int effY = j + dir[1];
-            if (backtrack(effX, effY, idx + 1, word, board)) {
+        char temp = board[i][j];
+        board[i][j] = '$';
+        for (int[] dir : directions) { // TC : O(4)
+            int effI = i + dir[0];
+            int effJ = j + dir[1];
+            if (backtrack(effI, effJ, board, idx + 1, word)) {
                 return true;
             }
         }
