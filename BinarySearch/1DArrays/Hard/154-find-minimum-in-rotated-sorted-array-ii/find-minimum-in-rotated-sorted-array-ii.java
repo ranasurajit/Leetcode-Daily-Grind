a4e1a3ffcb614b816ffb69fa85cfc94a940a35e4
Surajit Rana <surajit1602@gmail.com>
2026-05-16 12:49:49 +0530

    Time: 0 ms (100.00%) | Memory: 44.3 MB (97.91%) - LeetSync

--- BinarySearch/1DArrays/Hard/154-find-minimum-in-rotated-sorted-array-ii/find-minimum-in-rotated-sorted-array-ii.java
+++ BinarySearch/1DArrays/Hard/154-find-minimum-in-rotated-sorted-array-ii/find-minimum-in-rotated-sorted-array-ii.java
@@ -7,30 +7,19 @@ class Solution {
      * SC : O(1)
      */
     public int findMin(int[] nums) {
-        int n = nums.length;
-        int low = 0;
-        int high = n - 1;
-        int resultIdx = 0;
-        while (low <= high) { // TC : O(log(n))
-            // skipping duplicates from left
-            while (low < high && nums[low] == nums[low + 1]) {
-                low++;
-            }
-            // skipping duplicates from right
-            while (low < high && nums[high] == nums[high - 1]) {
-                high--;
-            }
+        int low = 0, high = nums.length - 1;
+
+        while (low < high) {
             int mid = low + (high - low) / 2;
-            if (nums[mid] < nums[resultIdx]) {
-                resultIdx = mid;
+
+            if (nums[mid] < nums[high]) {
+                high = mid;
             } else if (nums[mid] > nums[high]) {
-                // answer lies in right portion
                 low = mid + 1;
             } else {
-                // answer lies in left portion
-                high = mid - 1;
+                high--; // handle duplicates
             }
         }
-        return nums[resultIdx];
+        return nums[low];
     }
 }
