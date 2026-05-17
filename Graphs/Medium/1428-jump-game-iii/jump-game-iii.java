0440f3322033b6ce5fc239008984b7a3d65c0edd
Surajit Rana <surajit1602@gmail.com>
2026-05-17 12:31:26 +0530

    Time: 12 ms (10.82%) | Memory: 48.3 MB (96.48%) - LeetSync

--- Graphs/Medium/1428-jump-game-iii/jump-game-iii.java
+++ Graphs/Medium/1428-jump-game-iii/jump-game-iii.java
@@ -1,12 +1,42 @@
 class Solution {
     /**
+     * Approach III : Using Graph (BFS) Approach
+     *
+     * TC : O(n)
+     * SC : O(n) + O(n)
+     * - O(n) - 'visited' and 'queue' memory
+     */
+    public boolean canReach(int[] arr, int start) {
+        int n = arr.length;
+        boolean[] visited = new boolean[n]; // SC : O(n)
+        Queue<Integer> queue = new LinkedList<>(); // SC : O(n)
+        queue.offer(start);
+        while (!queue.isEmpty()) { // TC : O(n)
+            int size = queue.size();
+            for (int i = 0; i < size; i++) {
+                Integer u = queue.poll();
+                if (u < 0 || u >= n || visited[u]) {
+                    continue;   
+                }
+                if (arr[u] == 0) {
+                    return true;
+                }
+                visited[u] = true;
+                queue.offer(u + arr[u]);
+                queue.offer(u - arr[u]);
+            }
+        }
+        return false;
+    }
+
+    /**
      * Approach II : Using Graph (DFS) Approach
      *
      * TC : O(n)
      * SC : O(n) + O(n)
-     * - O(n) - visited array and DFS recursion stack
+     * - O(n) - 'visited' array and DFS recursion stack
      */
-    public boolean canReach(int[] arr, int start) {
+    public boolean canReachDFS(int[] arr, int start) {
         int n = arr.length;
         boolean[] visited = new boolean[n]; // SC : O(n)
         return dfsGraph(start, n, arr, visited);
@@ -40,7 +70,7 @@ class Solution {
      *
      * TC : O(n)
      * SC : O(n) + O(n)
-     * - O(n) - visited array and recursion stack
+     * - O(n) - 'visited' array and recursion stack
      */
     public boolean canReachRecursiveDFS(int[] arr, int start) {
         int n = arr.length;
