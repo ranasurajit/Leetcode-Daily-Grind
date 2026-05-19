131be695bf789662e71eda0091c4e2dbc5adcbe9
Surajit Rana <surajit1602@gmail.com>
2026-05-19 09:53:43 +0530

    Time: 1 ms (98.36%) | Memory: 80.3 MB (63.48%) - LeetSync

--- TwoPointers/Easy/2634-minimum-common-value/minimum-common-value.java
+++ TwoPointers/Easy/2634-minimum-common-value/minimum-common-value.java
@@ -1,11 +1,39 @@
 class Solution {
     /**
+     * Approach II : Using Two Pointers Approach
+     *
+     * TC : O(Min(m, n))
+     * SC : O(1)
+     */
+    public int getCommon(int[] nums1, int[] nums2) {
+        int m = nums1.length;
+        int n = nums2.length;
+        int i = 0; // pointer at the start of array 'nums1'
+        int j = 0; // pointer at the start of array 'nums2'
+        /**
+         * Since both the arrays are sorted so we can
+         * use Two pointers approach to compare elements
+         */
+        while (i < m && j < n) { // TC : O(Min(m, n))
+            if (nums1[i] < nums2[j]) {
+                i++;
+            } else if (nums1[i] > nums2[j]) {
+                j++;
+            } else {
+                // nums1[i] == nums2[j], then return any
+                return nums1[i];
+            }
+        }
+        return -1;
+    }
+
+    /**
      * Approach I : Using Hashing Approach
      *
      * TC : O(n + m)
      * SC : O(n)
      */
-    public int getCommon(int[] nums1, int[] nums2) {
+    public int getCommonUsingHashing(int[] nums1, int[] nums2) {
         int m = nums1.length;
         int n = nums2.length;
         Set<Integer> set = new HashSet<>(); // SC : O(n)
