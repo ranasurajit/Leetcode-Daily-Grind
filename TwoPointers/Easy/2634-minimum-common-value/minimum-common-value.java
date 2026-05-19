b6b3f22a79ecda3fc91ada026cc0647a9c857cd0
Surajit Rana <surajit1602@gmail.com>
2026-05-19 09:46:39 +0530

    Time: 16 ms (12.85%) | Memory: 93.5 MB (6.67%) - LeetSync

---
+++ TwoPointers/Easy/2634-minimum-common-value/minimum-common-value.java
@@ -0,0 +1,30 @@
+class Solution {
+    /**
+     * Approach I : Using Hashing Approach
+     *
+     * TC : O(n + m)
+     * SC : O(n)
+     */
+    public int getCommon(int[] nums1, int[] nums2) {
+        int m = nums1.length;
+        int n = nums2.length;
+        Set<Integer> set = new HashSet<>(); // SC : O(n)
+        /**
+         * to minimize the storage, let's keep the 
+         * 'nums1' array param always of greater size
+         */
+        if (m < n) {
+            return getCommon(nums2, nums1);
+        }
+        for (int i = 0; i < n; i++) { // TC : O(n)
+            set.add(nums2[i]);
+        }
+        int minValue = Integer.MAX_VALUE;
+        for (int i = 0; i < m; i++) { // TC : O(m)
+            if (set.contains(nums1[i])) {
+                minValue = Math.min(minValue, nums1[i]);
+            }
+        }
+        return minValue == Integer.MAX_VALUE ? -1 : minValue;
+    }
+}
