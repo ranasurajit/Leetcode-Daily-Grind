9141d6f89d886e7996fd1d0d72c2c1390241cb91
Surajit Rana <surajit1602@gmail.com>
2026-05-20 09:43:45 +0530

    Time: 2 ms (70.46%) | Memory: 47.4 MB (51.46%) - LeetSync

--- Hashing/Medium/2766-find-the-prefix-common-array-of-two-arrays/find-the-prefix-common-array-of-two-arrays.java
+++ Hashing/Medium/2766-find-the-prefix-common-array-of-two-arrays/find-the-prefix-common-array-of-two-arrays.java
@@ -1,11 +1,36 @@
 class Solution {
     /**
-     * Approach : Using Hashing Approach
+     * Approach II : Using Optimal (Hashing) Approach
+     *
+     * TC : O(n)
+     * SC : O(n)
+     */
+    public int[] findThePrefixCommonArray(int[] A, int[] B) {
+        int n = A.length;
+        int[] freq = new int[n + 1];          // SC : O(n)
+        int[] result = new int[n];
+        int count = 0;
+        for (int i = 0; i < n; i++) {         // TC : O(n)
+            freq[A[i]]++;
+            if (freq[A[i]] == 2) {
+                count++;
+            }
+            freq[B[i]]++;
+            if (freq[B[i]] == 2) {
+                count++;
+            }
+            result[i] = count;
+        }
+        return result;
+    }
+
+    /**
+     * Approach I : Using Brute-Force (Hashing) Approach
      *
      * TC : O(n²)
      * SC : O(n)
      */
-    public int[] findThePrefixCommonArray(int[] A, int[] B) {
+    public int[] findThePrefixCommonArrayBruteForce(int[] A, int[] B) {
         int n = A.length;
         int[] freq = new int[n + 1];          // SC : O(n)
         int[] result = new int[n];
