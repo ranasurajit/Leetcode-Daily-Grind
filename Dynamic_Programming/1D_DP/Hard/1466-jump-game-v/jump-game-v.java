6268e7290a336b148e86455f5fa9445f82fe36c6
Surajit Rana <surajit1602@gmail.com>
2026-05-24 17:31:02 +0530

    Time: 19 ms (21.14%) | Memory: 46.6 MB (21.43%) - LeetSync

--- Dynamic_Programming/1D_DP/Hard/1466-jump-game-v/jump-game-v.java
+++ Dynamic_Programming/1D_DP/Hard/1466-jump-game-v/jump-game-v.java
@@ -1,6 +1,69 @@
 class Solution {
     /**
-     * Approach II : Using Memoization Approach
+     * Approach III : Using Tabulation (Bottom-Up) Approach
+     *
+     * TC : O(n) + O(n x log(n)) + O(d x n) + O(n) ~ O(n x (d + log(n)))
+     * SC : O(n)
+     * - O(n) - dp memory
+     *
+     * Accepted (127 / 127 testcases passed)
+     */
+    public int maxJumps(int[] arr, int d) {
+        int n = arr.length;
+        int[] dp = new int[n]; // SC : O(n)
+        /**
+         * In Bottom-Up we need to process smaller
+         * values first
+         */
+        int[][] stepArr = new int[n][2];
+        for (int i = 0; i < n; i++) { // TC : O(n)
+            stepArr[i][0] = arr[i];
+            stepArr[i][1] = i;
+        }
+        Arrays.sort(stepArr, (a, b) -> a[0] - b[0]); // TC : O(n x log(n))
+        for (int i = 0; i < n; i++) { // TC : O(n)
+            int idx = stepArr[i][1];
+            // we will try to jump from every index
+            int leftVisits = 1;
+            int rightVisits = 1;
+            // jump towards right
+            for (int step = 1; step <= d; step++) { // TC : O(d)
+                int nextIdx = idx + step;
+                if (nextIdx >= n) {
+                    // invalid index range
+                    break;
+                }
+                if (arr[idx] <= arr[nextIdx]) {
+                    // invalid index as per condition
+                    break;
+                }
+                rightVisits = Math.max(rightVisits, 1 + dp[nextIdx]);
+            }
+            // jump towards left
+            for (int step = 1; step <= d; step++) { // TC : O(d)
+                int nextIdx = idx - step;
+                if (nextIdx < 0) {
+                    // invalid index range
+                    break;
+                }
+                if (arr[idx] <= arr[nextIdx]) {
+                    // invalid index as per condition
+                    break;
+                }
+                leftVisits = Math.max(leftVisits, 1 + dp[nextIdx]);
+            }
+            int currentVisits = Math.max(leftVisits, rightVisits);
+            dp[idx] = Math.max(dp[idx], currentVisits);
+        }
+        int maxVisits = 0;
+        for (int i = 0; i < n; i++) { // TC : O(n)
+            maxVisits = Math.max(maxVisits, dp[i]);
+        }
+        return maxVisits;
+    }
+
+    /**
+     * Approach II : Using Memoization (Top-Down) Approach
      *
      * TC : O(d x n)
      * SC : O(n) + O(n)
@@ -9,7 +72,7 @@ class Solution {
      *
      * Accepted (127 / 127 testcases passed)
      */
-    public int maxJumps(int[] arr, int d) {
+    public int maxJumpsMemoization(int[] arr, int d) {
         int n = arr.length;
         int maxVisits = Integer.MIN_VALUE;
         int[] memo = new int[n]; // SC : O(n)
