8914efdae249076b66d3ecaceceac89a757799a4
Surajit Rana <surajit1602@gmail.com>
2026-05-25 12:35:15 +0530

    Time: 10 ms (64.48%) | Memory: 47.9 MB (59.33%) - LeetSync

--- Graphs/Medium/2001-jump-game-vii/jump-game-vii.java
+++ Graphs/Medium/2001-jump-game-vii/jump-game-vii.java
@@ -1,13 +1,120 @@
 class Solution {
     /**
-     * Approach : Using Graph + BFS Approach
+     * Approach IV : Using Tabulation Approach
+     *
+     * TC : O(n), where r = Max Range(maxJump - minJump)
+     * SC : O(n)
+     * - O(n) - dp memory
+     *
+     * Accepted (143 / 143 testcases passed)
+     */
+    public boolean canReach(String s, int minJump, int maxJump) {
+        int n = s.length();
+        boolean[] dp = new boolean[n]; // SC : O(n)
+        dp[0] = true; // possible to reach from index '0' to '0'
+        int reachableCount = 0;
+        for (int i = 1; i < n; i++) { // TC : O(n)
+            if (i - minJump >= 0 && dp[i - minJump]) {
+                // sliding next window
+                reachableCount++;
+            }
+            if (i - maxJump - 1 >= 0 && dp[i - maxJump - 1]) {
+                // removing last index from leaving window
+                reachableCount--;
+            }
+            dp[i] = reachableCount > 0 && s.charAt(i) == '0';
+        }
+        return dp[n - 1];
+    }
+
+    /**
+     * Approach III : Using Memoization Approach
+     *
+     * TC : O(r x n), where r = Max Range(maxJump - minJump)
+     * SC : O(n) + O(n)
+     * - O(n) - memoization memory
+     * - O(n) - recursion stack
+     *
+     * Time Limit Exceeded (117 / 143 testcases passed)
+     */
+    public boolean canReachMemoization(String s, int minJump, int maxJump) {
+        int n = s.length();
+        Boolean[] memo = new Boolean[n]; // SC : O(n)
+        return solveMemoization(0, n, s, minJump, maxJump, memo);
+    }
+
+    /**
+     * Using Recursion Approach
+     *
+     * TC : O(r x n), where r = Max Range(maxJump - minJump)
+     * SC : O(n)
+     */
+    private boolean solveMemoization(int idx, int n, String s,
+        int minJump, int maxJump, Boolean[] memo) {
+        // Base Case
+        if (idx == n - 1) {
+            return true;
+        }
+        // Memoization Check
+        if (memo[idx] != null) {
+            return memo[idx];
+        }
+        // Recursion Calls
+        int start = idx + minJump;
+        int end = Math.min(idx + maxJump, n - 1);
+        for (int i = start; i <= end; i++) { // TC : O(r)
+            if (s.charAt(i) == '0' &&
+                solveMemoization(i, n, s, minJump, maxJump, memo)) {
+                return memo[idx] = true;
+            }
+        }
+        return memo[idx] = false;
+    }
+
+    /**
+     * Approach II : Using Recursion Approach
+     *
+     * TC : O(rⁿ), where r = Max Range(maxJump - minJump)
+     * SC : O(n)
+     * - O(n) - recursion stack
+     */
+    public boolean canReachRecursion(String s, int minJump, int maxJump) {
+        int n = s.length();
+        return solveRecursion(0, n, s, minJump, maxJump);
+    }
+
+    /**
+     * Using Recursion Approach
+     *
+     * TC : O(rⁿ), where r = Max Range(maxJump - minJump)
+     * SC : O(n)
+     */
+    private boolean solveRecursion(int idx, int n, String s, int minJump,
+        int maxJump) {
+        // Base Case
+        if (idx == n - 1) {
+            return true;
+        }
+        // Recursion Calls
+        int start = idx + minJump;
+        int end = Math.min(idx + maxJump, n - 1);
+        for (int i = start; i <= end; i++) { // TC : O(r)
+            if (s.charAt(i) == '0' && solveRecursion(i, n, s, minJump, maxJump)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    /**
+     * Approach I : Using Graph + BFS Approach
      *
      * TC : O(n)
      * SC : O(n) + O(n) ~ O(n)
      *
      * Accepted (143 / 143 testcases passed)
      */
-    public boolean canReach(String s, int minJump, int maxJump) {
+    public boolean canReachBFS(String s, int minJump, int maxJump) {
         int n = s.length();
         /**
          * Here we need to return if there is a path or way
