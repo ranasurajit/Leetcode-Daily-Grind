402a540e3d2afae9c0f8da3dd1998f5e7f2ff01c
Surajit Rana <surajit1602@gmail.com>
2026-05-26 10:33:50 +0530

    Time: 1 ms (100.00%) | Memory: 43.6 MB (71.43%) - LeetSync

--- Hashing/Easy/3408-count-the-number-of-special-characters-i/count-the-number-of-special-characters-i.java
+++ Hashing/Easy/3408-count-the-number-of-special-characters-i/count-the-number-of-special-characters-i.java
@@ -1,11 +1,39 @@
 class Solution {
     /**
-     * Approach : Using Hashing Approach
+     * Approach II : Using Bitmasking Approach
      *
      * TC : O(n) + O(26) ~ O(n)
-     * SC : O(26) + O(26) ~ O(26)
+     * SC : O(1)
      */
     public int numberOfSpecialChars(String word) {
+        int n = word.length();
+        int lower = 0;
+        int upper = 0;
+        for (int i = 0; i < n; i++) {  // TC : O(n)
+            char ch = word.charAt(i);
+            if (Character.isLowerCase(ch)) {
+                lower = lower | (1 << (ch - 'a'));
+            } else {
+                upper = upper | (1 << (ch - 'A'));
+            }
+        }
+        int count = 0;
+        // count the set bits in both lower and upper
+        for (int i = 0; i < 26; i++) { // TC : O(26)
+            if (((lower >> i) & 1) > 0 && ((upper >> i) & 1) > 0){
+                count++;
+            }
+        }
+        return count;
+    }
+
+    /**
+     * Approach I : Using Hashing Approach
+     *
+     * TC : O(n) + O(26) ~ O(n)
+     * SC : O(26) + O(26) ~ O(1)
+     */
+    public int numberOfSpecialCharsHashing(String word) {
         int n = word.length();
         int[] capsFreq = new int[26];  // SC : O(26)
         int[] smallFreq = new int[26]; // SC : O(26)
