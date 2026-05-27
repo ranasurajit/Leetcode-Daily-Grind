7fef1b7491963ec5c6460c0d5f35b3156e9b8727
Surajit Rana <surajit1602@gmail.com>
2026-05-27 16:08:20 +0530

    Time: 21 ms (66.94%) | Memory: 48.4 MB (13.22%) - LeetSync

--- Hashing/Medium/3405-count-the-number-of-special-characters-ii/count-the-number-of-special-characters-ii.java
+++ Hashing/Medium/3405-count-the-number-of-special-characters-ii/count-the-number-of-special-characters-ii.java
@@ -1,11 +1,59 @@
 class Solution {
     /**
-     * Approach : Using Hashing Approach
+     * Approach II : Using State-Machine + Hashing Approach
      *
      * TC : O(n) + O(26) ~ O(n)
      * SC : O(26) ~ O(1)
      */
     public int numberOfSpecialChars(String word) {
+        int n = word.length();
+        /**
+         * we will store the below values in the state array:
+         * 0 → not seen
+         * 1 → seen lowercase
+         * 2 → seen uppercase after lowercase (valid candidate)
+         * -1 → invalid
+         */
+        int[] state = new int[26]; // SC : O(26)
+        for (int i = 0; i < n; i++) { // TC : O(n)
+            char ch = word.charAt(i);
+            int offset = -1;
+            if (Character.isLowerCase(ch)) {
+                offset = ch - 'a';
+                if (state[offset] == 0) {
+                    state[offset] = 1;
+                } else if (state[offset] == 2) {
+                    state[offset] = -1; // invalid
+                }
+            } else {
+                offset = ch - 'A';
+                if (state[offset] == 0) {
+                    state[offset] = -1; // invalid
+                } else if (state[offset] == 1) {
+                    state[offset] = 2;
+                }
+            }
+        }
+        /**
+         * now for every letters we will count the
+         * number of characters whose state[i] = 2
+         */
+        int count = 0;
+        for (int i = 0; i < 26; i++) { // TC : O(26)
+            if (state[i] == 2) {
+                count++;
+            }
+        }
+        return count;
+    }
+
+    /**
+     * Approach I : Using Hashing Approach
+     *
+     * TC : O(n) + O(26) ~ O(n)
+     * SC : O(26) + O(26) ~ O(1)
+     */
+    public int numberOfSpecialCharsHashing(String word) {
         int n = word.length();
         /**
          * we will store the last index of any character
