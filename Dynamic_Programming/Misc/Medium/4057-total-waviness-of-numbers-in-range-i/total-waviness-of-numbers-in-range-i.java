d862cde22281e1524622d03b5bd23da39ba92b68
Surajit Rana <surajit1602@gmail.com>
2026-06-04 10:00:57 +0530

    Time: 29 ms (42.01%) | Memory: 46.9 MB (8.18%) - LeetSync

---
+++ Dynamic_Programming/Misc/Medium/4057-total-waviness-of-numbers-in-range-i/total-waviness-of-numbers-in-range-i.java
@@ -0,0 +1,130 @@
+class Solution {
+    /**
+     * Approach III : Using Dynamic Programming Approach
+     *
+     * TC : O(num2) ~ O(r)
+     * SC : O(num2)
+     */
+    public int totalWaviness(int num1, int num2) {
+        int max = num2 + 1;
+        int[] waviArr = new int[max]; // SC : O(num2)
+        int[] dp = new int[max];      // SC : O(num2)
+        for (int i = 100; i < max; i++) { // TC : O(num2)
+            int d1 = i % 10; // unit
+            int d2 = (i / 10) % 10; // tens
+            int d3 = (i / 100) % 10; // hundreds
+            int waviness = 0;
+            if ((d2 > d1 && d2 > d3) || (d2 < d1 && d2 < d3)) {
+                waviness++;
+            }
+            dp[i] = dp[i / 10] + waviness;
+            waviArr[i] = waviArr[i - 1] + dp[i];
+        }
+        return waviArr[num2] - waviArr[num1 - 1];
+    }
+
+    /**
+     * Approach II : Using Math + Simulation (No Extra Space) Approach
+     *
+     * TC : O(r x d) ~ O(r)
+     * SC : O(1)
+     *
+     * where r = num2 - num1 + 1,
+     * d = Max(1 + log10(num1), 1 + log10(num2)) ~ 5
+     * as 1 <= num1 <= num2 <= 105
+     */
+    public int totalWavinessBetter(int num1, int num2) {
+        int totalWaviness = 0;
+        for (int i = num1; i <= num2; i++) { // TC : O(r)
+            totalWaviness += countWaviness(i); // TC : O(d), SC : O(1)
+        }
+        return totalWaviness;
+    }
+
+    /**
+     * Using Math Approach
+     *
+     * TC : O(d)
+     * SC : O(1)
+     */
+    private int countWaviness(int num) {
+        int digits = 0;
+        int prev = -1;
+        int current = -1;
+        int next = -1;
+        int waviness = 0;
+        while (num > 0) { // TC : O(d)
+            digits++;
+            next = num % 10;
+            num = num / 10;
+            if (digits >= 3 && 
+                ((current > prev && current > next) ||
+                (current < prev && current < next))) {
+                waviness++;
+            }
+            prev = current;
+            current = next;
+        }
+        return waviness;
+    }
+
+    /**
+     * Approach I : Using Math + Simulation (Extra Space) Approach
+     *
+     * TC : O(r x d) ~ O(r)
+     * SC : O(r x d) ~ O(r)
+     *
+     * where r = num2 - num1 + 1,
+     * d = Max(1 + log10(num1), 1 + log10(num2)) ~ 5
+     * as 1 <= num1 <= num2 <= 105
+     */
+    public int totalWavinessBruteForce(int num1, int num2) {
+        int len1 = 1 + (int) Math.log10(num1);
+        int len2 = 1 + (int) Math.log10(num2);
+        if (len1 < 3 && len2 < 3) {
+            /**
+             * range [num1, num2] have elements less than 3 digits
+             * so all of the numbers between then have waviness of 0
+             */
+            return 0;
+        }
+        int totalWaviness = 0;
+        for (int i = num1; i <= num2; i++) { // TC : O(r)
+            int len = 1 + (int) Math.log10(i);
+            if (len < 3) {
+                // elements less than 3 digits has waviness of 0
+                continue;
+            }
+            totalWaviness += getNumberWaviness(i, len); // TC : O(d), SC : O(d)
+        }
+        return totalWaviness;
+    }
+
+    /**
+     * Using Math Approach
+     *
+     * TC : O(d)
+     * SC : O(d)
+     */
+    private int getNumberWaviness(int num, int d) {
+        // here count digits of num >= 3
+        int[] digits = new int[d]; // SC : O(d)
+        int idx = d - 1;
+        while (num > 0) {
+            digits[idx] = num % 10;
+            num = num / 10;
+            idx--;
+        }
+        int peaks = 0;
+        int valleys = 0;
+        for (int i = 1; i < d - 1; i++) { // TC : O(d)
+            if (digits[i] > digits[i - 1] && digits[i] > digits[i + 1]) {
+                peaks++;
+            }
+            if (digits[i] < digits[i - 1] && digits[i] < digits[i + 1]) {
+                valleys++;
+            }
+        }
+        return peaks + valleys;
+    }
+}
