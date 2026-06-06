5bfb5b530a7971cc33361b7bc1f8313e5d506bfe
Surajit Rana <surajit1602@gmail.com>
2026-06-06 13:08:28 +0530

    Time: 2 ms (98.84%) | Memory: 46.4 MB (84.77%) - LeetSync

--- Arrays/PrefixSuffix/Easy/2714-left-and-right-sum-differences/left-and-right-sum-differences.java
+++ Arrays/PrefixSuffix/Easy/2714-left-and-right-sum-differences/left-and-right-sum-differences.java
@@ -1,11 +1,33 @@
 class Solution {
     /**
-     * Approach : Using Prefix-Suffix Array Approach
+     * Approach II : Using Running Prefix-Suffix Array Approach
+     *
+     * TC : O(n) + O(n) ~ O(n)
+     * SC : O(1)
+     */
+    public int[] leftRightDifference(int[] nums) {
+        int n = nums.length;
+        int[] diff = new int[n];
+        int leftSum = 0;
+        for (int i = 0; i < n; i++) { // TC : O(n)
+            diff[i] = leftSum;
+            leftSum += nums[i];
+        }
+        int rightSum = 0;
+        for (int i = n - 1; i >= 0; i--) { // TC : O(n)
+            diff[i] = Math.abs(diff[i] - rightSum);
+            rightSum += nums[i];
+        }
+        return diff;
+    }
+
+    /**
+     * Approach I : Using Brute-Force (Prefix-Suffix Array) Approach
      *
      * TC : O(n) + O(n) + O(n) ~ O(n)
      * SC : O(n) + O(n) ~ O(n)
      */
-    public int[] leftRightDifference(int[] nums) {
+    public int[] leftRightDifferenceBruteForce(int[] nums) {
         int n = nums.length;
         int[] leftSum = new int[n];   // SC : O(n)
         leftSum[0] = nums[0];
