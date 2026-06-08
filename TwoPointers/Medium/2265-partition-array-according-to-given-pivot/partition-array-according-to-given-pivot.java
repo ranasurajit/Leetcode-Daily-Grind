78f582bc0d04d7b46c305707b7b2c0594c1d2683
Surajit Rana <surajit1602@gmail.com>
2026-06-08 11:22:51 +0530

    Time: 5 ms (73.56%) | Memory: 159.7 MB (73.16%) - LeetSync

---
+++ TwoPointers/Medium/2265-partition-array-according-to-given-pivot/partition-array-according-to-given-pivot.java
@@ -0,0 +1,78 @@
+class Solution {
+    /**
+     * Approach II : Using Two Pointers Approach
+     *
+     * TC : O(n) + O(n) ~ O(n)
+     * SC : O(1)
+     */
+    public int[] pivotArray(int[] nums, int pivot) {
+        int n = nums.length;
+        int smallCount = 0;
+        int pivotCount = 0;
+        int largeCount = 0;
+        for (int num : nums) { // TC : O(n)
+            if (num < pivot) {
+                smallCount++;
+            } else if (num == pivot) {
+                pivotCount++;
+            } else {
+                largeCount++;
+            }
+        }
+        int smallStart = 0;
+        int pivotStart = smallStart + smallCount;
+        int largeStart = pivotStart + pivotCount;
+        int[] result = new int[n];
+        for (int i = 0; i < n; i++) { // TC : O(n)
+            if (nums[i] < pivot) {
+                result[smallStart++] = nums[i];
+            } else if (nums[i] == pivot) {
+                result[pivotStart++] = nums[i];
+            } else {
+                result[largeStart++] = nums[i];
+            }
+        }
+        return result;
+    }
+
+    /**
+     * Approach I : Using Brute-Force (Array Simulation) Approach
+     *
+     * TC : O(n) + O(n) ~ O(n)
+     * SC : O(n)
+     * as n1 + n2 + n3 = n
+     */
+    public int[] pivotArrayBruteForce(int[] nums, int pivot) {
+        int n = nums.length;
+        ArrayList<Integer> smaller = new ArrayList<>(); // SC : O(n1)
+        ArrayList<Integer> mid = new ArrayList<>();     // SC : O(n2)
+        ArrayList<Integer> larger = new ArrayList<>();  // SC : O(n3)
+        for (int num : nums) { // TC : O(n)
+            if (num < pivot) {
+                smaller.add(num);
+            } else if (num == pivot) {
+                mid.add(num);
+            } else {
+                larger.add(num);
+            }
+        }
+        int[] result = new int[n];
+        int index = 0;
+        int i = 0;
+        int size = smaller.size();
+        while (index < size) { // TC : O(n1)
+            result[index++] = smaller.get(i++);
+        }
+        i = 0;
+        size += mid.size();
+        while (index < size) { // TC : O(n2)
+            result[index++] = mid.get(i++);
+        }
+        i = 0;
+        size += larger.size();
+        while (index < size) { // TC : O(n3)
+            result[index++] = larger.get(i++);
+        }
+        return result;
+    }
+}
