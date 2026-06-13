a083a9f386e541dbc1fea7e55deedb2e84db7048
Surajit Rana <surajit1602@gmail.com>
2026-06-13 12:59:15 +0530

    Time: 2 ms (96.85%) | Memory: 46.5 MB (49.79%) - LeetSync

--- Strings/Simulation/Easy/4216-weighted-word-mapping/weighted-word-mapping.java
+++ Strings/Simulation/Easy/4216-weighted-word-mapping/weighted-word-mapping.java
@@ -10,25 +10,14 @@ class Solution {
     public String mapWordWeights(String[] words, int[] weights) {
         StringBuilder sb = new StringBuilder(); // SC : O(n)
         for (String word : words) { // TC : O(n)
-            int charIndex =
-                getCharacterOffsetIndexInWord(word, weights); // TC : O(l)
+            int len = word.length();
+            int sum = 0;
+            for (int i = 0; i < len; i++) { // TC : O(l)
+                sum += weights[word.charAt(i) - 'a'];
+            }
+            int charIndex = 25 - (sum % 26);
             sb.append((char) (charIndex + 'a'));
         }
         return sb.toString();
-    }
-
-    /**
-     * Using String Simulation Approach
-     *
-     * TC : O(l)
-     * SC : O(1)
-     */
-    private int getCharacterOffsetIndexInWord(String word, int[] weights) {
-        int len = word.length();
-        int sum = 0;
-        for (int i = 0; i < len; i++) { // TC : O(l)
-            sum += weights[word.charAt(i) - 'a'];
-        }
-        return 25 - (sum % 26);
     }
 }
