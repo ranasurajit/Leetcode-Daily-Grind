ac050304dab791c3d1660d778ee1a04811e0d4e2
Surajit Rana <surajit1602@gmail.com>
2026-06-16 10:01:53 +0530

    Time: 163 ms (6.62%) | Memory: 89.5 MB (5.88%) - LeetSync

--- Strings/Simulation/Medium/3931-process-string-with-special-operations-i/process-string-with-special-operations-i.java
+++ Strings/Simulation/Medium/3931-process-string-with-special-operations-i/process-string-with-special-operations-i.java
@@ -1,11 +1,67 @@
 class Solution {
+    private static final int LIMIT = (int) 1e8;
     /**
-     * Approach : Using String Simulation Approach
+     * Approach II : Using Optimal (Lazy Deque and Reverse Flag) Approach
+     *
+     * TC : O(n + LIMIT)
+     * SC : O(LIMIT)
+     */
+    public String processStr(String s) {
+        int n = s.length();
+        Deque<Character> deque = new ArrayDeque<>();
+        boolean isReversed = false;
+        for (char ch : s.toCharArray()) { // TC : O(n)
+            if (ch == '*') {
+                if (!deque.isEmpty()) {
+                    if (isReversed) {
+                        deque.pollFirst(); // TC : O(1)
+                    } else {
+                        deque.pollLast(); // TC : O(1)
+                    }
+                }
+            } else if (ch == '#') {
+                Deque<Character> copy = new ArrayDeque<>(deque);
+                if (isReversed) {
+                    while (!copy.isEmpty()) {
+                        deque.offerFirst(copy.pollLast());
+                    }
+                } else {
+                    deque.addAll(copy);
+                }
+            } else if (ch == '%') {
+                isReversed = !isReversed; // TC : O(1)
+            } else {
+                // ch is lowercase letter 
+                if (isReversed) {
+                    deque.offerFirst(ch); // TC : O(1)
+                } else {
+                    deque.offerLast(ch); // TC : O(1)
+                }
+            }
+            // if (deque.size() > LIMIT) {
+            //     break;
+            // }
+        }
+        StringBuilder sb = new StringBuilder();
+        if (isReversed) {
+            while (!deque.isEmpty()) { // TC : O(m)
+                sb.append(deque.pollLast());
+            }
+        } else {
+            while (!deque.isEmpty()) { // TC : O(m)
+                sb.append(deque.pollFirst());
+            }
+        }
+        return sb.toString();
+    }
+
+    /**
+     * Approach I : Using Brute-Force (String Simulation) Approach
      *
      * TC : O(2ⁿ)
      * SC : O(2ⁿ)
      */
-    public String processStr(String s) {
+    public String processStrBruteForce(String s) {
         int n = s.length();
         StringBuilder sb = new StringBuilder();
         for (int i = 0; i < n; i++) { // TC : O(n)
