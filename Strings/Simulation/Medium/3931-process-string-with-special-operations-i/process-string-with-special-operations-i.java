6c20103fe410e852a2fea15bd46f18bc7547220b
Surajit Rana <surajit1602@gmail.com>
2026-06-16 10:03:11 +0530

    Time: 3 ms (100.00%) | Memory: 55.6 MB (28.68%) - LeetSync

--- Strings/Simulation/Medium/3931-process-string-with-special-operations-i/process-string-with-special-operations-i.java
+++ Strings/Simulation/Medium/3931-process-string-with-special-operations-i/process-string-with-special-operations-i.java
@@ -1,11 +1,38 @@
 class Solution {
     /**
+     * Approach I : Using Brute-Force (String Simulation) Approach
+     *
+     * TC : O(2ⁿ)
+     * SC : O(2ⁿ)
+     */
+    public String processStr(String s) {
+        int n = s.length();
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < n; i++) { // TC : O(n)
+            char ch = s.charAt(i);
+            if (ch == '*') {
+                if (sb.length() > 0) {
+                    sb.setLength(sb.length() - 1); // TC : O(1)
+                }
+            } else if (ch == '#') {
+                sb.append(sb.toString()); // TC : O(k)
+            } else if (ch == '%') {
+                sb.reverse(); // TC : O(k)
+            } else {
+                // ch is lowercase letter 
+                sb.append(ch); // TC : O(1)
+            }
+        }
+        return sb.toString();
+    }
+
+    /**
      * Approach II : Using Optimal (Lazy Deque and Reverse Flag) Approach
      *
      * TC : O(n + LIMIT)
      * SC : O(LIMIT)
      */
-    public String processStr(String s) {
+    public String processStrOptimal(String s) {
         int n = s.length();
         int LIMIT = (int) 1e8;
         Deque<Character> deque = new ArrayDeque<>();
@@ -50,33 +77,6 @@ class Solution {
         } else {
             while (!deque.isEmpty()) { // TC : O(m)
                 sb.append(deque.pollFirst());
-            }
-        }
-        return sb.toString();
-    }
-
-    /**
-     * Approach I : Using Brute-Force (String Simulation) Approach
-     *
-     * TC : O(2ⁿ)
-     * SC : O(2ⁿ)
-     */
-    public String processStrBruteForce(String s) {
-        int n = s.length();
-        StringBuilder sb = new StringBuilder();
-        for (int i = 0; i < n; i++) { // TC : O(n)
-            char ch = s.charAt(i);
-            if (ch == '*') {
-                if (sb.length() > 0) {
-                    sb.setLength(sb.length() - 1); // TC : O(1)
-                }
-            } else if (ch == '#') {
-                sb.append(sb.toString()); // TC : O(k)
-            } else if (ch == '%') {
-                sb.reverse(); // TC : O(k)
-            } else {
-                // ch is lowercase letter 
-                sb.append(ch); // TC : O(1)
             }
         }
         return sb.toString();
