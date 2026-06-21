2d900a0bb30c65d0c40a15e7f583b667b1f4ba38
Surajit Rana <surajit1602@gmail.com>
2026-06-21 12:06:08 +0530

    Time: 5 ms (91.89%) | Memory: 82.9 MB (86.97%) - LeetSync

--- GreedyAlgorithm/Medium/1961-maximum-ice-cream-bars/maximum-ice-cream-bars.java
+++ GreedyAlgorithm/Medium/1961-maximum-ice-cream-bars/maximum-ice-cream-bars.java
@@ -1,11 +1,47 @@
 class Solution {
     /**
-     * Approach : Using Greedy + Sorting Approach
+     * Approach II : Using Greedy + Sorting (Count Sort) Approach
+     *
+     * TC : O(n) + O(n) + O(Max(costs)) ~ O(n + Max(costs))
+     * SC : O(Max(costs))
+     */
+    public int maxIceCream(int[] costs, int coins) {
+        int n = costs.length;
+        int max = 0;
+        for (int i = 0; i < n; i++) { // TC : O(n)
+            max = Math.max(max, costs[i]);
+        }
+        /**
+         * ice cream bars can be purchased in any order
+         * so since we need to maximize ice cream bars
+         * we can sort the 'costs' array and greedily 
+         * try to buy as many ice cream bars as possible
+         * since here we know the range to optimize
+         * sorting, we can go with Count Sort Algorithm
+         */
+        int[] freq = new int[max + 1]; // TC : O(Max(costs))
+        for (int i = 0; i < n; i++) {  // TC : O(n)
+            freq[costs[i]]++;
+        }
+        int count = 0;
+        for (int cost = 0; cost <= max; cost++) { // TC : O(Max(costs))
+            if (freq[cost] == 0) {
+                continue;
+            }
+            int countBuyAtCost = Math.min(freq[cost], coins / cost);
+            count += countBuyAtCost;
+            coins -= countBuyAtCost * cost;
+        }
+        return count;
+    }
+
+    /**
+     * Approach I : Using Greedy + Sorting (In-Built Sort) Approach
      *
      * TC : O(n x log(n)) + O(n) ~ O(n x log(n))
      * SC : O(1)
      */
-    public int maxIceCream(int[] costs, int coins) {
+    public int maxIceCreamBruteForce(int[] costs, int coins) {
         int n = costs.length;
         /**
          * ice cream bars can be purchased in any order
