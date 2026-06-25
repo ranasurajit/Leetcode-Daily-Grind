1c81fbbbc7cf8c7af9a14f91c82e4e6b7770110e
Surajit Rana <surajit1602@gmail.com>
2026-06-25 13:05:43 +0530

    Time: 25 ms (82.15%) | Memory: 46.9 MB (80.92%) - LeetSync

---
+++ Arrays/PrefixSuffix/Medium/4074-count-subarrays-with-majority-element-i/count-subarrays-with-majority-element-i.java
@@ -0,0 +1,44 @@
+class Solution {
+    
+    public int countMajoritySubarrays(int[] nums, int target) {
+        int n = nums.length;
+        /**
+         * we will pre-compute the prefix count of target
+         * for every index and so we create a 'prefix' array
+         * prefix[i] = number of target occurrences in nums[0..i-1]
+         */
+        int[] prefix = new int[n + 1];    // SC : O(n)
+        for (int i = 0; i < n; i++) { // TC : O(n)
+            prefix[i + 1] = prefix[i] + (nums[i] == target ? 1 : 0);
+        }
+        /**
+         * for a number target to be majority elements so
+         * count of that number in a sub-array of size 'len'
+         * should be such that count > len / 2
+         * i.e. freq > (r - l + 1) / 2
+         * i.e. prefix[r] - prefix[l - 1] > (r - l + 1) / 2
+         * i.e. 2 * prefix[r] - 2 x prefix[l - 1] > r - l + 1
+         * i.e. (2 * prefix[r] - r) > (2 x prefix[l - 1] - (l - 1))
+         * i.e. score[r] > score[l - 1]
+         * where score[i] = (2 * prefix[i] - i)
+         * so now, let's pre-compute score[i] for each index 'i'
+         */
+        int[] score = new int[n + 1];     // SC : O(n)
+        for (int i = 0; i <= n; i++) { // TC : O(n)
+            score[i] = (2 * prefix[i]) - i;
+        }
+        /**
+         * now we will count the valid sub-arrays with 
+         * score[r] > score[l - 1]
+         */
+        int count = 0;
+        for (int l = 0; l <= n; l++) {         // TC : O(n)
+            for (int r = l + 1; r <= n; r++) { // TC : O(n)
+                if (score[r] > score[l]) {
+                    count++;
+                }
+            }
+        }
+        return count;
+    }
+}
