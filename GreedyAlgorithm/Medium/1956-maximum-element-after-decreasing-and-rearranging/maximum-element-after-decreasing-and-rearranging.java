43dc688deec7ecccc2a6eb25ab320ab68542503b
Surajit Rana <surajit1602@gmail.com>
2026-06-28 12:57:30 +0530

    Time: 3 ms (99.58%) | Memory: 79.3 MB (5.00%) - LeetSync

--- GreedyAlgorithm/Medium/1956-maximum-element-after-decreasing-and-rearranging/maximum-element-after-decreasing-and-rearranging.java
+++ GreedyAlgorithm/Medium/1956-maximum-element-after-decreasing-and-rearranging/maximum-element-after-decreasing-and-rearranging.java
@@ -1,11 +1,31 @@
 class Solution {
     /**
-     * Approach : Using Greedy + Sorting Approach
+     * Approach II : Using Greedy + Hashing Approach
+     *
+     * TC : O(n) + O(n) ~ O(n)
+     * SC : O(n)
+     */
+    public int maximumElementAfterDecrementingAndRearranging(int[] arr) {
+        int n = arr.length;
+        int[] freq = new int[n + 1]; // SC : O(n)
+        // array 'arr' after operations cannot have a value more than n
+        for (int x : arr) { // TC : O(n)
+            freq[Math.min(x, n)]++;
+        }
+        int value = 0;
+        for (int i = 1; i <= n; i++) { // TC : O(n)
+            value = Math.min(i, value + freq[i]);
+        }
+        return value; // this will have the maximum value
+    }
+
+    /**
+     * Approach I : Using Greedy + Sorting Approach
      *
      * TC : O(n x log(n)) + O(n) ~ O(n x log(n))
      * SC : O(1)
      */
-    public int maximumElementAfterDecrementingAndRearranging(int[] arr) {
+    public int maximumElementAfterDecrementingAndRearrangingSorting(int[] arr) {
         int n = arr.length;
         /**
          * we are free to re-arrange the elements during operations
