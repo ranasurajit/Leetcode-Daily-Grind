e441024910c2b16e3384bae2235f17ed80265da6
Surajit Rana <surajit1602@gmail.com>
2026-06-29 11:34:35 +0530

    Time: 2 ms (7.11%) | Memory: 43.6 MB (8.19%) - LeetSync

---
+++ Strings/StringMatching/Easy/2099-number-of-strings-that-appear-as-substrings-in-word/number-of-strings-that-appear-as-substrings-in-word.java
@@ -0,0 +1,46 @@
+class Solution {
+    /**
+     * Approach I : Using Two Pointers Approach
+     *
+     * TC : O(m x n x l)
+     * SC : O(1)
+     * where m = length(word), l = max(length(patterns)), n = length(patterns)
+     */
+    public int numOfStrings(String[] patterns, String word) {
+        int count = 0;
+        for (String s : patterns) { // TC : O(n)
+            if (isSubstring(word, s)) { // TC : O(l x m)
+                count++;
+            }
+        }
+        return count;
+    }
+
+    /**
+     * Using Two Pointers Approach
+     *
+     * TC : O(m x l)
+     * SC : O(1)
+     */
+    private boolean isSubstring(String word, String s) {
+        int m = word.length();
+        int n = s.length();
+        int p = 0; // pointer at the start of String 'word'
+        int q = 0; // pointer at the start of String 's'
+        int start = 0;
+        while (p < m && q < n) { // TC : O(m x l)
+            if (word.charAt(p) == s.charAt(q)) {
+                p++;
+                q++;
+                if (q == n) {
+                    return true;
+                }
+            } else {
+                start++;
+                p = start;
+                q = 0;
+            }
+        }
+        return false;
+    }
+}
