380e0d627055ae339ecc585e7a425b1d3a6d7137
Surajit Rana <surajit1602@gmail.com>
2026-06-29 11:42:10 +0530

    Time: 1 ms (75.75%) | Memory: 43.6 MB (19.94%) - LeetSync

--- Strings/StringMatching/Easy/2099-number-of-strings-that-appear-as-substrings-in-word/number-of-strings-that-appear-as-substrings-in-word.java
+++ Strings/StringMatching/Easy/2099-number-of-strings-that-appear-as-substrings-in-word/number-of-strings-that-appear-as-substrings-in-word.java
@@ -1,29 +1,12 @@
 class Solution {
     /**
-     * Approach II : Using String InBuilt Methods Approach
-     *
-     * TC : O(n x (m + l))
-     * SC : O(1)
-     * where m = length(word), l = max(length(patterns)), n = length(patterns)
-     */
-    public int numOfStrings(String[] patterns, String word) {
-        int count = 0;
-        for (String s : patterns) { // TC : O(n)
-            if (word.indexOf(s) > -1) { // TC : O(l + m)
-                count++;
-            }
-        }
-        return count;
-    }
-
-    /**
      * Approach I : Using Brute-Force (Two Pointers) Approach
      *
      * TC : O(m x n x l)
      * SC : O(1)
      * where m = length(word), l = max(length(patterns)), n = length(patterns)
      */
-    public int numOfStringsBruteForce(String[] patterns, String word) {
+    public int numOfStrings(String[] patterns, String word) {
         int count = 0;
         for (String s : patterns) { // TC : O(n)
             if (isSubstring(word, s)) { // TC : O(l x m)
@@ -42,21 +25,19 @@ class Solution {
     private boolean isSubstring(String word, String s) {
         int m = word.length();
         int n = s.length();
-        int p = 0; // pointer at the start of String 'word'
-        int q = 0; // pointer at the start of String 's'
         int start = 0;
         while (start <= m - n) { // TC : O(m x l)
-            if (word.charAt(p) == s.charAt(q)) {
+            int p = start; // pointer at the start of String 'word'
+            int q = 0; // pointer at the start of String 's'
+            while (q < n && word.charAt(p) == s.charAt(q)) {
                 p++;
                 q++;
-                if (q == n) {
-                    return true;
-                }
-            } else {
-                start++;
-                p = start;
-                q = 0;
+                
             }
+            if (q == n) {
+                return true;
+            }
+            start++;
         }
         return false;
     }
