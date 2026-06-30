84e2e832cc13cb2163ed8e3b665027fdd639d19a
Surajit Rana <surajit1602@gmail.com>
2026-06-30 11:14:10 +0530

    Time: 13 ms (72.65%) | Memory: 46.6 MB (17.76%) - LeetSync

--- SlidingWindow/VariableSize/Medium/1460-number-of-substrings-containing-all-three-characters/number-of-substrings-containing-all-three-characters.java
+++ SlidingWindow/VariableSize/Medium/1460-number-of-substrings-containing-all-three-characters/number-of-substrings-containing-all-three-characters.java
@@ -1,5 +1,52 @@
 class Solution {
+    /**
+     * Approach II : Using Sliding Window + Hashing (No Extra Space) Approach
+     *
+     * TC: O(n)
+     * SC: O(3) ~ O(1)
+     */
     public int numberOfSubstrings(String s) {
+        int n = s.length();
+        int i = 0;
+        int j = 0;
+        /**
+         * we will store frequencies of characters 'a', 'b', 'c'
+         * in a map of  size 3 and we can proceed using
+         * sliding window approach to get all [start, end] indices
+         * which has a minimum of 1 occcurences of characters 
+         * 'a', 'b', 'c'
+         */
+        int[] map = new int[3]; // SC : O(3)
+        /**
+         * now we can compute sub-arrays as for example
+         * s = "aaaaabcabc", so for start and end as
+         * [4, 6], we can have sub-arrays as below: 
+         * [abc], [abca], [abcab], [abcabc] = (n - end)
+         */
+        int count = 0;
+        while (j < n) { // TC : O(n)
+            int charIdx = s.charAt(j) - 'a';
+            map[charIdx]++;
+            while (map[0] > 0 && map[1] > 0 && map[2] > 0) {
+                // all characters 'a', 'b' and 'c' are present in sub-array [i...j]
+                count += (n - j);
+                // now we need to shrink 'i'
+                int charI = s.charAt(i) - 'a';
+                map[charI]--;
+                i++;
+            }
+            j++;
+        }
+        return count;
+    }
+
+    /**
+     * Approach I : Using Sliding Window + Hashing (Extra Space) Approach
+     *
+     * TC: O(n) + O(n) ~ O(n)
+     * SC: O(n) + O(3) ~ O(n)
+     */
+    public int numberOfSubstringsWithExtraSpace(String s) {
         int n = s.length();
         int i = 0;
         int j = 0;
