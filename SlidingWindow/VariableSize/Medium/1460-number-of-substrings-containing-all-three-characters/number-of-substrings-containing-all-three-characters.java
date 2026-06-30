139335504d4ce99968038701143cc2bef346c201
Surajit Rana <surajit1602@gmail.com>
2026-06-30 11:06:26 +0530

    Time: 16 ms (40.27%) | Memory: 48.7 MB (5.43%) - LeetSync

---
+++ SlidingWindow/VariableSize/Medium/1460-number-of-substrings-containing-all-three-characters/number-of-substrings-containing-all-three-characters.java
@@ -0,0 +1,43 @@
+class Solution {
+    public int numberOfSubstrings(String s) {
+        int n = s.length();
+        int i = 0;
+        int j = 0;
+        /**
+         * we will store frequencies of characters 'a', 'b', 'c'
+         * in a map of  size 3 and we can proceed using
+         * sliding window approach to get all [start, end] indices
+         * which has a minimum of 1 occcurences of characters 
+         * 'a', 'b', 'c'
+         */
+        int[] map = new int[3]; // SC : O(3)
+        List<int[]> indices = new ArrayList<>(); // SC : O(n)
+        while (j < n) { // TC : O(n)
+            int charIdx = s.charAt(j) - 'a';
+            map[charIdx]++;
+            while (map[0] > 0 && map[1] > 0 && map[2] > 0) {
+                // all characters 'a', 'b' and 'c' are present in sub-array [i...j]
+                indices.add(new int[] { i, j });
+                // now we need to shrink 'i'
+                int charI = s.charAt(i) - 'a';
+                map[charI]--;
+                i++;
+            }
+            j++;
+        }
+        /**
+         * now we can compute sub-arrays as for example
+         * s = "aaaaabcabc", so for start and end as
+         * [4, 6], we can have sub-arrays as below: 
+         * [abc], [abca], [abcab], [abcabc] = (n - end)
+         */
+        int count = 0;
+        for (int[] ele : indices) { // TC : O(n)
+            int start = ele[0];
+            int end = ele[1];
+            // number of sub-arrays
+            count += (n - end);
+        }
+        return count;
+    }
+}
