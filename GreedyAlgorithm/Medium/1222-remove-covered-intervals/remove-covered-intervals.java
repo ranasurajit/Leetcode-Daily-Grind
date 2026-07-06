2bfbc05fead43714d6a6e47869787e6fd6ca1d4f
Surajit Rana <surajit1602@gmail.com>
2026-07-06 11:07:42 +0530

    Time: 8 ms (14.35%) | Memory: 46.5 MB (62.65%) - LeetSync

--- GreedyAlgorithm/Medium/1222-remove-covered-intervals/remove-covered-intervals.java
+++ GreedyAlgorithm/Medium/1222-remove-covered-intervals/remove-covered-intervals.java
@@ -1,11 +1,44 @@
 class Solution {
     /**
-     * Approach : Using Greedy + Sorting + Stack Approach
+     * Approach II : Using Greedy + Sorting Approach
      *
      * TC : O(n x log(n)) + O(n) ~ O(n x log(n))
      * SC : O(n)
      */
     public int removeCoveredIntervals(int[][] intervals) {
+        int n = intervals.length;
+        Arrays.sort(intervals, (a, b) -> {
+            if (a[0] != b[0]) {
+                return a[0] - b[0];
+            }
+            return b[1] - a[1];
+        }); // TC : O(n x log(n))
+        Deque<int[]> deque = new ArrayDeque<>(); // SC : O(n)
+        for (int i = 0; i < n; i++) { // TC : O(n)
+            int start = intervals[i][0];
+            int end = intervals[i][1];
+            if (!deque.isEmpty()) {
+                int[] prev = deque.peek();
+                if (start >= prev[0] && end <= prev[1]) {
+                    // current interval is covered
+                    continue;
+                } else {
+                    deque.push(intervals[i]);
+                }
+            } else {
+                deque.push(intervals[i]);
+            }
+        }
+        return deque.size();
+    }
+
+    /**
+     * Approach I : Using Greedy + Sorting + Stack Approach
+     *
+     * TC : O(n x log(n)) + O(n) ~ O(n x log(n))
+     * SC : O(n)
+     */
+    public int removeCoveredIntervalsUsingStack(int[][] intervals) {
         int n = intervals.length;
         Arrays.sort(intervals, (a, b) -> {
             if (a[0] != b[0]) {
