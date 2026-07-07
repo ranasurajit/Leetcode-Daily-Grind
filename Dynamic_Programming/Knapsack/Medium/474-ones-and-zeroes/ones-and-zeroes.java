6453a15d088490d83285666dfb1e263abab5fa69
Surajit Rana <surajit1602@gmail.com>
2026-07-07 09:06:36 +0530

    Time: 49 ms (36.72%) | Memory: 86 MB (28.80%) - LeetSync

--- Dynamic_Programming/Knapsack/Medium/474-ones-and-zeroes/ones-and-zeroes.java
+++ Dynamic_Programming/Knapsack/Medium/474-ones-and-zeroes/ones-and-zeroes.java
@@ -1,169 +1,53 @@
 class Solution {
     /**
-     * Approach IV : Using Space Optimization Approach
+     * Approach : Using Multi-Knapsack (Bottom Up DP) Approach
      *
-     * TC: O(K x P x M x N)
-     * SC: O(2 x M x N) ~ O(M x N)
-     *
-     * - O(M x N) - prev and current memory
-     *
-     * Accepted (77 / 77 testcases passed)
+     * TC : O(p x m x n)
+     * SC : O(p x m x n)
      */
     public int findMaxForm(String[] strs, int m, int n) {
-        int P = strs.length;
-        int[][] prev = new int[m + 1][n + 1]; // SC: O(M x N)
-        for (int i = 0; i < P; i++) { // TC: O(P)
-            int[] zerosOnes = getZerosAndOnes(strs[i]); // TC: O(K)
-            int[][] current = new int[m + 1][n + 1]; // SC: O(M x N)
-            for (int j = 0; j <= m; j++) { // TC: O(M)
-                for (int k = 0; k <= n; k++) { // TC: O(N)
+        int p = strs.length;
+        int[][][] dp = new int[p + 1][m + 1][n + 1]; // SC : O(p x m x n)
+        /**
+         * Here dp[i][j][k] represents the maximum size
+         * that is computed if we consider first (i) Strings
+         * with maximum j 0's and k 1's
+         */
+        for (int i = 1; i <= p; i++) {         // TC : O(p)
+            int[] countZeroOnes = getCountZeroOnes(strs[i - 1]);
+            int zeroes = countZeroOnes[0];
+            int ones = countZeroOnes[1];
+            for (int j = 0; j <= m; j++) {     // TC : O(m)
+                for (int k = 0; k <= n; k++) { // TC : O(n)
+                    // skip
+                    int skip = dp[i - 1][j][k];
+                    // pick
                     int pick = 0;
-                    if (j >= zerosOnes[0] && k >= zerosOnes[1]) {
-                        pick = 1 + (i > 0 ? prev[j - zerosOnes[0]][k - zerosOnes[1]] : 0);
+                    if (zeroes <= j && ones <= k) {
+                        pick = 1 + dp[i - 1][j - zeroes][k - ones];
                     }
-                    int skip = i > 0 ? prev[j][k] : 0;
-                    current[j][k] = Math.max(pick, skip);
-                }
-            }
-            prev = current.clone();
-        }
-        return prev[m][n];
-    }
-
-    /**
-     * Approach III : Using Tabulation Approach
-     *
-     * TC: O(K x P x M x N)
-     * SC: O(P x M x N)
-     *
-     * - O(P x M x N) - dp memory
-     *
-     * Accepted (77 / 77 testcases passed)
-     */
-    public int findMaxFormTabulation(String[] strs, int m, int n) {
-        int P = strs.length;
-        int[][][] dp = new int[P][m + 1][n + 1]; // SC: O(P x M x N)
-        for (int i = 0; i < P; i++) { // TC: O(P)
-            int[] zerosOnes = getZerosAndOnes(strs[i]); // TC: O(K)
-            for (int j = 0; j <= m; j++) { // TC: O(M)
-                for (int k = 0; k <= n; k++) { // TC: O(N)
-                    int pick = 0;
-                    if (j >= zerosOnes[0] && k >= zerosOnes[1]) {
-                        pick = 1 + (i > 0 ? dp[i - 1][j - zerosOnes[0]][k - zerosOnes[1]] : 0);
-                    }
-                    int skip = i > 0 ? dp[i - 1][j][k] : 0;
-                    dp[i][j][k] = Math.max(pick, skip);
+                    dp[i][j][k] = Math.max(skip, pick);
                 }
             }
         }
-        return dp[P - 1][m][n];
-    }
-
-    /**
-     * Approach II : Using Memoization Approach
-     *
-     * TC: O(K x P x M x N)
-     * SC: O(P x M x N) + O(P x M x N)
-     *
-     * - O(P x M x N) - memoization memory
-     * - O(P x M x N) - recursion stack
-     *
-     * Accepted (77 / 77 testcases passed)
-     */
-    public int findMaxFormMemoization(String[] strs, int m, int n) {
-        int P = strs.length;
-        int[][][] memo = new int[P][m + 1][n + 1]; // SC: O(P x M x N)
-        for (int[][] mem : memo) {
-            for (int[] me : mem) {
-                Arrays.fill(me, -1);
-            }
-        }
-        return solveMemoization(P - 1, strs, m, n, memo); // TC: O(K x P x M x N), SC: O(P x M x N)
-    }
-
-    /**
-     * Using Memoization Approach
-     *
-     * TC: O(K x P x M x N)
-     * SC: O(P x M x N)
-     */
-    private int solveMemoization(int idx, String[] strs, int m, int n, int[][][] memo) {
-        // Base Case
-        if (idx < 0) {
-            return 0;
-        }
-        if (m == 0 && n == 0) {
-            return 0;
-        }
-        // Memoization Check
-        if (memo[idx][m][n] != -1) {
-            return memo[idx][m][n];
-        }
-        // Recursion Calls
-        // pick or skip - returns the count of Strings added
-        int[] zerosOnes = getZerosAndOnes(strs[idx]); // TC: O(K)
-        int pick = 0;
-        if (zerosOnes[0] <= m && zerosOnes[1] <= n) {
-            // pick
-            pick = 1 + solveMemoization(idx - 1, strs, m - zerosOnes[0], n - zerosOnes[1], memo);
-        }
-        int skip = solveMemoization(idx - 1, strs, m, n, memo);
-        return memo[idx][m][n] = Math.max(pick, skip);
-    }
-
-    /**
-     * Approach I : Using Recursion Approach
-     *
-     * TC: O(K x 2 ^ N)
-     * SC: O(N)
-     *
-     * Time Limit Exceeded (27 / 77 testcases passed)
-     */
-    public int findMaxFormRecursion(String[] strs, int m, int n) {
-        return solveRecursion(strs.length - 1, strs, m, n); // TC: O(K x 2 ^ N), SC: O(N)
-    }
-
-    /**
-     * Using Recursion Approach
-     *
-     * TC: O(K x 2 ^ N)
-     * SC: O(N)
-     */
-    private int solveRecursion(int idx, String[] strs, int m, int n) {
-        // Base Case
-        if (idx < 0) {
-            return 0;
-        }
-        if (m == 0 && n == 0) {
-            return 0;
-        }
-        // Recursion Calls
-        // pick or skip - returns the count of Strings added
-        int[] zerosOnes = getZerosAndOnes(strs[idx]); // TC: O(K)
-        int pick = 0;
-        if (zerosOnes[0] <= m && zerosOnes[1] <= n) {
-            // pick
-            pick = 1 + solveRecursion(idx - 1, strs, m - zerosOnes[0], n - zerosOnes[1]);
-        }
-        int skip = solveRecursion(idx - 1, strs, m, n);
-        return Math.max(pick, skip);
+        return dp[p][m][n];
     }
 
     /**
      * Using String Simulation Approach
+     * returns int[] as [ zeroes, ones ]
      *
-     * TC: O(K)
-     * SC: O(1)
+     * TC : O(n)
+     * SC : O(1)
      */
-    private int[] getZerosAndOnes(String s) {
-        int len = s.length();
-        int zeros = 0;
-        int ones = 0;
-        for (int i = 0; i < len; i++) { // TC: O(K)
-            if (s.charAt(i) == '1') {
-                ones++;
+    private int[] getCountZeroOnes(String s) {
+        int n = s.length();
+        int zeroes = 0;
+        for (int i = 0; i < n; i++) { // TC : O(n)
+            if (s.charAt(i) == '0') {
+                zeroes++;
             }
         }
-        return new int[] { len - ones , ones };
+        return new int[] { zeroes, n - zeroes };
     }
 }
