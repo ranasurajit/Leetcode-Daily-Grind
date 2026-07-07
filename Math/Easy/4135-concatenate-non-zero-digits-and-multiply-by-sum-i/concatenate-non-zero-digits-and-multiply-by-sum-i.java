7235fdb563d32a0b56dfd23550acf991b4bdf239
Surajit Rana <surajit1602@gmail.com>
2026-07-07 09:55:48 +0530

    Time: 1 ms (99.85%) | Memory: 42.7 MB (34.98%) - LeetSync

--- Math/Easy/4135-concatenate-non-zero-digits-and-multiply-by-sum-i/concatenate-non-zero-digits-and-multiply-by-sum-i.java
+++ Math/Easy/4135-concatenate-non-zero-digits-and-multiply-by-sum-i/concatenate-non-zero-digits-and-multiply-by-sum-i.java
@@ -1,11 +1,33 @@
 class Solution {
     /**
-     * Approach : Using Math + String Simulation Approach
+     * Approach II : Using Math + String Simulation Approach
+     *
+     * TC : O(d) ~ O(log10(n))
+     * TC : O(1)
+     */
+    public long sumAndMultiply(int n) {
+        long x = 0L;
+        long sum = 0L;
+        int place = 1;
+        while (n > 0) { // TC : O(d)
+            int rem = n % 10;
+            if (rem != 0) {
+                sum += (long) rem;
+                x += rem * place;
+                place *= 10;
+            }
+            n = n / 10;
+        }
+        return x * sum;
+    }
+
+    /**
+     * Approach I : Using Math + String Simulation Approach
      *
      * TC : O(d) + O(d) ~ O(d) ~ O(log10(n))
      * TC : O(d) ~ O(log10(n))
      */
-    public long sumAndMultiply(int n) {
+    public long sumAndMultiplyBruteForce(int n) {
         long x = 0L;
         long sum = 0L;
         StringBuilder sb = new StringBuilder();      // SC : O(d)
