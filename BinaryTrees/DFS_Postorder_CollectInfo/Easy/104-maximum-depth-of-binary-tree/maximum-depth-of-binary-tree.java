aacaeb55a2531eff77cf30473f79e49d406b6922
Surajit Rana <surajit1602@gmail.com>
2026-07-14 01:30:02 +0530

    Time: 0 ms (100.00%) | Memory: 47 MB (9.55%) - LeetSync

---
+++ BinaryTrees/DFS_Postorder_CollectInfo/Easy/104-maximum-depth-of-binary-tree/maximum-depth-of-binary-tree.java
@@ -0,0 +1,31 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    /**
+     * Approach : Using DFS Post-Order Traversal Approach
+     * 
+     * TC : O(n)
+     * SC : O(h) ~ O(n) in worst case
+     */
+    public int maxDepth(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        int leftDepth = maxDepth(root.left);
+        int rightDepth = maxDepth(root.right);
+        return 1 + Math.max(leftDepth, rightDepth);
+    }
+}
