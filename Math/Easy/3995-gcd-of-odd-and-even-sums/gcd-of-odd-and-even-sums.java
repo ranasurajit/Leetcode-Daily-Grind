a721602f9b2e897cf1e81ca0bfd8c7c2fb10a820
Surajit Rana <surajit1602@gmail.com>
2026-07-15 10:58:56 +0530

    Time: 1 ms (83.11%) | Memory: 42.6 MB (39.85%) - LeetSync

--- Math/Easy/3995-gcd-of-odd-and-even-sums/gcd-of-odd-and-even-sums.java
+++ Math/Easy/3995-gcd-of-odd-and-even-sums/gcd-of-odd-and-even-sums.java
@@ -1,5 +1,24 @@
 class Solution {
     /**
+     * Approach II : Using Math Approach
+     *
+     * TC : O(k)
+     * SC : O(1)
+     *
+     * where k = Sum(n odd numbers)
+     */
+    public int gcdOfOddEvenSums(int n) {
+        /**
+         * Odd and Even n numbers will form an AP series
+         *
+         * Sum = (n / 2) * (2 * a + (n - 1) * d)
+         */
+        int sumOdd = (n * (2 * 1 + (n - 1) * 2)) / 2;
+        int sumEven = (n * (2 * 2 + (n - 1) * 2)) / 2;
+        return gcd(sumEven, sumOdd); // TC : O(sumOdd)
+    }
+
+    /**
      * Approach I : Using Math + Simulation Approach
      *
      * TC : O(n) + O(k)
@@ -7,7 +26,7 @@ class Solution {
      *
      * where k = Sum(n odd numbers)
      */
-    public int gcdOfOddEvenSums(int n) {
+    public int gcdOfOddEvenSumsSimulation(int n) {
         int sumOdd = 0;
         int sumEven = 0;
         int oddNum = 1;
