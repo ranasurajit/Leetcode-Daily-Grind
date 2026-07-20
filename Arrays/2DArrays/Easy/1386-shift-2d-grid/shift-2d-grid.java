960c77296ad57cbd984a9a3eb2226eb4a5a61100
Surajit Rana <surajit1602@gmail.com>
2026-07-20 12:20:37 +0530

    Time: 7 ms (43.22%) | Memory: 46.9 MB (96.55%) - LeetSync

---
+++ Arrays/2DArrays/Easy/1386-shift-2d-grid/shift-2d-grid.java
@@ -0,0 +1,40 @@
+class Solution {
+    /**
+     * Approach : Using Array Simulation Approach
+     *
+     * TC : O(m x n x k) + O(m x n) ~ O(m x n x k)
+     * SC : O(1)
+     */
+    public List<List<Integer>> shiftGrid(int[][] grid, int k) {
+        int m = grid.length;
+        int n = grid[0].length;
+        /**
+         * the result of rotation will repeat after every (m x n) shifts
+         */
+        k = k % (m * n);
+        int firstCell = 0;
+        for (int shift = 0; shift < k; shift++) {  // TC : O(k)
+            for (int i = m - 1; i >= 0; i--) {     // TC : O(m)
+                int temp = grid[i][n - 1];
+                for (int j = n - 2; j >= 0; j--) { // TC : O(n)
+                    grid[i][j + 1] = grid[i][j]; 
+                }
+                if (i + 1 < m) {
+                    grid[i + 1][0] = temp;
+                } else {
+                    firstCell = temp;
+                }
+            }
+            grid[0][0] = firstCell;
+        }
+        List<List<Integer>> result = new ArrayList<>();
+        for (int i = 0; i < m; i++) {     // TC : O(m)
+            List<Integer> row = new ArrayList<>();
+            for (int j = 0; j < n; j++) { // TC : O(n)
+                row.add(grid[i][j]);
+            }
+            result.add(row);
+        }
+        return result;
+    }
+}
