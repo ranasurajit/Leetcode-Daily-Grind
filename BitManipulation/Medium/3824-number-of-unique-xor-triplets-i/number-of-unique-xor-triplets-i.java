d89ad7d34f0c2b8cb840194ba142fc248c1bd84e
Surajit Rana <surajit1602@gmail.com>
2026-07-23 13:24:08 +0530

    Time: 0 ms (100.00%) | Memory: 125.3 MB (53.06%) - LeetSync

--- BitManipulation/Medium/3824-number-of-unique-xor-triplets-i/number-of-unique-xor-triplets-i.java
+++ BitManipulation/Medium/3824-number-of-unique-xor-triplets-i/number-of-unique-xor-triplets-i.java
@@ -1,5 +1,32 @@
 class Solution {
     /**
+     * Approach III : Using Bit Manipulation Approach
+     *
+     * TC : O(1)
+     * SC : O(1)
+     *
+     * Accepted (785 / 785 testcases passed)
+     */
+    public int uniqueXorTriplets(int[] nums) {
+        int n = nums.length;
+        if (n <= 2) {
+            return n;
+        }
+        /**
+         * we need to find the number of bits used for n
+         * i.e. if n = 6 so it's binary representation is 110
+         * so maximum we can have all 1's in 3 bits i.e. 111
+         * after performing XOR operations so we need to find
+         * 3 = 32 - number of leading zeroes in bit 
+         * representation of n
+         *
+         * all unique values that can be formed are 2 ^ (bits used in n)
+         */
+        int zeroes = Integer.numberOfLeadingZeros(n);
+        return 1 << (32 - zeroes);
+    }
+
+    /**
      * Approach II : Using Math + Bit Manipulation Approach
      *
      * TC : O(32) ~ O(1)
@@ -7,7 +34,7 @@ class Solution {
      *
      * Accepted (785 / 785 testcases passed)
      */
-    public int uniqueXorTriplets(int[] nums) {
+    public int uniqueXorTripletsBetter(int[] nums) {
         int n = nums.length;
         if (n <= 2) {
             return n;
