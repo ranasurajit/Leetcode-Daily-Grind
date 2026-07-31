99b6401ca44c841c593691c76faebbac984636ac
Surajit Rana <surajit1602@gmail.com>
2026-07-31 13:19:19 +0530

    Time: 8 ms (99.71%) | Memory: 47.7 MB (95.95%) - LeetSync

--- GreedyAlgorithm/Medium/3276-minimum-number-of-pushes-to-type-word-ii/minimum-number-of-pushes-to-type-word-ii.java
+++ GreedyAlgorithm/Medium/3276-minimum-number-of-pushes-to-type-word-ii/minimum-number-of-pushes-to-type-word-ii.java
@@ -3,7 +3,7 @@ class Solution {
      * Approach : Using Greedy + Hashing Approach
      *
      * TC : O(n) + O(26) ~ O(n)
-     * SC : O(26) ~ O(1)
+     * SC : O(52) ~ O(1)
      */
     public int minimumPushes(String word) {
         int n = word.length();
@@ -14,29 +14,26 @@ class Solution {
          * to be mapped to the keys so that minimum
          * key push is needed to type the same
          */
-        int[][] freq = new int[26][2];     // SC : O(26 x 2)
+        int[] freq = new int[26];     // SC : O(26)
         for (int i = 0; i < n; i++) { // TC : O(n)
-            int idx = word.charAt(i) - 'a';
-            freq[idx][0] = idx;
-            freq[idx][1]++;
+            freq[word.charAt(i) - 'a']++;
         }
         /**
          * now we need to sort the 'freq' array so
          * that we can map the highest frequencies first
          */
-        Arrays.sort(freq, (a, b) -> {
-            if (a[1] == b[1]) {
-                return b[0] - a[0];
-            }
-            return b[1] - a[1];
-        }); // TC : O(52 x log(52)) ~ O(1)
+        Arrays.sort(freq); // TC : O(26 x log(26)) ~ O(1)
         int step = 1;
         int pushes = 0;
         int alloc = 0;
-        for (int i = 0; i < 26; i++) { // TC : O(26)
-            pushes += freq[i][1] * step;
+        for (int i = 25; i >= 0; i--) { // TC : O(26)
+            pushes += freq[i] * step;
             alloc++;
             if (alloc % 8 == 0) {
+                /*
+                 * keys (2-9) can only be mapped 
+                 * so, maximum key allocation = 8
+                 */
                 step++;
                 alloc = 0;
             }
