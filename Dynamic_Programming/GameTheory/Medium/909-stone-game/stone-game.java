3a7cc857e21517884885ae78c048bd753ed987b7
Surajit Rana <surajit1602@gmail.com>
2026-08-02 13:27:22 +0530

    Time: 11 ms (7.25%) | Memory: 49.6 MB (7.53%) - LeetSync

--- Dynamic_Programming/GameTheory/Medium/909-stone-game/stone-game.java
+++ Dynamic_Programming/GameTheory/Medium/909-stone-game/stone-game.java
@@ -1,6 +1,112 @@
 class Solution {
     /**
-     * Approach II : Using Memoization (Top-Down) Approach
+     * Approach IV : Using Memoization (Top-Down) - II Approach
+     *
+     * TC : O(n²)
+     * SC : O(n²) + O(n) ~ O(n²)
+     * - O(n) - recursion stack
+     * - O(n²) - memoization memory
+     *
+     * Accepted (46 / 46 testcases passed)
+     */
+    public boolean stoneGame(int[] piles) {
+        int n = piles.length;
+        /**
+         * we need to maximize the difference of aliceScore - bobScore
+         * and return true if aliceScore - bobScore > 0
+         *
+         * below solve(i, j) returns the maximum difference of scores
+         * for Alice and Bob 
+         */
+        Integer[][] memo = new Integer[n + 1][n + 1];
+        return solveMem(0, n - 1, piles, memo) > 0; // TC : O(n²), SC : O(n)
+    }
+
+    /**
+     * Using Memoization Approach
+     *
+     * TC : O(n²)
+     * SC : O(n)
+     */
+    private int solveMem(int i, int j, int[] piles, Integer[][] memo) {
+        // Base Case
+        if (i > j) {
+            // out of bounds
+            return 0;
+        }
+        if (i == j) {
+            return piles[i];
+        }
+        // Memoization Check
+        if (memo[i][j] != null) {
+            return memo[i][j];
+        }
+        // Recursion Calls
+        /**
+         * Alice has two choices:
+         * 1. if Alice chooses index 'i', then Bob can choose
+         * index [i + 1, j]
+         *
+         * 2. if Alice chooses index 'j', then Bob can choose
+         * index [i, j - 1]
+         */
+        int take_i = piles[i] - solveMem(i + 1, j, piles, memo);
+        int take_j = piles[j] - solveMem(i, j - 1, piles, memo);
+        return memo[i][j] = Math.max(take_i, take_j);
+    }
+
+    /**
+     * Approach III : Using Recursion - II Approach
+     *
+     * TC : O(2ⁿ)
+     * SC : O(n)
+     * - O(n) - recursion stack
+     *
+     * Time Limit Exceeded (26 / 46 testcases passed)
+     */
+    public boolean stoneGameRecursionII(int[] piles) {
+        int n = piles.length;
+        /**
+         * we need to maximize the difference of aliceScore - bobScore
+         * and return true if aliceScore - bobScore > 0
+         *
+         * below solve(i, j) returns the maximum difference of scores
+         * for Alice and Bob 
+         */
+        return solve(0, n - 1, piles) > 0; // TC : O(2ⁿ), SC : O(n)
+    }
+
+    /**
+     * Using Recursion Approach
+     *
+     * TC : O(2ⁿ)
+     * SC : O(n)
+     */
+    private int solve(int i, int j, int[] piles) {
+        // Base Case
+        if (i > j) {
+            // out of bounds
+            return 0;
+        }
+        if (i == j) {
+            return piles[i];
+        }
+        // Recursion Calls
+        /**
+         * Alice has two choices:
+         * 1. if Alice chooses index 'i', then Bob can choose
+         * index [i + 1, j]
+         *
+         * 2. if Alice chooses index 'j', then Bob can choose
+         * index [i, j - 1]
+         */
+        int take_i = piles[i] - solve(i + 1, j, piles);
+        int take_j = piles[j] - solve(i, j - 1, piles);
+        return Math.max(take_i, take_j);
+    }
+
+    /**
+     * Approach II : Using Memoization (Top-Down) - I Approach
      *
      * TC : O(n) + O(n²) ~ O(n²)
      * SC : O(n²) + O(n) ~ O(n²)
@@ -9,7 +115,7 @@ class Solution {
      *
      * Accepted (46 / 46 testcases passed)
      */
-    public boolean stoneGame(int[] piles) {
+    public boolean stoneGameMemoization(int[] piles) {
         int n = piles.length;
         int totalScore = 0;
         for (int i = 0; i < n; i++) { // TC : O(n)
@@ -62,7 +168,7 @@ class Solution {
     }
 
     /**
-     * Approach I : Using Recursion Approach
+     * Approach I : Using Recursion - I Approach
      *
      * TC : O(n) + O(2ⁿ) ~ O(2ⁿ)
      * SC : O(n)
@@ -70,7 +176,7 @@ class Solution {
      *
      * Time Limit Exceeded (26 / 46 testcases passed)
      */
-    public boolean stoneGameRecursion(int[] piles) {
+    public boolean stoneGameRecursionI(int[] piles) {
         int n = piles.length;
         int totalScore = 0;
         for (int i = 0; i < n; i++) { // TC : O(n)
