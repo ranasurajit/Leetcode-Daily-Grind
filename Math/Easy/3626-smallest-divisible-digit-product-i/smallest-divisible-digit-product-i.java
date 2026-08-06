77c8ebadb09652848be1bb5b151969f8341e737d
Surajit Rana <surajit1602@gmail.com>
2026-08-06 10:49:49 +0530

    Time: 1 ms (100.00%) | Memory: 42.7 MB (50.26%) - LeetSync

---
+++ Math/Easy/3626-smallest-divisible-digit-product-i/smallest-divisible-digit-product-i.java
@@ -0,0 +1,32 @@
+class Solution {
+    /**
+     * Approach : Using Math + Simulation Approach
+     *
+     * TC : O(m x log10(m))
+     * SC : O(1)
+     */
+    public int smallestNumber(int n, int t) {
+        for (int i = n; i < Integer.MAX_VALUE; i++) { // TC : O(m)
+            if (isDivisible(i, t)) { // TC : O(log10(m))
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    /**
+     * Using Math + Simulation Approach
+     *
+     * TC : O(log10(num))
+     * SC : O(1)
+     */
+    private boolean isDivisible(int num, int t) {
+        long product = 1L;
+        while (num > 0) {
+            int digit = num % 10;
+            product *= (long) digit;
+            num /= 10;
+        }
+        return product % t == 0;
+    }
+}
