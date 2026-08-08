d3e142a35c5e987bc7beaa06b33cc2d09dbc9fdb
Surajit Rana <surajit1602@gmail.com>
2026-08-08 15:14:56 +0530

    Time: 49 ms (13.89%) | Memory: 137.9 MB (16.67%) - LeetSync

--- GreedyAlgorithm/Medium/3584-find-the-lexicographically-smallest-valid-sequence/find-the-lexicographically-smallest-valid-sequence.java
+++ GreedyAlgorithm/Medium/3584-find-the-lexicographically-smallest-valid-sequence/find-the-lexicographically-smallest-valid-sequence.java
@@ -1,4 +1,10 @@
 class Solution {
+    /**
+     * Approach : Using Prefix-Array + Two Pointers Approach
+     *
+     * TC : O(n + m) + O(n + m) + O(n + m) ~ O(n + m)
+     * SC : O(m) + O(m) ~ O(m)
+     */
     public int[] validSequence(String word1, String word2) {
         int n = word1.length();
         int m = word2.length();
@@ -6,22 +12,22 @@ class Solution {
          * we can greedily try to fill the matching index
          * of word1 that matches with the indices of word2
          */
-        int[] suffix = new int[m];
+        int[] suffix = new int[m]; // SC : O(m)
         Arrays.fill(suffix, -1);
         int p = n - 1; // pointer at the end of String 'word1'
         int q = m - 1; // pointer at the end of String 'word2'
-        while (p >= 0 && q >= 0) {
+        while (p >= 0 && q >= 0) { // TC : O(n + m)
             if (word1.charAt(p) == word2.charAt(q)) {
                 suffix[q] = p;
                 q--;
             }
             p--;
         }
-        int[] prefix = new int[m];
+        int[] prefix = new int[m]; // SC : O(m)
         Arrays.fill(prefix, -1);
         p = 0; // pointer at the start of String 'word1'
         q = 0; // pointer at the start of String 'word2'
-        while (p < n && q < m) {
+        while (p < n && q < m) { // TC : O(n + m)
             if (word1.charAt(p) == word2.charAt(q)) {
                 prefix[q] = p;
                 q++;
@@ -32,7 +38,7 @@ class Solution {
         q = 0; // pointer at the start of array 'prefix'
         boolean usedModification = false;
         int[] result = new int[m];
-        while (p < n && q < m) {
+        while (p < n && q < m) { // TC : O(n + m)
             if (word1.charAt(p) == word2.charAt(q)) {
                 // matches
                 result[q] = p;
