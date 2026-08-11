95a95eb756bd1260b37e6697a6274fe9ccac40d5
Surajit Rana <surajit1602@gmail.com>
2026-08-11 12:20:44 +0530

    Time: 0 ms (100.00%) | Memory: 44.1 MB (50.35%) - LeetSync

---
+++ Hashing/Easy/3236-smallest-missing-integer-greater-than-sequential-prefix-sum/smallest-missing-integer-greater-than-sequential-prefix-sum.java
@@ -0,0 +1,34 @@
+class Solution {
+    /**
+     * Approach : Using Hashing + DP Approach
+     *
+     * TC : O(n) + O(n) + O(n) + O(r) ~ O(n + r)
+     * SC : O(n)
+     *
+     * where r = (totalSum - longestPrefixSum)
+     */
+    public int missingInteger(int[] nums) {
+        int n = nums.length;
+        boolean[] present = new boolean[51]; // SC : O(51) ~ O(1)
+        int totalSum = 0;
+        for (int i = 0; i < n; i++) { // TC : O(n)
+            present[nums[i]] = true; 
+            totalSum += nums[i];
+        }
+        int sum = nums[0];
+        for (int i = 1; i < n; i++) {
+            if (nums[i] == nums[i - 1] + 1) {
+                sum += nums[i];
+            } else {
+                // we cannot start prefix without nums[0]
+                break;
+            }
+        }
+        for (int i = sum; i <= totalSum; i++) { // TC : O(r)
+            if (i >= 51 || (i < 51 && !present[i])) {
+                return i;
+            }
+        }
+        return sum + 1;
+    }
+}
