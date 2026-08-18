c0c19f36a00eec3fae85ad024a2809df86662220
Surajit Rana <surajit1602@gmail.com>
2026-08-18 14:11:55 +0530

    Time: 1 ms (99.30%) | Memory: 44.5 MB (83.51%) - LeetSync

--- SlidingWindow/FixedSize/Easy/3705-find-the-largest-almost-missing-integer/find-the-largest-almost-missing-integer.java
+++ SlidingWindow/FixedSize/Easy/3705-find-the-largest-almost-missing-integer/find-the-largest-almost-missing-integer.java
@@ -1,11 +1,66 @@
 class Solution {
     /**
-     * Approach II : Using Sliding Window (Fixed Size) + Hashing Approach
+     * Approach III : Using Optimal Hashing Approach
+     *
+     * TC : O(n)
+     * SC : O(51) ~ O(1)
+     */
+    public int largestInteger(int[] nums, int k) {
+        int n = nums.length;
+        int maxMissing = -1;
+        /**
+         * there are essentially 3 cases:
+         *
+         * case 1 : k == n,
+         * we need to return maximum element in 'nums' array  
+         *
+         * case 2 : k == 1,
+         * all elements will appear uniquely so we return the
+         * maximum  element in 'nums' array
+         *
+         * case 3 : 1 < k < n
+         * all elements except nums[0] and nums[n - 1]
+         * will appear more than once so, we can return
+         * Maximum of nums[0] & nums[n - 1] if they appear 
+         * once in the array
+         * so we need frequency of elements in 'nums'
+         */
+        // case 1 : k == n
+        if (k == n) {
+            for (int x : nums) { // TC : O(n)
+                maxMissing = Math.max(maxMissing, x);
+            }
+            return maxMissing;
+        }
+        int[] freq = new int[51]; // SC : O(51), as 1 <= nums[i] <= 50
+        for (int x : nums) { // TC : O(n)
+            freq[x]++;
+        }
+        // case 2 : k == 1
+        if (k == 1) {
+            for (int i = 50; i >= 0; i--) { // TC : O(51)
+                if (freq[i] == 1) {
+                    return i;
+                }
+            }
+        }
+        // case 3 : 1 < k < n
+        if (freq[nums[0]] == 1) {
+            maxMissing = Math.max(maxMissing, nums[0]);
+        }
+        if (freq[nums[n - 1]] == 1) {
+            maxMissing = Math.max(maxMissing, nums[n - 1]);
+        }
+        return maxMissing;
+    }
+
+    /**
+     * Approach II : Using Better Sliding Window (Fixed Size) + Hashing Approach
      *
      * TC : O(k x n) ~ O(n²)
      * SC : O(n) + O(n) ~ O(n)
      */
-    public int largestInteger(int[] nums, int k) {
+    public int largestIntegerBetter(int[] nums, int k) {
         int n = nums.length;
         int maxMissing = -1;
         /**
