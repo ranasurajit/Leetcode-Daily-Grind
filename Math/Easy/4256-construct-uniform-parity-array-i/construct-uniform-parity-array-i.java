410809aa2eb71b768d3193d0d78c82b838995a1f
Surajit Rana <surajit1602@gmail.com>
2026-09-02 12:10:47 +0530

    Time: 0 ms (100.00%) | Memory: 45.1 MB (57.87%) - LeetSync

--- Math/Easy/4256-construct-uniform-parity-array-i/construct-uniform-parity-array-i.java
+++ Math/Easy/4256-construct-uniform-parity-array-i/construct-uniform-parity-array-i.java
@@ -1,19 +1,15 @@
 class Solution {
+    /**
+     * Approach : Using Math Approach
+     *
+     * TC : O(1)
+     * SC : O(1)
+     */
     public boolean uniformArray(int[] nums1) {
-        int n = nums1.length;
-        int[] nums2 = new int[n];
-        int odds = 0;
-        int evens = 0;
-        for (int i = 0; i < n; i++) {
-            if ((nums1[i] & 1) == 0) {
-                evens++;
-            } else {
-                odds++;
-            }
-        }
-        if (odds == 0 || evens == 0) {
-            return true;
-        }
+        /**
+         * Mathematically, it is always possible 
+         * to make the array uniform
+         */
         return true;
     }
 }
