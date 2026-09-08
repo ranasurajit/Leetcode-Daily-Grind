0f0504c2cc23c86b00b4b42d7acb2cbcd6f63e5d
Surajit Rana <surajit1602@gmail.com>
2026-09-08 11:01:46 +0530

    Time: 18 ms (5.40%) | Memory: 42.8 MB (22.48%) - LeetSync

--- Math/Easy/4245-count-commas-in-range/count-commas-in-range.java
+++ Math/Easy/4245-count-commas-in-range/count-commas-in-range.java
@@ -10,10 +10,11 @@ class Solution {
         for (int i = n; i >= 1; i--) {    // TC : O(n)
             int num = i;
             if (num / 1000 >= 1) { 
-                while (num / 1000 >= 1) { // TC : O(1)
-                    count++;
-                    num /= 1000;
-                }
+                count++;
+                // while (num / 1000 >= 1) { // TC : O(1)
+                //     count++;
+                //     num /= 1000;
+                // }
             } else {
                 // early exit as it is not possible to have any more commas
                 break;
