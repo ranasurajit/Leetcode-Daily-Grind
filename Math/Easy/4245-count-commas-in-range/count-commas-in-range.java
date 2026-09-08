ee281953fa8c18e5d53bec0b38ee55bc7df1e82a
Surajit Rana <surajit1602@gmail.com>
2026-09-08 11:00:59 +0530

    Time: 53 ms (5.40%) | Memory: 42.8 MB (22.48%) - LeetSync

---
+++ Math/Easy/4245-count-commas-in-range/count-commas-in-range.java
@@ -0,0 +1,24 @@
+class Solution {
+    /**
+     * Approach : Using Math Approach
+     *
+     * TC : O(n)
+     * SC : O(1)
+     */
+    public int countCommas(int n) {
+        int count = 0;
+        for (int i = n; i >= 1; i--) {    // TC : O(n)
+            int num = i;
+            if (num / 1000 >= 1) { 
+                while (num / 1000 >= 1) { // TC : O(1)
+                    count++;
+                    num /= 1000;
+                }
+            } else {
+                // early exit as it is not possible to have any more commas
+                break;
+            }
+        }
+        return count;
+    }
+}
