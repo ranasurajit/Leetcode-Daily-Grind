c4d1b93ba5a5521c9f6b93789205539c7c3429e2
Surajit Rana <surajit1602@gmail.com>
2026-09-14 17:06:05 +0530

    Time: 0 ms (100.00%) | Memory: 42.2 MB (98.23%) - LeetSync

---
+++ Math/Easy/866-rectangle-overlap/rectangle-overlap.java
@@ -0,0 +1,13 @@
+class Solution {
+    /**
+     * Approach : Using Math + Geometry Approach
+     *
+     * TC : O(1)
+     * SC : O(1)
+     */
+    public boolean isRectangleOverlap(int[] rec1, int[] rec2) {
+        int xOverlap = Math.min(rec1[2], rec2[2]) - Math.max(rec1[0], rec2[0]);
+        int yOverlap = Math.min(rec1[3], rec2[3]) - Math.max(rec1[1], rec2[1]);
+        return xOverlap > 0 && yOverlap > 0;
+    }
+}
