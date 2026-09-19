031befc0356ce8789b3106e83385352a09a98af7
Surajit Rana <surajit1602@gmail.com>
2026-09-19 14:59:00 +0530

    Time: 0 ms (100.00%) | Memory: 41.8 MB (94.96%) - LeetSync

---
+++ Math/Medium/1501-circle-and-rectangle-overlapping/circle-and-rectangle-overlapping.java
@@ -0,0 +1,21 @@
+class Solution {
+    public boolean checkOverlap(int radius, int xCenter, int yCenter, int x1,
+        int y1, int x2, int y2) {
+        /**
+         * find the closest point of rectangle to the center 
+         * of circle in both (x, y) coordinates
+         */
+        int closestX = Math.max(x1, Math.min(xCenter, x2));
+        int closestY = Math.max(y1, Math.min(yCenter, y2));
+        /**
+         * find the distance of the closest point 
+         */
+        int dx = xCenter - closestX;
+        int dy = yCenter - closestY;
+        /**
+         * the circle and rectangle will ovrelap if the squared
+         * distance of coordinated distance <= squared radius
+         */
+         return dx * dx + dy * dy <= radius * radius;
+    }
+}
